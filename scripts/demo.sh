aaede38640cd5f36f25958ee1b9ee3d19a8fab20
Alexander Diemand <codieplusplus@apax.net>
2019-06-20 15:14:04 +0200

    start nodes with logging configs
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -6,15 +6,19 @@ ALGO="--bft"
 SCR="./scripts/start-node.sh"
 CMD="${SCR} ${ALGO}"
 
+function mklogcfg () {
+  echo "--log-config configuration/log-config-${1}.log"
+}
+
 tmux split-window -h
 tmux split-window -v
 tmux select-pane -t 0
 tmux split-window -v
 
 tmux select-pane -t 0
-tmux send-keys "${CMD} -n 0" C-m
+tmux send-keys "${CMD} $(mklogcfg 0) -n 0" C-m
 tmux select-pane -t 1
-tmux send-keys "${CMD} -n 1" C-m
+tmux send-keys "${CMD} $((mklogcfg 1)) -n 1" C-m
 tmux select-pane -t 2
-tmux send-keys "${CMD} -n 2" C-m
+tmux send-keys "${CMD} $(mklogcfg 2) -n 2" C-m
 
