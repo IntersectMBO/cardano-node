a940f8f6f6373be8daec0cfc4f443a48b010fd76
Duncan Coutts <duncan@well-typed.com>
2019-08-10 14:44:53 +0100

    Address TODOs in Cardano.Node.Configuration.Conversion
    This conversion code is not yet used however. Instead the conversion is
    currently mixed into handleSimpleNode.

--- cardano-node/Cardano/Node/Configuration/Conversion.hs
+++ cardano-node/Cardano/Node/Configuration/Conversion.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | This module converts `CardanoConfiguration` to `Genesis.Config`
--- TODO(KS): We need to promote the hidden module in cardano-ledger to public.
+
+-- TODO: This is not yet used.
 
 module Cardano.Node.Configuration.Conversion
   ( convertConfig
@@ -12,43 +13,26 @@ where
 import Cardano.Prelude
 
 import Cardano.Binary (Raw)
--- TODO(KS): Hidden module in cardano-ledger!
---import Cardano.Chain.Genesis.Config
---  ( Config
---  , ConfigurationError(..)
---  , mkConfigFromFile
---  )
+import Cardano.Chain.Genesis (Config, ConfigurationError(..), mkConfigFromFile)
 import Cardano.Crypto.Hashing (Hash, decodeAbstractHash)
-import Cardano.Crypto.ProtocolMagic
-  (RequiresNetworkMagic(..))
+import Cardano.Crypto.ProtocolMagic (RequiresNetworkMagic(..))
 import Cardano.Node.Configuration.Types as Shell
-
-
-data ConfigurationError
-    = MissingConfigurationError
-    | ConfigurationLoadingError Text
-    deriving (Eq, Show)
 
 -- | Converts 'CardanoConfiguration' (from 'cardano-shell') into a 'Config' (from 'cardano-ledger')
 --   This function mostly relies on FromJSON parsing, because much of 'Config' originates from
---   data locked in the genesis block as JSON. Sadly when generating the 'Config' for mainnet,
---   this means that `cardano-ledger` can't benefit as much from `cardano-shell`'s function as
---   an insulating layer around config & parsing. - @mhuesch
+--   data locked in the genesis block as JSON.
+--
 convertConfig
   :: (MonadIO m, MonadError ConfigurationError m)
   => Shell.CardanoConfiguration
-  -> m ()
---  -> m Config
+  -> m Config
 convertConfig cc = do
 
     -- Genesis hash
     let mainnetGenFp = coGenesisFile $ ccCore cc
-    gHash <- decodeGenesisHash genesisHash `wrapError` ConfigurationLoadingError
-    --gHash <- decodeGenesisHash genesisHash `wrapError` GenesisHashDecodeError
+    gHash <- decodeGenesisHash genesisHash `wrapError` GenesisHashDecodeError
 
-    --mkConfigFromFile (cvtRNM . coRequiresNetworkMagic $ ccCore cc) mainnetGenFp gHash
-
-    pure ()
+    mkConfigFromFile (cvtRNM . coRequiresNetworkMagic $ ccCore cc) mainnetGenFp gHash
  where
   decodeGenesisHash :: Text -> Either Text (Hash Raw)
   decodeGenesisHash genHash = decodeAbstractHash genHash
