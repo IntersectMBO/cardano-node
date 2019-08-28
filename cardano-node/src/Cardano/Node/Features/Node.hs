b08dd0b0e12c9251ad122aa2e2fc8ce8204072b8
Jordan Millar <jordan.millar@iohk.io>
2019-08-28 15:14:14 -0400

    Factor out cardano node feature

---
+++ cardano-node/src/Cardano/Node/Features/Node.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE RankNTypes #-}
+
+
+module Cardano.Node.Features.Node
+  ( NodeLayer(..)
+  , createNodeFeature
+  ) where
+
+import           Cardano.Prelude
+
+import           Cardano.Config.Types (CardanoConfiguration (..),
+                                       CardanoEnvironment (..))
+import           Cardano.Node.Features.Logging (LoggingLayer (..),)
+import           Cardano.Node.Run
+import           Cardano.Shell.Types (CardanoFeature (..))
+
+
+-------------------------------------------------------------------------------
+-- Layer
+-------------------------------------------------------------------------------
+
+data NodeLayer = NodeLayer
+    { nlRunNode   :: forall m. MonadIO m => m ()
+    }
+
+-------------------------------------------------------------------------------
+-- Node Feature
+-------------------------------------------------------------------------------
+
+
+createNodeFeature
+  :: LoggingLayer
+  -> NodeCLIArguments
+  -> CardanoEnvironment
+  -> CardanoConfiguration
+  -> IO (NodeLayer, CardanoFeature)
+createNodeFeature loggingLayer nodeCLI cardanoEnvironment cardanoConfiguration = do
+    -- we parse any additional configuration if there is any
+    -- We don't know where the user wants to fetch the additional
+    -- configuration from, it could be from the filesystem, so
+    -- we give him the most flexible/powerful context, @IO@.
+
+    -- Construct the node layer
+    nodeLayer <- createNodeLayer
+                   cardanoEnvironment
+                   loggingLayer
+                   cardanoConfiguration
+                   nodeCLI
+
+    -- Construct the cardano feature
+    let cardanoFeature :: CardanoFeature
+        cardanoFeature =
+         CardanoFeature
+           { featureName = "NodeFeature"
+           , featureStart = void $ pure nodeLayer
+           , featureShutdown = pure ()
+           }
+
+    pure (nodeLayer, cardanoFeature)
+  where
+    createNodeLayer :: CardanoEnvironment -> LoggingLayer -> CardanoConfiguration -> NodeCLIArguments -> IO NodeLayer
+    createNodeLayer _ logLayer cc nodeCli = do
+        pure $ NodeLayer {nlRunNode = liftIO $ runNode nodeCli logLayer cc}
