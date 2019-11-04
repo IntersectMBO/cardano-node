6d413c2eb98463d70f938561716104e50208a583
Duncan Coutts <duncan@well-typed.com>
2019-11-04 13:29:40 +0000

    Update version and changelog

--- nix/.stack.nix/cardano-node.nix
+++ nix/.stack.nix/cardano-node.nix
@@ -3,7 +3,7 @@
     flags = {};
     package = {
       specVersion = "1.10";
-      identifier = { name = "cardano-node"; version = "3.0.1.87"; };
+      identifier = { name = "cardano-node"; version = "1.0.0"; };
       license = "Apache-2.0";
       copyright = "";
       maintainer = "operations@iohk.io";
