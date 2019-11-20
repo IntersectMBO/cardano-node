748744e92e9f8b585274e3f4e230adf1402c6ca4
Alexander Diemand <codieplusplus@apax.net>
2019-11-20 22:45:27 +0100

    clone and compile explorer
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- benchmarking/cluster3nodes/benchmark.sh
+++ benchmarking/cluster3nodes/benchmark.sh
@@ -19,10 +19,12 @@ mkdir -p db logs socket
 # exit on first error
 set -e
 
+
 # generate new genesis
+./genesis.sh
+
 
 # prepare SQL database
-
 # (assuming user 'cexplorer' has been defined in the database system)
 . configuration/psql-settings.sh
 
@@ -33,15 +35,17 @@ psql -d postgres -c "CREATE DATABASE cexplorer OWNER=cexplorer;"
 # run cluster in tmux
 tmux new-s -s Cluster3Node "./run-3node-cluster.sh" 
 
+
 # run transaction generator (parallel)
 {
   sleep 15
   echo "running tx generator"
 } &
 
+
 # run explorer (parallel)
 {
-  sleep 18
+  sleep 14
   echo "running explorer"
 } &
 
@@ -53,4 +57,13 @@ echo "waiting for transactions..."
 # analyse benchmark run
 sleep 5
 echo "analyse data"
+
+echo "TPS - transactions per second"
+echo "TBPS - transaction bytes per second"
+echo "number of slots total"
+echo "number of slots per node"
+echo "number of missed slots"
+echo "distribution of block size in transactions"
+echo "distribution of block size in bytes"
+echo "distribution of seconds between blocks"
 
