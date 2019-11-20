8a8278cb776581602a51c78f8d4799c4b375b3a2
Samuel Leathers <samuel.leathers@iohk.io>
2019-11-20 13:51:28 -0500

    switch to niv for dependency management

--- lib.nix
+++ lib.nix
@@ -1,21 +1,11 @@
 let
-  # iohk-nix can be overridden for debugging purposes by setting
-  # NIX_PATH=iohk_nix=/path/to/iohk-nix
-  iohkNix = import (
-    let try = builtins.tryEval <iohk_nix>;
-    in if try.success
-    then builtins.trace "using host <iohk_nix>" try.value
-    else
-      let
-        spec = builtins.fromJSON (builtins.readFile ./nix/iohk-nix-src.json);
-      in builtins.fetchTarball {
-        url = "${spec.url}/archive/${spec.rev}.tar.gz";
-        inherit (spec) sha256;
-      }) {};
-
+  sources = import ./nix/sources.nix;
+  iohkNix = import sources.iohk-nix { };
   pkgs = iohkNix.pkgs;
   lib = pkgs.lib;
+  niv = (import sources.niv {}).niv;
 in lib // iohkNix.cardanoLib // {
   inherit iohkNix pkgs;
   inherit (iohkNix) nix-tools;
+  inherit niv;
 }
--- shell.nix
+++ shell.nix
@@ -1,7 +1,26 @@
 { withHoogle ? true
+, localLib ? import ./lib.nix
 }:
 let
+  pkgs = localLib.iohkNix.pkgs;
   default = import ./default.nix {};
+  devops = pkgs.stdenv.mkDerivation {
+    name = "devops-shell";
+    buildInputs = [
+      localLib.niv
+    ];
+    shellHook = ''
+      echo "DevOps Tools" \
+      | ${pkgs.figlet}/bin/figlet -f banner -c \
+      | ${pkgs.lolcat}/bin/lolcat
+
+      echo "NOTE: you may need to export GITHUB_TOKEN if you hit rate limits with niv"
+      echo "Commands:
+        * niv update <package> - update package
+
+      "
+    '';
+  };
 in
 default.nix-tools._raw.shellFor {
   packages    = ps: with ps; [ cardano-node ];
@@ -14,4 +33,4 @@ default.nix-tools._raw.shellFor {
   (with default.nix-tools._raw._config._module.args.pkgs; [
     tmux
   ]);
-}
+} // { inherit devops; }
