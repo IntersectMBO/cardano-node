5a9e20a8546de51cd768409c2a07c40c5825c6ef
Samuel Leathers <samuel.leathers@iohk.io>
2019-11-27 16:48:31 -0500

    fix svcLib dependency

--- lib.nix
+++ lib.nix
@@ -1,6 +1,7 @@
 let
   sources = import ./nix/sources.nix;
   pkgs' = import sources.nixpkgs {};
+  nixTools = import ./nix/nix-tools.nix {};
   haskellNixJson = let
     src = sources."haskell.nix";
   in __toJSON {
@@ -9,7 +10,7 @@ let
   };
   iohkNix = import sources.iohk-nix { haskellNixJsonOverride = pkgs'.writeText "haskell-nix.json" haskellNixJson; };
   pkgs = iohkNix.pkgs;
-  svcLib = import ./nix/svclib.nix { inherit pkgs; };
+  svcLib = import ./nix/svclib.nix { inherit pkgs; cardano-node = nixTools.nix-tools.exes.cardano-node; };
   lib = pkgs.lib;
   niv = (import sources.niv {}).niv;
 in lib // iohkNix.cardanoLib // svcLib // {
