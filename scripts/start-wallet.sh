beec33816aa1daa38cb77b8b312bf0ca7d3bb16b
Jordan Millar <jordan.millar@iohk.io>
2019-12-10 13:30:38 -0400

    Update `scripts/start-wallet.sh`

--- scripts/start-wallet.sh
+++ scripts/start-wallet.sh
@@ -2,17 +2,13 @@
 
 set -x
 
-genesis_hash=`cat configuration/GenesisFiles/GENHASH`
-genesis_root="configuration/GenesisFiles"
+genesis_root="configuration/genesis"
+genesis_hash=`cat "${genesis_root}/GENHASH"`
 genesis_file="${genesis_root}/genesis.json"
 
 cabal v2-run wallet-client -- \
-    --log-config configuration/log-config-0.yaml \
-    --topology configuration/simple-topology.json \
-    --database-path ./db/ \
-    --genesis-file ${genesis_file} \
-    --socket-dir ./socket/ \
-    --genesis-hash ${genesis_hash} \
-    --port 1234 \
     --config configuration/log-config-0.yaml \
+    --socket-dir ./socket/ \
+    --genesis-json ${genesis_file} \
+    --genesis-hash ${genesis_hash} \
     $@
