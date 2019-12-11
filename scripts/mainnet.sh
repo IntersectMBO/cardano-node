fcdd9823218292fdd302fd5690d750fc18b6be8b
Alexander Diemand <codieplusplus@apax.net>
2019-12-11 11:48:00 +0100

    debugging without logging
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- scripts/mainnet.sh
+++ scripts/mainnet.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
 RUNNER=${RUNNER:-cabal v2-run --}
 TOPOLOGY=${TOPOLOGY:-"configuration/mainnet-topology.json"}
 
 ARGS=(
-        --database-path           "./db/"
+        --database-path           "./db-0/"
         --genesis-file            "configuration/mainnet-genesis.json"
         --genesis-hash            "5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb"
         --topology                "${TOPOLOGY}"
