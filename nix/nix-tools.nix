ad537d9ab96cb9b696503a64982292db1331ae3b
Samuel Leathers <samuel.leathers@iohk.io>
2020-01-29 00:07:55 -0500

    haskell.nix update to use overlays

--- nix/nix-tools.nix
+++
@@ -1,6 +0,0 @@
-{ ... }@args:
-
-let
-  commonLib = import ../lib.nix args;
-
-in commonLib.nix-tools.default-nix ./pkgs.nix args
--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,50 +1,44 @@
-{ pkgs ? import <nixpkgs> {}
-, iohk-extras ? {}
-, iohk-module ? {}
+{ pkgs
+, src
+, haskellCompiler ? "ghc865"
 , profiling ? false
-, haskell
-, ...
 }:
 let
 
-   # our packages
-  stack-pkgs = import ./.stack.nix/default.nix;
+  haskell = pkgs.haskell-nix;
 
+  # TODO: move to iohk-nix
+  # Chop out a subdirectory of the source, so that the package is only
+  # rebuilt when something in the subdirectory changes.
+  filterSubDir = dir:  with pkgs.lib; let
+      isFiltered = src ? _isLibCleanSourceWith;
+      origSrc = if isFiltered then src.origSrc else src;
+    in cleanSourceWith {
+      inherit src;
+      filter = path: type:
+        type == "directory" ||
+        hasPrefix (toString origSrc + toString dir) path;
+    } + dir;
 
-  # Build the packageset with module support.
-  # We can essentially override anything in the modules
-  # section.
-  #
-  #  packages.cbors.patches = [ ./one.patch ];
-  #  packages.cbors.flags.optimize-gmp = false;
-  #
-  compiler = (stack-pkgs.extras haskell.hackage).compiler.nix-name;
-  pkgSet = haskell.mkStackPkgSet {
-    inherit stack-pkgs;
-    pkg-def-extras = [
-      stack-pkgs.extras
-      iohk-extras.${compiler}
-    ];
-    modules = [
-      # the iohk-module will supply us with the necessary
-      # cross compilation plumbing to make Template Haskell
-      # work when cross compiling.  For now we need to
-      # list the packages that require template haskell
-      # explicity here.
-      iohk-module
-
-       {
-        # Packages we wish to ignore version bounds of.
-        # This is similar to jailbreakCabal, however it
-        # does not require any messing with cabal files.
-        packages.katip.doExactConfig = true;
-        packages.ekg.components.library.enableSeparateDataOutput = true;
-        packages.cardano-node.configureFlags = [ "--ghc-option=-Werror" ];
-        packages.cardano-config.configureFlags = [ "--ghc-option=-Werror" ];
-        enableLibraryProfiling = profiling;
-      }
-    ];
-  };
-
- in
-  pkgSet.config.hsPkgs // { _config = pkgSet.config; }
+  recRecurseIntoAttrs = with pkgs; pred: x: if pred x then recurseIntoAttrs (lib.mapAttrs (n: v: if n == "buildPackages" then v else recRecurseIntoAttrs pred v) x) else x;
+  pkgSet = recRecurseIntoAttrs (x: with pkgs; lib.isAttrs x && !lib.isDerivation x)
+    # we are only intersted in listing the project packages
+    (pkgs.lib.filterAttrs (with pkgs.haskell-nix.haskellLib; (n: p: p != null && (isLocalPackage p && isProjectPackage p) || n == "shellFor"))
+      # from our project which is based on a cabal project.
+      (pkgs.haskell-nix.cabalProject {
+          src = pkgs.haskell-nix.haskellLib.cleanGit { inherit src; };
+          ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+          modules = [
+             {
+              # Packages we wish to ignore version bounds of.
+              # This is similar to jailbreakCabal, however it
+              # does not require any messing with cabal files.
+              packages.katip.doExactConfig = true;
+              packages.ekg.components.library.enableSeparateDataOutput = true;
+              packages.cardano-node.configureFlags = [ "--ghc-option=-Werror" ];
+              packages.cardano-config.configureFlags = [ "--ghc-option=-Werror" ];
+              enableLibraryProfiling = profiling;
+            }
+          ];
+      }));
+ in pkgSet
