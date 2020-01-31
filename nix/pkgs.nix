153f3f013e6bbfd7f3af8aa695a4463856be3ea2
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-01-31 15:48:58 +1300

    Fix windows build
    (cherry picked from commit 2cb3077fd4fed9b21ce22dd95d8b41e993e3b53a)

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -27,10 +27,25 @@ let
       # from our project which is based on a cabal project.
       (pkgs.haskell-nix.cabalProject {
           src = pkgs.haskell-nix.haskellLib.cleanGit { inherit src; };
-          ghc = pkgs.haskell-nix.compiler.${haskellCompiler};
+          ghc = pkgs.buildPackages.haskell-nix.compiler.${haskellCompiler};
           modules = [
-
-             {
+            # Allow reinstallation of Win32
+            { nonReinstallablePkgs =
+              [ "rts" "ghc-heap" "ghc-prim" "integer-gmp" "integer-simple" "base"
+                "deepseq" "array" "ghc-boot-th" "pretty" "template-haskell"
+                # ghcjs custom packages
+                "ghcjs-prim" "ghcjs-th"
+                "ghc-boot"
+                "ghc" "array" "binary" "bytestring" "containers"
+                "filepath" "ghc-boot" "ghc-compact" "ghc-prim"
+                # "ghci" "haskeline"
+                "hpc"
+                "mtl" "parsec" "text" "transformers"
+                "xhtml"
+                # "stm" "terminfo"
+              ];
+            }
+            {
               # Packages we wish to ignore version bounds of.
               # This is similar to jailbreakCabal, however it
               # does not require any messing with cabal files.
@@ -42,7 +57,26 @@ let
               packages.cardano-config.configureFlags = [ "--ghc-option=-Werror" ];
               packages.cardano-config.doCheck = true;
               enableLibraryProfiling = profiling;
-            }
+           }
+           (pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isWindows {
+             # Disable cabal-doctest tests by turning off custom setups
+             packages.comonad.package.buildType = pkgs.lib.mkForce "Simple";
+             packages.distributive.package.buildType = pkgs.lib.mkForce "Simple";
+             packages.lens.package.buildType = pkgs.lib.mkForce "Simple";
+             packages.nonempty-vector.package.buildType = pkgs.lib.mkForce "Simple";
+             packages.semigroupoids.package.buildType = pkgs.lib.mkForce "Simple";
+
+             # Make sure we use a buildPackages version of happy
+             packages.pretty-show.components.library.build-tools = [ pkgs.buildPackages.haskell-nix.haskellPackages.happy ];
+
+             # Remove hsc2hs build-tool dependencies (suitable version will be available as part of the ghc derivation)
+             packages.Win32.components.library.build-tools = pkgs.lib.mkForce [];
+             packages.terminal-size.components.library.build-tools = pkgs.lib.mkForce [];
+             packages.network.components.library.build-tools = pkgs.lib.mkForce [];
+           })
           ];
+        # TODO add flags to packages (like cs-ledger) so we can turn off tests that will
+        # not build for windows on a per package bases (rather than using --disable-tests).
+        configureArgs = pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isWindows "--disable-tests";
       }));
  in pkgSet
