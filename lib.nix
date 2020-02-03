287ba3a17544658b316473b2362856bc8821b546
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-02-03 23:46:54 +0100

    Revert "Make shell functionnal. some progress on stack build."
    This reverts commit 73ac1a50d99423610ed26d3246a10615773b9e7b.

--- lib.nix
+++ lib.nix
@@ -2,19 +2,22 @@
 , crossSystem ? null
 , config ? {}
 , overlays ? []
-, sourcesOverride ? {}
 , profiling ? false
 }:
 
 let
-  sources = (import ./nix/sources.nix) // sourcesOverride;
+  sources = import ./nix/sources.nix;
   iohkNix = import sources.iohk-nix {
-    inherit config system crossSystem;
-    # will override iohk-nix default haskell.nix or custom_nixpkgs if needed:
-    sourcesOverride = builtins.removeAttrs sources [ "nixpkgs" ];
-    nixpkgsOverlays = overlays;
+    sourcesOverride = sources;
   };
-  pkgs = iohkNix.pkgs;
+  haskellNix = import sources."haskell.nix";
+  args = haskellNix // {
+    inherit system crossSystem;
+    overlays = (haskellNix.overlays or []) ++ overlays;
+    config = (haskellNix.config or {}) // config;
+  };
+  nixpkgs = import sources.nixpkgs;
+  pkgs = nixpkgs args;
   haskellPackages = import ./nix/pkgs.nix {
     inherit pkgs profiling;
     src = ./.;
@@ -22,6 +25,7 @@ let
   # TODO: add haskellPackages and fix svclib
   svcLib = import ./nix/svclib.nix { inherit pkgs; cardano-node-packages = haskellPackages.cardano-node.components.exes; };
   lib = pkgs.lib;
+  niv = (import sources.niv {}).niv;
   isCardanoNode = with lib; package:
     (package.isHaskell or false) &&
       ((hasPrefix "cardano-node" package.identifier.name) ||
@@ -30,8 +34,8 @@ let
   getPackageChecks = pkgs.lib.mapAttrs (_: package: package.checks);
 in lib // iohkNix.cardanoLib // {
   inherit (pkgs.haskell-nix.haskellLib) collectComponents;
-  inherit (iohkNix) niv;
   inherit
+    niv
     sources
     haskellPackages
     pkgs
