2f2df7cdb0f7ccad7311d7d104d395bb169d7182
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-02-04 13:04:14 -0600

    Rename mempoolTraceTransformer to mempoolMetricsTraceTransformer

--- cardano-node/src/Cardano/Tracing/Tracers.hs
+++ cardano-node/src/Cardano/Tracing/Tracers.hs
@@ -263,9 +263,9 @@ mkTracers traceOptions tracer = do
           let tr' = appendName "peers" tr
           traceNamedObject tr' (meta, LogValue "connectedPeers" . PureI $ fromIntegral $ length peers)
 
-    mempoolTraceTransformer :: Tracer IO (LogObject a)
-                            -> Tracer IO (TraceEventMempool blk)
-    mempoolTraceTransformer tr = Tracer $ \mempoolEvent -> do
+    mempoolMetricsTraceTransformer :: Tracer IO (LogObject a)
+                                   -> Tracer IO (TraceEventMempool blk)
+    mempoolMetricsTraceTransformer tr = Tracer $ \mempoolEvent -> do
         let tr' = appendName "metrics" tr
             (n, tot) = case mempoolEvent of
                   TraceMempoolAddTxs      txs0 tot0 -> (length txs0, tot0)
@@ -297,7 +297,7 @@ mkTracers traceOptions tracer = do
 
     mempoolTracer :: Tracer IO (TraceEventMempool blk)
     mempoolTracer = Tracer $ \ev -> do
-        traceWith (mempoolTraceTransformer tracer) ev
+        traceWith (mempoolMetricsTraceTransformer tracer) ev
         traceWith (measureTxsStart tracer) ev
         traceWith mpTracer ev
       where
