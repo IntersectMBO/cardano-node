4c69a7d8eb8c416fe6466bc7a047cb216b6c1a0f
Samuel Leathers <samuel.leathers@iohk.io>
2020-02-07 13:37:24 -0500

    more tetst

--- scripts/buildkite/check-hydra-eval.py
+++ scripts/buildkite/check-hydra-eval.py
@@ -3,6 +3,8 @@
 
 import requests
 import os
+import time
+import json
 
 HYDRA_BASE_URL = "https://hydra.iohk.io"
 
@@ -18,4 +20,20 @@ if not BUILDKITE_PR:
 
 hydra_eval = requests.get(hydra_pr_url, headers={"Content-Type": "application/json"})
 
-print(hydra_eval.text)
+retry_count = 0
+while hydra_eval.status_code != 200:
+    print("Hydra PR not created yet - sleeping 1 minute")
+    retry_count = retry_count + 1
+    time.sleep(60)
+    hydra_eval = requests.get(hydra_pr_url, headers={"Content-Type": "application/json"})
+    if retry_count > 60:
+        print("Retried 1 hour - exiting")
+        exit(1)
+
+hydra_eval_data = json.loads(hydra_eval.text)
+
+if hydra_eval_data.errormsg != "":
+    print(f"An error occurred in evaluation:\n{hydra_eval_data.errormsg}")
+    exit(1)
+
+exit(0)
