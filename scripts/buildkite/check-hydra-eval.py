d3c956e84520bb742d59f0c2ab5a5ff67f9da2a8
Samuel Leathers <samuel.leathers@iohk.io>
2020-02-07 13:11:05 -0500

    playing with hydra eval errors

---
+++ scripts/buildkite/check-hydra-eval.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p python3Packages.requests -i python3
+
+import requests
+import os
+
+HYDRA_BASE_URL = "https://hydra.iohk.io"
+
+BUILDKITE_BRANCH = os.getenv("BUILDKITE_PULL_REQUEST", None)
+BUILDKITE_PR = os.getenv("BUILDKITE_PULL_REQUEST", None)
+BUILDKITE_REPO = os.getenv("BUILDKITE_PULL_REQUEST_REPO", None)
+
+hydra_pr_url = f"{HYDRA_BASE_URL}/jobset/Cardano/{BUILDKITE_REPO}-pr-{BUILDKITE_PR}"
+
+
+print(f"{hydra_pr_url}")
