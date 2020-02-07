e5a901f005d7a9f9c6c42cb82826c157ade86b55
Samuel Leathers <samuel.leathers@iohk.io>
2020-02-07 13:23:10 -0500

    more tests

--- scripts/buildkite/check-hydra-eval.py
+++ scripts/buildkite/check-hydra-eval.py
@@ -6,9 +6,9 @@ import os
 
 HYDRA_BASE_URL = "https://hydra.iohk.io"
 
-BUILDKITE_BRANCH = os.getenv("BUILDKITE_PULL_REQUEST", None)
+BUILDKITE_BRANCH = os.getenv("BUILDKITE_BRANCH", None)
 BUILDKITE_PR = os.getenv("BUILDKITE_PULL_REQUEST", None)
-BUILDKITE_REPO = os.getenv("BUILDKITE_PULL_REQUEST_REPO", None)
+BUILDKITE_REPO = os.getenv("BUILDKITE_PIPELINE_NAME", None)
 
 hydra_pr_url = f"{HYDRA_BASE_URL}/jobset/Cardano/{BUILDKITE_REPO}-pr-{BUILDKITE_PR}"
 
