6740afb600d686074ac23822ea6cfcef0d8af652
Alexander Diemand <codieplusplus@apax.net>
2020-02-10 15:32:48 +0100

    CAD-517 adapted analyse script; tested against json log
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- benchmarking/chain-sync/analyse-logs.sh
+++ benchmarking/chain-sync/analyse-logs.sh
@@ -4,11 +4,11 @@ NETWORK=${1:-mainnet}
 
 # get first block copy time
 LOGFILE_JSON=`ls -1 state-node-${NETWORK}/node-0-*.json | head -2`
-FIRSTSLOT=`jq --slurp 'map (select (.data.event.kind == "TraceCopyToImmDBEvent.CopiedBlockToImmDB")) | .[0] | "\(.at);\(.data.tip | sub (".*@(?<slot>.*)$"; "\(.slot)"))"' $LOGFILE_JSON | xargs echo | sed 's/\([0-9]\)T\([0-9]\)/\1 \2/; s/\(20[0-9][0-9]\)/"\1/; s/Z/"/'`
+FIRSTSLOT=`jq -r 'select(.data.event.kind=="TraceCopyToImmDBEvent.CopiedBlockToImmDB") | [ .at, .data.event.kind, .data.event.slot.tip ] | @csv' $LOGFILE_JSON | sed -e 's/"[a-f0-9]\+@\([0-9]\+\)"/\1/;' | sort -k 1.2,1.23 | head -n 1 | sed -e 's/^\("[0-9]\+-[0-9]\+-[0-9]\+\)T\([0-9]\+:[0-9]\+:[0-9]\+[.][0-9]\+\)Z\(.*\)$/\1 \2\3/'`
 
 LOGFILE_JSON=`ls -1r state-node-${NETWORK}/node-0-*.json | head -2`
-echo $LOGFILE_TEXT
-LASTSLOT=`jq 'select (.data.event.kind == "TraceCopyToImmDBEvent.CopiedBlockToImmDB") | "\(.at);\(.data.tip | sub (".*@(?<slot>.*)$"; "\(.slot)"))"' $LOGFILE_JSON | sed 's/T/ /; s/\(20[0-9][0-9]\)/"\1/; s/Z/"/' | sort -k 1.2,1.23 | tail -n 1`
+echo $LOGFILE_JSON
+LASTSLOT=`jq -r 'select(.data.event.kind=="TraceCopyToImmDBEvent.CopiedBlockToImmDB") | [ .at, .data.event.kind, .data.event.slot.tip ] | @csv' $LOGFILE_JSON | sort -k 1.2,1.23 | tail -1 | sed -e 's/"[a-f0-9]\+@\([0-9]\+\)"/\1/;s/^\("[0-9]\+-[0-9]\+-[0-9]\+\)T\([0-9]\+:[0-9]\+:[0-9]\+[.][0-9]\+\)Z\(.*\)$/\1 \2\3/'`
 LASTRSS=`jq -r 'select(.data.kind=="LogValue" and .data.name=="Mem.resident") | [ .at, .data.name, .data.value.contents * 4096 ] | @csv' $LOGFILE_JSON | sort -k 1.2,1.23 | tail -1 | sed -e 's/^"\(.*\)"$/\1/;s/\\"/"/g;s/^\("[0-9]\+-[0-9]\+-[0-9]\+\)T\([0-9]\+:[0-9]\+:[0-9]\+[.][0-9]\+\)Z\(.*\)$/\1 \2\3/'`
 LASTNETIN=`jq -r 'select(.data.kind=="LogValue" and .data.name=="Net.IpExt:InOctets") | [ .at, .data.name, .data.value.contents ] | @csv' $LOGFILE_JSON | sort -k 1.2,1.23 | tail -1 | sed -e 's/^"\(.*\)"$/\1/;s/\\"/"/g;s/^\("[0-9]\+-[0-9]\+-[0-9]\+\)T\([0-9]\+:[0-9]\+:[0-9]\+[.][0-9]\+\)Z\(.*\)$/\1 \2\3/'`
 LASTNETOUT=`jq -r 'select(.data.kind=="LogValue" and .data.name=="Net.IpExt:OutOctets") | [ .at, .data.name, .data.value.contents ] | @csv' $LOGFILE_JSON | sort -k 1.2,1.23 | tail -1 | sed -e 's/^"\(.*\)"$/\1/;s/\\"/"/g;s/^\("[0-9]\+-[0-9]\+-[0-9]\+\)T\([0-9]\+:[0-9]\+:[0-9]\+[.][0-9]\+\)Z\(.*\)$/\1 \2\3/'`
