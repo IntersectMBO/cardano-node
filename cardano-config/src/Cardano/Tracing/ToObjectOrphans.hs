f1e558084ac1543f424146dac856e1a62f4685c4
Kosyrev Serge <serge.kosyrev@iohk.io>
2020-02-19 12:50:20 +0300

    CAD-640:  ToObjectOrphans:  move to cardano-config

--- cardano-config/src/Cardano/Tracing/ToObjectOrphans.hs
+++ cardano-config/src/Cardano/Tracing/ToObjectOrphans.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE UndecidableInstances  #-}
