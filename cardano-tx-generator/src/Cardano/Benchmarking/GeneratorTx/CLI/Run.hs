822bb863a969008eb4b1a688e4a46bff7f2eaec1
Denis Shevchenko <denis.shevchenko@iohk.io>
2020-03-05 11:47:39 +0400

    CAD-701: run generator.

--- cardano-tx-generator/src/Cardano/Benchmarking/GeneratorTx/CLI/Run.hs
+++ cardano-tx-generator/src/Cardano/Benchmarking/GeneratorTx/CLI/Run.hs
@@ -1,70 +1,135 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Cardano.Benchmarking.GeneratorTx.CLI.Run
-  ( runCommand 
+  ( runCommand
   ) where
 
-import           Prelude (error)
+import           Data.Version
+                    ( showVersion )
+import           Data.Text
+                    ( pack )
+import           Paths_cardano_tx_generator
+                    ( version )
+import qualified Formatting as F
 import           Cardano.Prelude hiding (option)
+import           Control.Monad.Trans.Except.Extra
+                    ( firstExceptT, left )
 
-import           Cardano.Benchmarking.GeneratorTx.CLI.Parsers (GenerateTxs (..))
+import qualified Ouroboros.Consensus.Cardano as Consensus
+import           Ouroboros.Network.NodeToClient
+                    ( AssociateWithIOCP
+                    , withIOManager
+                    )
 
-data CliError = CliError Int deriving Show
+import qualified Cardano.Chain.Genesis as Genesis
+import           Cardano.Chain.Update (ApplicationName(..))
+import           Cardano.Config.Logging
+                    ( createLoggingFeatureCLI )
+import           Cardano.Config.Protocol
+                    ( ProtocolInstantiationError
+                    , SomeProtocol(..)
+                    , fromProtocol
+                    )
+import           Cardano.Config.Types
+                    ( GenesisFile(..)
+                    , CardanoEnvironment(..), LastKnownBlockVersion(..)
+                    , Protocol, SigningKeyFile(..), Update(..)
+                    , ncLogMetrics, ncReqNetworkMagic, ncProtocol
+                    , parseNodeConfigurationFP
+                    )
+
+import qualified Cardano.Crypto.Hashing as Crypto
+
+import           Cardano.Benchmarking.GeneratorTx.Error
+                    ( TxGenError )
+import           Cardano.Benchmarking.GeneratorTx.CLI.Parsers
+                    ( GenerateTxs (..) )
+import           Cardano.Benchmarking.GeneratorTx
+                    ( genesisBenchmarkRunner )
+
+data RealPBFTError =
+    IncorrectProtocolSpecified !Protocol
+  | FromProtocolError !ProtocolInstantiationError
+  | GenesisBenchmarkRunnerError !TxGenError
+  deriving Show
+
+data CliError =
+    GenesisReadError !FilePath !Genesis.GenesisDataError
+  | GenerateTxsError !RealPBFTError
+  deriving Show
+
+------------------------------------------------------------------------------------------------
 
 runCommand :: GenerateTxs -> ExceptT CliError IO ()
-runCommand _ = error ""
+runCommand (GenerateTxs logConfigFp
+                        signingKey
+                        delegCert
+                        genFile
+                        socketFp
+                        targetNodeAddresses
+                        numOfTxs
+                        numOfInsPerTx
+                        numOfOutsPerTx
+                        feePerTx
+                        tps
+                        txAdditionalSize
+                        explorerAPIEndpoint
+                        sigKeysFiles) =
+  withIOManagerE $ \iocp -> do
+    -- Default update value
+    let update = Update (ApplicationName "cardano-tx-generator") 1 $ LastKnownBlockVersion 0 2 0
+    nc <- liftIO $ parseNodeConfigurationFP logConfigFp
 
-{-
-runCommand (GenerateTxs
-            logConfigFp
-            signingKey
-            delegCert
-            genFile
-            socketFp
-            targetNodeAddresses
-            numOfTxs
-            numOfInsPerTx
-            numOfOutsPerTx
-            feePerTx
-            tps
-            txAdditionalSize
-            explorerAPIEndpoint
-            sigKeysFiles) = withIOManagerE $ \iocp -> do
-  -- Default update value
-  let update = Update (ApplicationName "cardano-sl") 1 $ LastKnownBlockVersion 0 2 0
-  nc <- liftIO $ parseNodeConfigurationFP logConfigFp
+    -- Logging layer
+    (loggingLayer, _) <- liftIO $ createLoggingFeatureCLI
+                                    (pack $ showVersion version)
+                                    NoEnvironment
+                                    (Just logConfigFp)
+                                    (ncLogMetrics nc)
 
-  -- Logging layer
-  (loggingLayer, _) <- liftIO $ createLoggingFeatureCLI
-                                  (pack $ showVersion version)
-                                  NoEnvironment
-                                  (Just logConfigFp)
-                                  (ncLogMetrics nc)
+    genHash <- getGenesisHash genFile
 
-  genHash <- getGenesisHash genFile
+    SomeProtocol p <- firstExceptT GenerateTxsError $
+        firstExceptT FromProtocolError $ fromProtocol genHash
+                                                      Nothing
+                                                      Nothing
+                                                      (Just genFile)
+                                                      (ncReqNetworkMagic nc)
+                                                      Nothing
+                                                      (Just delegCert)
+                                                      (Just signingKey)
+                                                      update
+                                                      (ncProtocol nc)
+    case p of
+        proto@Consensus.ProtocolRealPBFT{} -> firstExceptT GenerateTxsError $
+            firstExceptT GenesisBenchmarkRunnerError $ genesisBenchmarkRunner
+                                                        loggingLayer
+                                                        iocp
+                                                        socketFp
+                                                        proto
+                                                        targetNodeAddresses
+                                                        numOfTxs
+                                                        numOfInsPerTx
+                                                        numOfOutsPerTx
+                                                        feePerTx
+                                                        tps
+                                                        txAdditionalSize
+                                                        explorerAPIEndpoint
+                                                        [fp | SigningKeyFile fp <- sigKeysFiles]
+        _ -> left $ GenerateTxsError $ IncorrectProtocolSpecified (ncProtocol nc)
 
-  firstExceptT
-    GenerateTxsError
-    $  withRealPBFT
-         genHash
-         genFile
-         (ncReqNetworkMagic nc)
-         Nothing
-         (Just delegCert)
-         (Just signingKey)
-         update
-         (ncProtocol nc) $ \protocol@(Consensus.ProtocolRealPBFT _ _ _ _ _) ->
-                             firstExceptT GenesisBenchmarkRunnerError
-                               $ genesisBenchmarkRunner
-                                    loggingLayer
-                                    iocp
-                                    socketFp
-                                    protocol
-                                    targetNodeAddresses
-                                    numOfTxs
-                                    numOfInsPerTx
-                                    numOfOutsPerTx
-                                    feePerTx
-                                    tps
-                                    txAdditionalSize
-                                    explorerAPIEndpoint
-                                    [fp | SigningKeyFile fp <- sigKeysFiles]
--}
+----------------------------------------------------------------------------
+
+getGenesisHash :: GenesisFile -> ExceptT CliError IO Text
+getGenesisHash genFile = do
+  (_, Genesis.GenesisHash gHash) <- readGenesis genFile
+  return $ F.sformat Crypto.hashHexF gHash
+ where
+  -- | Read genesis from a file.
+  readGenesis :: GenesisFile -> ExceptT CliError IO (Genesis.GenesisData, Genesis.GenesisHash)
+  readGenesis (GenesisFile fp) = firstExceptT (GenesisReadError fp) $ Genesis.readGenesisData fp
+
+withIOManagerE :: (AssociateWithIOCP -> ExceptT e IO a) -> ExceptT e IO a
+withIOManagerE k = ExceptT $ withIOManager (runExceptT . k)
