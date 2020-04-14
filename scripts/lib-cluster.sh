c51707d9eeca693635acc4b0674010c8a7028b9d
Kosyrev Serge <kosyrev.serge@protonmail.com>
2020-04-14 18:37:01 +0300

    CAD-828 scripts | tmux:  ignore user configurations, as they can wreak havoc

--- scripts/lib-cluster.sh
+++ scripts/lib-cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
+TMUX_CONFIG="$(mktemp -t --suffix=tmuXXXXXXXX)"
 run_3node_cluster() {
         dprint "run_3node_cluster: $*"
         local config="$1" topo="${2:-indexed}" delegate="${3:-indexed}"
@@ -8,7 +9,9 @@ run_3node_cluster() {
         setup_genesis_for_config "${config}"
         prebuild 'cardano-node'
 
-        tmux new-session \
+        tmux \
+          -f "${TMUX_CONFIG}" \
+          new-session \
           -E \
           -s "3node-cluster-${config}" \
           -n 'Main' \
