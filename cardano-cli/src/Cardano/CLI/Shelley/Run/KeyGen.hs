a49297c25fabae88f4a914fbe11ba5248af8115e
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-04-28 12:53:25 +1000

    cli: Split key generation into a separate file to allow reuse

---
+++ cardano-cli/src/Cardano/CLI/Shelley/Run/KeyGen.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StrictData #-}
+
+{-# OPTIONS_GHC -Wno-all-missed-specialisations #-}
+
+module Cardano.CLI.Shelley.Run.KeyGen
+  ( runGenesisKeyGenGenesis
+  , runGenesisKeyGenDelegate
+  , runGenesisKeyGenUTxO
+  , runGenesisKeyHash
+  , runGenesisVerKey
+  , runColdKeyGen
+  ) where
+
+import           Cardano.Prelude hiding (option, trace)
+
+import qualified Data.ByteString.Char8 as BS
+
+import           Control.Monad.Trans.Except (ExceptT)
+import           Control.Monad.Trans.Except.Extra (firstExceptT)
+
+import qualified Shelley.Spec.Ledger.Keys as Ledger
+import qualified Cardano.Crypto.Hash.Class as Crypto
+
+import           Cardano.CLI.Key (VerificationKeyFile(..))
+import           Cardano.CLI.Ops (CliError (..))
+import           Cardano.CLI.Shelley.Parsers
+
+import           Cardano.Config.Shelley.ColdKeys
+import           Cardano.Config.Shelley.OCert
+import           Cardano.Config.Types (SigningKeyFile(..))
+
+
+
+runGenesisKeyGenGenesis :: VerificationKeyFile -> SigningKeyFile
+                        -> ExceptT CliError IO ()
+runGenesisKeyGenGenesis = runColdKeyGen GenesisKey
+
+
+runGenesisKeyGenDelegate :: VerificationKeyFile
+                         -> SigningKeyFile
+                         -> OpCertCounterFile
+                         -> ExceptT CliError IO ()
+runGenesisKeyGenDelegate vkeyPath skeyPath (OpCertCounterFile ocertCtrPath) = do
+    runColdKeyGen (OperatorKey GenesisDelegateKey) vkeyPath skeyPath
+    firstExceptT OperationalCertError $
+      writeOperationalCertIssueCounter ocertCtrPath initialCounter
+  where
+    initialCounter = 0
+
+
+runGenesisKeyGenUTxO :: VerificationKeyFile -> SigningKeyFile
+                     -> ExceptT CliError IO ()
+runGenesisKeyGenUTxO = runColdKeyGen GenesisUTxOKey
+
+
+runColdKeyGen :: KeyRole -> VerificationKeyFile -> SigningKeyFile
+              -> ExceptT CliError IO ()
+runColdKeyGen role (VerificationKeyFile vkeyPath) (SigningKeyFile skeyPath) =
+    firstExceptT KeyCliError $ do
+      (vkey, skey) <- liftIO genKeyPair
+      writeVerKey     role vkeyPath vkey
+      writeSigningKey role skeyPath skey
+
+
+runGenesisKeyHash :: VerificationKeyFile -> ExceptT CliError IO ()
+runGenesisKeyHash (VerificationKeyFile vkeyPath) =
+    firstExceptT KeyCliError $ do
+      (vkey, _role) <- readVerKeySomeRole genesisKeyRoles vkeyPath
+      let Ledger.KeyHash khash = Ledger.hashKey vkey
+      liftIO $ BS.putStrLn $ Crypto.getHashBytesAsHex khash
+
+
+runGenesisVerKey :: VerificationKeyFile -> SigningKeyFile
+                 -> ExceptT CliError IO ()
+runGenesisVerKey (VerificationKeyFile vkeyPath) (SigningKeyFile skeyPath) =
+    firstExceptT KeyCliError $ do
+      (skey, role) <- readSigningKeySomeRole genesisKeyRoles skeyPath
+      let vkey = deriveVerKey skey
+      writeVerKey role vkeyPath vkey
+
+genesisKeyRoles :: [KeyRole]
+genesisKeyRoles =
+  [ GenesisKey
+  , GenesisUTxOKey
+  , OperatorKey GenesisDelegateKey
+  ]
+
