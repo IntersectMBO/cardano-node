956e1c1126d5feb5d533b06257fcdc6c5b1f2d44
Craige McWhirter <craige.mcwhirter@iohk.io>
2020-04-29 16:50:56 +1000

    [ADP-286] Enable tagging latest containers

--- .buildkite/docker-build-push.nix
+++ .buildkite/docker-build-push.nix
@@ -1,20 +1,9 @@
-# This script will load nix-built docker images of cardano-node
-# into the Docker daemon (must be running), and then push to the
-# Docker Hub. Credentials for the hub must already be installed with
-# "docker login".
+# This script will load nix-built docker image of cardano-node applications
+# into the Docker daemon (must be running), and then push to the Docker Hub.
+# Credentials for the hub must already be installed with # "docker login".
 #
 # There is a little bit of bash logic to replace the default repo and
 # tag from the nix-build (../nix/docker.nix).
-#
-# 1. The repo (default "inputoutput/cardano-node") is changed to match
-#    the logged in Docker user's credentials.
-#
-# 2. The tag (default "VERSION") is changed to reflect the
-#    branch which is being built under this Buildkite pipeline.
-#
-#    - All commits are tagged with commit hash.
-#    - If the branch is master then master tag is updated
-#    - If there is a tag associated with the commit then that tag is pushed as well
 
 { nodePackages ?  import ../. {}
 
@@ -36,7 +25,7 @@ let
   impureCreated = image: image.overrideAttrs (oldAttrs: { created = "now"; }) // { inherit (image) version; };
 
 in
-  writeScript "docker-build-push" ''
+  writeScript "docker-build-push" (''
     #!${runtimeShell}
 
     set -euo pipefail
@@ -51,24 +40,44 @@ in
     fullrepo="${dockerHubRepoName}"
     ''}
 
+  '' + concatMapStringsSep "\n" (image: ''
     branch="''${BUILDKITE_BRANCH:-}"
-    tag="''${BUILDKITE_TAG:-}"
-    tagged="$fullrepo:$tag"
+    event="''${GITHUB_EVENT_NAME:-}"
+
     gitrev="${image.imageTag}"
+
     echo "Loading $fullrepo:$gitrev"
     docker load -i ${image}
+
+    # An image is built on each commit
     echo "Pushing $fullrepo:$gitrev"
     docker push "$fullrepo:$gitrev"
+
+    # If a release event, apply two tags to the image
+    # e.g. "1.0.0" AND "latest"
+    if [[ "$event" = release ]]; then
+      ref="''${GITHUB_REF:-}"
+      version="$(echo $ref | sed -e 's/refs\/tags\///')"
+
+      echo "Tagging with a version number: $fullrepo:$version"
+      docker tag $fullrepo:$gitrev $fullrepo:$version
+      echo "Pushing $fullrepo:$version"
+      docker push "$fullrepo:$version"
+
+      echo "Tagging as latest"
+      docker tag $fullrepo:$version $fullrepo:latest
+      echo "Pushing $fullrepo:latest"
+      docker push "$fullrepo:latest"
+    fi
+
+    # Every commit to master needs to be tagged with master
     if [[ "$branch" = master ]]; then
       echo "Tagging as master"
       docker tag $fullrepo:$gitrev $fullrepo:$branch
       echo "Pushing $fullrepo:$branch"
       docker push "$fullrepo:$branch"
     fi
-    if [[ "$tag" ]]; then
-      echo "Tagging as $tag"
-      docker tag $fullrepo:$gitrev $fullrepo:$tag
-      echo "Pushing $fullrepo:$tag"
-      docker push "$fullrepo:$tag"
-    fi
-  ''
+
+    echo "Cleaning up with docker system prune"
+    docker system prune -f
+  '') [ image ] )
