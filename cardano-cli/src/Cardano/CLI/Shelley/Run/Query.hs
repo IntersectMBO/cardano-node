c711a46f171e1bcc2ae575d53183f123b8e102ab
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-05-05 21:20:01 -0500

    Add CLI command for querying UTxO filtered by an address

--- cardano-cli/src/Cardano/CLI/Shelley/Run/Query.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Run/Query.hs
@@ -6,14 +6,15 @@ module Cardano.CLI.Shelley.Run.Query
 
 import           Cardano.Prelude
 
-import           Cardano.Api (queryPParamsFromLocalState)
+import           Cardano.Api (queryFilteredUTxOFromLocalState, queryPParamsFromLocalState)
 
 import           Cardano.CLI.Ops (CliError (..), getLocalTip, withIOManagerE)
-import           Cardano.CLI.Shelley.Parsers (OutputFile (..), QueryCmd (..))
+import           Cardano.CLI.Shelley.Parsers (AddressFile (..), OutputFile (..), QueryCmd (..))
 
 import           Cardano.Common.LocalSocket (chooseSocketPath)
 
 import           Cardano.Config.Protocol (mkConsensusProtocol)
+import           Cardano.Config.Shelley.Address (AddressRole (..), readAddress)
 import           Cardano.Config.Types (CLISocketPath, ConfigYamlFilePath, NodeConfiguration (..),
                      SomeConsensusProtocol (..), parseNodeConfigurationFP)
 
@@ -22,6 +23,7 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, handleIOExceptT
 
 import           Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Set as Set
 
 import           Ouroboros.Consensus.Cardano (Protocol (..))
 
@@ -32,6 +34,8 @@ import           Shelley.Spec.Ledger.PParams (PParams)
 runQueryCmd :: QueryCmd -> ExceptT CliError IO ()
 runQueryCmd (QueryProtocolParameters configFp mbSockPath outFile) =
   runQueryProtocolParameters configFp mbSockPath outFile
+runQueryCmd (QueryFilteredUTxO af configFp mbSockPath outFile) =
+  runQueryFilteredUTxO af configFp mbSockPath outFile
 runQueryCmd cmd = liftIO $ putStrLn $ "runQueryCmd: " ++ show cmd
 
 runQueryProtocolParameters
@@ -49,6 +53,28 @@ runQueryProtocolParameters configFp mbSockPath (OutputFile outFile) = do
         pparams <- firstExceptT NodeLocalStateQueryError $
           queryPParamsFromLocalState ptcl sockPath (getTipPoint tip)
         writeProtocolParameters outFile pparams
+      _ -> left $ IncorrectProtocolSpecifiedError (ncProtocol nc)
+
+runQueryFilteredUTxO
+  :: AddressFile
+  -> ConfigYamlFilePath
+  -> Maybe CLISocketPath
+  -> OutputFile
+  -> ExceptT CliError IO ()
+runQueryFilteredUTxO (AddressFile addrPath) configFp mbSockPath (OutputFile _outFile) = do
+    -- TODO: Add support for other 'AddressRole's.
+    addr <- firstExceptT AddressCliError $ readAddress BootstrapAddr addrPath
+
+    nc <- liftIO $ parseNodeConfigurationFP configFp
+    sockPath <- pure $ chooseSocketPath (ncSocketPath nc) mbSockPath
+    SomeConsensusProtocol p <- firstExceptT ProtocolError $ mkConsensusProtocol nc Nothing
+
+    case p of
+      ptcl@ProtocolRealTPraos{} -> do
+        tip <- withIOManagerE $ \iocp -> liftIO $ getLocalTip p iocp sockPath
+        filteredUtxo <- firstExceptT NodeLocalStateQueryError $
+          queryFilteredUTxOFromLocalState ptcl sockPath (Set.singleton addr) (getTipPoint tip)
+        liftIO $ putStrLn $ "Filtered UTxO: " ++ show filteredUtxo
       _ -> left $ IncorrectProtocolSpecifiedError (ncProtocol nc)
 
 writeProtocolParameters :: FilePath -> PParams -> ExceptT CliError IO ()
