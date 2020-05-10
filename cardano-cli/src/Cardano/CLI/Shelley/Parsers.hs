9096962be3b98ce08bfec7ffd781142fd4e98507
Jordan Millar <jordan.millar@iohk.io>
2020-05-10 18:16:57 +0100

    Add commands to generate staking address keys and stake pool keys

--- cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -110,6 +110,8 @@ data NodeCmd
   | NodeKeyGenVRF  VerificationKeyFile SigningKeyFile
   | NodeIssueOpCert VerificationKeyFile SigningKeyFile OpCertCounterFile
                     KESPeriod OutputFile
+  | NodeStakingKeyGen  VerificationKeyFile SigningKeyFile
+  | NodeStakePoolKeyGen  VerificationKeyFile SigningKeyFile
   deriving (Eq, Show)
 
 
@@ -443,6 +445,12 @@ pNodeCmd =
       , Opt.command "issue-op-cert"
           (Opt.info pIssueOpCert $
              Opt.progDesc "Issue a node operational certificate")
+      , Opt.command "key-gen-staking"
+          (Opt.info pStakingKeyPair $
+             Opt.progDesc "Create an address staking key pair")
+      , Opt.command "key-gen-stake-pool"
+          (Opt.info pStakePoolKeyPair $
+             Opt.progDesc "Create a stake pool key pair")
       ]
   where
     pKeyGenOperator :: Parser NodeCmd
@@ -466,6 +474,16 @@ pNodeCmd =
                       <*> pOperatorCertIssueCounterFile
                       <*> pKesPeriod
                       <*> pOutputFile
+
+    pStakingKeyPair :: Parser NodeCmd
+    pStakingKeyPair = NodeStakingKeyGen
+                        <$> pVerificationKeyFile Output
+                        <*> pSigningKeyFile Output
+
+    pStakePoolKeyPair :: Parser NodeCmd
+    pStakePoolKeyPair = NodeStakePoolKeyGen
+                          <$> pVerificationKeyFile Output
+                          <*> pSigningKeyFile Output
 
 
 pPoolCmd :: Parser PoolCmd
