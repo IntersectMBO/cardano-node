b4093f0cbd85c93f3588382780bfd45b780dbe9c
Duncan Coutts <duncan@well-typed.com>
2020-05-11 18:15:05 +0100

    More tx submit plubming
    Add result reporting to the new tx submit API and add stubs in the CLI.

--- cardano-api/src/Cardano/Api/TxSubmitChairman.hs
+++ cardano-api/src/Cardano/Api/TxSubmitChairman.hs
@@ -8,13 +8,14 @@
 
 module Cardano.Api.TxSubmitChairman
   ( submitTx
+  , TxSubmitResult(..)
   ) where
 
 import           Cardano.Prelude
 
 import           Control.Tracer
+import           Control.Concurrent.STM
 
-import           Cardano.Api.TxSubmit (prepareTxByron, prepareTxShelley)
 import           Cardano.Api.Types
 
 import           Ouroboros.Consensus.Cardano (protocolClientInfo)
@@ -23,46 +24,75 @@ import           Ouroboros.Consensus.Node.ProtocolInfo (ProtocolClientInfo(..))
 import           Ouroboros.Consensus.Node.NetworkProtocolVersion
                   (nodeToClientProtocolVersion, supportedNodeToClientVersions)
 import           Ouroboros.Consensus.Node.Run
-import           Ouroboros.Consensus.Mempool (GenTx)
+import           Ouroboros.Consensus.Mempool (GenTx, ApplyTxErr)
 
 import           Ouroboros.Network.Driver (runPeer)
 import           Ouroboros.Network.Mux
 import           Ouroboros.Network.NodeToClient hiding (NodeToClientVersion (..))
 import qualified Ouroboros.Network.NodeToClient as NtC
-import qualified Ouroboros.Network.Protocol.LocalTxSubmission.Client as LocalTxSub
+import           Ouroboros.Network.Protocol.LocalTxSubmission.Client
 
 import           Cardano.Chain.Slotting (EpochSlots (..))
+
+import qualified Ouroboros.Consensus.Byron.Ledger as Byron
+import qualified Cardano.Chain.UTxO as Byron
+
+import           Ouroboros.Consensus.Shelley.Ledger.Mempool (mkShelleyTx)
+import           Ouroboros.Consensus.Shelley.Ledger (ShelleyBlock)
+import           Ouroboros.Consensus.Byron.Ledger (ByronBlock)
+import           Ouroboros.Consensus.Shelley.Protocol.Crypto (TPraosStandardCrypto)
 
 import           Cardano.Config.Byron.Protocol (mkNodeClientProtocolRealPBFT)
 import           Cardano.Config.Shelley.Protocol (mkNodeClientProtocolTPraos)
 import           Cardano.Config.Types (SocketPath(..))
 
 
+data TxSubmitResult
+   = TxSubmitSuccess
+   | TxSubmitFailureByron   (ApplyTxErr ByronBlock)
+   | TxSubmitFailureShelley (ApplyTxErr (ShelleyBlock TPraosStandardCrypto))
+
 submitTx
   :: Network
   -> SocketPath
   -> TxSigned
-  -> IO ()
+  -> IO TxSubmitResult
 submitTx network socketPath tx =
     NtC.withIOManager $ \iocp ->
       case tx of
-        TxSignedByron _ _ _ _ ->
+        TxSignedByron txbody _txCbor _txHash vwit -> do
+          let aTxAux = Byron.annotateTxAux (Byron.mkTxAux txbody vwit)
+              genTx  = Byron.ByronTx (Byron.byronIdTx aTxAux) aTxAux
+          resultVar <- newEmptyTMVarIO
           submitGenTx
             nullTracer
             iocp
             (protocolClientInfo $ mkNodeClientProtocolRealPBFT $ EpochSlots 21600)
             network
             socketPath
-            (prepareTxByron tx)
+            resultVar
+            genTx
+          result <- atomically (readTMVar resultVar)
+          case result of
+            Nothing  -> return TxSubmitSuccess
+            Just err -> return (TxSubmitFailureByron err)
 
-        TxSignedShelley _ -> do
+        TxSignedShelley stx -> do
+          let genTx = mkShelleyTx stx
+          resultVar <- newEmptyTMVarIO
           submitGenTx
             nullTracer
             iocp
             (protocolClientInfo mkNodeClientProtocolTPraos)
             network
             socketPath
-            (prepareTxShelley tx)
+            resultVar
+            genTx
+          result <- atomically (readTMVar resultVar)
+          case result of
+            Nothing  -> return TxSubmitSuccess
+            Just err -> return (TxSubmitFailureShelley err)
+
 
 submitGenTx
   :: forall blk.
@@ -72,16 +102,17 @@ submitGenTx
   -> ProtocolClientInfo blk
   -> Network
   -> SocketPath
+  -> TMVar (Maybe (ApplyTxErr blk)) -- ^ Result will be placed here
   -> GenTx blk
   -> IO ()
-submitGenTx tracer iomgr cfg nm (SocketPath path) genTx =
+submitGenTx tracer iomgr cfg nm (SocketPath path) resultVar genTx =
       connectTo
         (localSnocket iomgr path)
         NetworkConnectTracers {
             nctMuxTracer       = nullTracer,
             nctHandshakeTracer = nullTracer
             }
-        (localInitiatorNetworkApplication tracer cfg nm genTx)
+        (localInitiatorNetworkApplication tracer cfg nm resultVar genTx)
         path
         --`catch` handleMuxError tracer chainsVar socketPath
 
@@ -94,11 +125,12 @@ localInitiatorNetworkApplication
   -- in 'ouroboros-network' package).
   -> ProtocolClientInfo blk
   -> Network
+  -> TMVar (Maybe (ApplyTxErr blk)) -- ^ Result will be placed here
   -> GenTx blk
   -> Versions NtC.NodeToClientVersion DictVersion
               (LocalConnectionId
                -> OuroborosApplication InitiatorApp LByteString IO () Void)
-localInitiatorNetworkApplication tracer cfg nm genTx =
+localInitiatorNetworkApplication tracer cfg nm resultVar genTx =
     foldMapVersions
       (\v ->
         NtC.versionedNodeToClientProtocols
@@ -129,12 +161,12 @@ localInitiatorNetworkApplication tracer cfg nm genTx =
                             nullTracer -- (contramap show tracer)
                             cTxSubmissionCodec
                             channel
-                            (LocalTxSub.localTxSubmissionClientPeer
+                            (localTxSubmissionClientPeer
                                (txSubmissionClientSingle tx))
                 case result of
-                  Nothing  -> traceWith tracer "Transaction accepted"
-                  Just _TODO -> traceWith tracer "Transaction rejected"
-                  --TODO: return the result
+                  Nothing -> traceWith tracer "Transaction accepted"
+                  Just _  -> traceWith tracer "Transaction rejected"
+                atomically $ putTMVar resultVar result
 
         , localStateQueryProtocol =
             InitiatorProtocolOnly $
@@ -154,7 +186,8 @@ txSubmissionClientSingle
   :: forall tx reject m.
      Applicative m
   => tx
-  -> LocalTxSub.LocalTxSubmissionClient tx reject m (Maybe reject)
-txSubmissionClientSingle tx = LocalTxSub.LocalTxSubmissionClient $ do
-    pure $ LocalTxSub.SendMsgSubmitTx tx $ \mreject ->
-      pure (LocalTxSub.SendMsgDone mreject)
+  -> LocalTxSubmissionClient tx reject m (Maybe reject)
+txSubmissionClientSingle tx =
+    LocalTxSubmissionClient $
+    pure $ SendMsgSubmitTx tx $ \result ->
+    pure (SendMsgDone result)
