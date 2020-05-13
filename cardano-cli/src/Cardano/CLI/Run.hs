9d55b931229baaa558e63e70db6439843f2f8065
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-05-13 18:01:26 +1000

    cli: Add the git revision to the version command output

--- cardano-cli/src/Cardano/CLI/Run.hs
+++ cardano-cli/src/Cardano/CLI/Run.hs
@@ -18,6 +18,7 @@ import           Cardano.CLI.Shelley.Run (runShelleyClientCommand)
 import           Data.Version (showVersion)
 import           Paths_cardano_cli (version)
 import           System.Info (arch, compilerName, compilerVersion, os)
+import           Cardano.Config.GitRev (gitRev)
 
 
 runClientCommand :: ClientCommand -> ExceptT CliError IO ()
@@ -29,9 +30,10 @@ runClientCommand DisplayVersion     = runDisplayVersion
 runDisplayVersion :: ExceptT CliError IO ()
 runDisplayVersion = do
     liftIO . putTextLn $ mconcat
-                [ "cardano-cli " <> renderVersion version
-                , " - " <> Text.pack os <> "-" <> Text.pack arch
-                , " - " <> Text.pack compilerName <> "-" <> renderVersion compilerVersion
+                [ "cardano-cli ", renderVersion version
+                , " - ", Text.pack os, "-", Text.pack arch
+                , " - ", Text.pack compilerName, "-", renderVersion compilerVersion
+                , "\ngit rev ", gitRev
                 ]
   where
     renderVersion = Text.pack . showVersion
