3a269a1407e2d7b95bd82156f03b86a02b0e2cd5
Jordan Millar <jordan.millar@iohk.io>
2020-05-27 16:12:06 +0100

    Implement conversion of JSON to MetaData

--- cardano-api/test/Test/Cardano/Api/Gen.hs
+++ cardano-api/test/Test/Cardano/Api/Gen.hs
@@ -7,6 +7,7 @@ module Test.Cardano.Api.Gen
   ( genAddress
   , genCertificate
   , genGenesisVerificationKey
+  , genMetaData
   , genNetwork
   , genNetworkMagic
   , genPaymentVerificationKey
@@ -34,7 +35,7 @@ import           Cardano.Binary (serialize)
 import qualified Cardano.Crypto as Byron
 import           Cardano.Crypto.DSIGN
 import           Cardano.Crypto.VRF.Class (deriveVerKeyVRF, genKeyVRF, seedSizeVRF)
-import           Cardano.Prelude
+import           Cardano.Prelude hiding (MetaData)
 
 import qualified Cardano.Crypto.Hash.Class as Crypto
 import qualified Cardano.Crypto.Hash.Blake2b as Crypto
@@ -56,6 +57,7 @@ import           Shelley.Spec.Ledger.Coin (Coin (..))
 import           Shelley.Spec.Ledger.Crypto (DSIGN, VRF)
 import           Shelley.Spec.Ledger.Keys (KeyHash, SignKeyVRF, VerKeyVRF,
                    VKey(..), hash, hashKey)
+import           Shelley.Spec.Ledger.MetaData (MetaData(..), MetaDatum(..))
 import           Shelley.Spec.Ledger.PParams (PParamsUpdate,
                    PParams'(..), ProtVer(..))
 import           Shelley.Spec.Ledger.Slot (EpochNo(..))
@@ -217,8 +219,6 @@ genStakePoolOwnersShelley = do
   keyHashes <- Gen.list (Range.linear 1 5) genVerificationKeyHashStakingShelley
   return $ Set.fromList keyHashes
 
--- TODO: Should make sure this covers
--- the 0 to 1 range adequately.
 genStakePoolMarginShelley :: Gen UnitInterval
 genStakePoolMarginShelley = do
   numerator' <- Gen.double (Range.constantFrom 0 0 1)
@@ -419,6 +419,29 @@ genVRFVerificationKeyHashShelley :: Gen ShelleyVRFVerificationKeyHash
 genVRFVerificationKeyHashShelley = do
   (_, vrfVKey) <- genVRFKeyPair
   return $ hash vrfVKey
+
+genMetaData :: Gen MetaData
+genMetaData = do
+  numberOfIndicies <- Gen.integral (Range.linear 1 15)
+  let indexes = map (\i -> fromIntegral i :: Word64) [1..numberOfIndicies]
+  mDatums <- Gen.list (Range.singleton numberOfIndicies) genMetaDatum
+  return . MetaData . Map.fromList $ zip indexes mDatums
+
+genMetaDatum :: Gen MetaDatum
+genMetaDatum = do
+    int <- Gen.list (Range.linear 1 5) (I <$> Gen.integral (Range.linear 1 100))
+    bytes <- Gen.list (Range.linear 1 5) (B <$> Gen.bytes (Range.linear 1 20))
+    str <- Gen.list (Range.linear 1 5) (S <$> Gen.text (Range.linear 1 20) Gen.alphaNum)
+    let mDatumList = int ++ bytes ++ str
+
+    singleMetaDatum <- Gen.element mDatumList
+
+    Gen.choice
+      [ return $ List mDatumList
+      , return $ Map [(singleMetaDatum, singleMetaDatum)]
+      , return $ Map [(List mDatumList, singleMetaDatum)]
+      , return $ Map [(singleMetaDatum, List mDatumList)]
+      ]
 
 -- -------------------------------------------------------------------------------------------------
 
