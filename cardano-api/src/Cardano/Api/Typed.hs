e2fee80374723ecca669966b3ab7a34d87fd31ad
Duncan Coutts <duncan@well-typed.com>
2020-06-24 16:02:35 +0100

    Initial support for Byron witnesses in Shelley txs in typed API
    Also fill in shelleyKeyWitness for typed api
    
    So we can now construct Byron and Shelley witnesses for Byron and
    Shelley era transactions, and assemble them into a full Tx.
    
    And also convenience functions signByronTransaction and
    signShelleyTransaction

--- cardano-api/src/Cardano/Api/Typed.hs
+++ cardano-api/src/Cardano/Api/Typed.hs
@@ -106,9 +106,8 @@ module Cardano.Api.Typed (
     -- ** Incremental signing and separate witnesses
     makeSignedTransaction,
     Witness(..),
-    byronKeyWitness,
-    shelleyKeyWitness,
-    shelleyScriptWitness,
+    makeKeyWitness,
+    makeScriptWitness,
 
     -- * Fee calculation
 
@@ -224,7 +223,6 @@ import           Data.Proxy (Proxy(..))
 import           Data.Kind (Constraint)
 import           Data.Word
 import           Data.Maybe
-import           Data.Either
 import           Data.Bifunctor (first)
 import           Data.List as List
 import qualified Data.List.NonEmpty as NonEmpty
@@ -258,7 +256,9 @@ import           Data.Aeson (ToJSON(..), FromJSON(..))
 -- Common types, consensus, network
 --
 import qualified Cardano.Binary as CBOR
-import           Cardano.Binary (ToCBOR(toCBOR), FromCBOR(fromCBOR))
+import           Cardano.Binary
+                   (ToCBOR(toCBOR), FromCBOR(fromCBOR),
+                    Annotated(..), reAnnotate, recoverBytes)
 import qualified Cardano.Prelude as CBOR (cborError)
 import           Shelley.Spec.Ledger.Serialization (CBORGroup(..))
 import           Cardano.Slotting.Slot (SlotNo)
@@ -279,6 +279,8 @@ import qualified Cardano.Crypto.VRF.Class   as Crypto
 import qualified Cardano.Crypto.Hashing as Byron
 import qualified Cardano.Crypto.Signing as Byron
 import qualified Cardano.Crypto.ProtocolMagic as Byron
+import qualified Cardano.Crypto.Wallet as Byron.Crypto.Wallet
+
 import qualified Cardano.Chain.Common as Byron
 import qualified Cardano.Chain.Genesis as Byron
 import qualified Cardano.Chain.UTxO   as Byron
@@ -289,7 +291,7 @@ import qualified Cardano.Chain.UTxO   as Byron
 import qualified Ouroboros.Consensus.Shelley.Protocol.Crypto as Shelley
 
 import qualified Shelley.Spec.Ledger.Address                 as Shelley
---import qualified Shelley.Spec.Ledger.Address.Bootstrap       as Shelley
+import qualified Shelley.Spec.Ledger.Address.Bootstrap       as Shelley
 import qualified Shelley.Spec.Ledger.BaseTypes               as Shelley
 import qualified Shelley.Spec.Ledger.Coin                    as Shelley
 import qualified Shelley.Spec.Ledger.Credential              as Shelley
@@ -620,8 +622,7 @@ getTxId :: TxBody era -> TxId
 getTxId (ByronTxBody tx) =
     TxId
   . Crypto.UnsafeHash
-  . Byron.hashToBytes
-  . Byron.serializeCborHash
+  . recoverBytes
   $ tx
 
 getTxId (ShelleyTxBody tx) =
@@ -684,7 +685,7 @@ toShelleyLovelace (Lovelace l) = Shelley.Coin l
 data TxBody era where
 
      ByronTxBody
-       :: Byron.Tx
+       :: Annotated Byron.Tx ByteString
        -> TxBody Byron
 
      ShelleyTxBody
@@ -702,10 +703,15 @@ instance HasTypeProxy (TxBody Shelley) where
 
 
 instance SerialiseAsCBOR (TxBody Byron) where
-    serialiseToCBOR (ByronTxBody txbody) = CBOR.serialize' txbody
+    serialiseToCBOR (ByronTxBody txbody) =
+      recoverBytes txbody
 
-    deserialiseFromCBOR AsByronTxBody bs =
-      ByronTxBody <$> CBOR.decodeFull' bs
+    deserialiseFromCBOR AsByronTxBody bs = do
+      ByronTxBody <$>
+        CBOR.decodeFullAnnotatedBytes
+          "Byron TxBody"
+          CBOR.fromCBORAnnotated
+          (LBS.fromStrict bs)
 
 instance SerialiseAsCBOR (TxBody Shelley) where
     serialiseToCBOR (ShelleyTxBody txbody) =
@@ -713,7 +719,10 @@ instance SerialiseAsCBOR (TxBody Shelley) where
 
     deserialiseFromCBOR AsShelleyTxBody bs =
       ShelleyTxBody <$>
-        CBOR.decodeAnnotator "Shelley TxBody" fromCBOR (LBS.fromStrict bs)
+        CBOR.decodeAnnotator
+          "Shelley TxBody"
+          fromCBOR
+          (LBS.fromStrict bs)
 
 
 instance HasTextEnvelope (TxBody Byron) where
@@ -742,7 +751,10 @@ makeByronTransaction ins outs = do
                 outs'
     return $
       ByronTxBody $
-        Byron.UnsafeTx ins'' outs'' (Byron.mkAttributes ())
+        reAnnotate $
+          Annotated
+            (Byron.UnsafeTx ins'' outs'' (Byron.mkAttributes ()))
+            ()
 
 
 data TxExtraContent =
@@ -787,7 +799,7 @@ makeShelleyTransaction TxExtraContent {
            Shelley.maybeToStrictMaybe txMetadata))
 
 toShelleyCertificate :: Certificate -> Shelley.DCert ShelleyCrypto
-toShelleyCertificate = undefined
+toShelleyCertificate = error "TODO: toShelleyCertificate"
 
 toShelleyWdrl :: [(StakeAddress, Lovelace)] -> Shelley.Wdrl ShelleyCrypto
 toShelleyWdrl wdrls =
@@ -856,6 +868,10 @@ data Witness era where
        :: Byron.TxInWitness
        -> Witness Byron
 
+     ShelleyBootstrapWitness
+       :: Shelley.BootstrapWitness ShelleyCrypto
+       -> Witness Shelley
+
      ShelleyKeyWitness
        :: Shelley.WitVKey ShelleyCrypto Shelley.Witness
        -> Witness Shelley
@@ -885,8 +901,10 @@ instance SerialiseAsCBOR (Witness Shelley) where
         encodeShelleyWitness :: Witness Shelley -> CBOR.Encoding
         encodeShelleyWitness (ShelleyKeyWitness    wit) =
             CBOR.encodeListLen 2 <> CBOR.encodeWord 0 <> toCBOR wit
-        encodeShelleyWitness (ShelleyScriptWitness wit) =
+        encodeShelleyWitness (ShelleyBootstrapWitness wit) =
             CBOR.encodeListLen 2 <> CBOR.encodeWord 1 <> toCBOR wit
+        encodeShelleyWitness (ShelleyScriptWitness wit) =
+            CBOR.encodeListLen 2 <> CBOR.encodeWord 2 <> toCBOR wit
 
     deserialiseFromCBOR AsShelleyWitness bs =
         CBOR.decodeAnnotator "Shelley Witness"
@@ -898,7 +916,8 @@ instance SerialiseAsCBOR (Witness Shelley) where
           t <- CBOR.decodeWord
           case t of
             0 -> fmap (fmap ShelleyKeyWitness) fromCBOR
-            1 -> fmap (pure . ShelleyScriptWitness) fromCBOR
+            1 -> fmap (fmap ShelleyBootstrapWitness) fromCBOR
+            2 -> fmap (pure . ShelleyScriptWitness) fromCBOR
             _ -> CBOR.cborError $ CBOR.DecoderErrorUnknownTag
                                     "Shelley Witness" (fromIntegral t)
 
@@ -926,7 +945,7 @@ makeSignedTransaction witnesses (ByronTxBody txbody) =
     ByronTx
   . Byron.annotateTxAux
   $ Byron.mkTxAux
-      txbody
+      (unAnnotated txbody)
       (Vector.fromList (map selectByronWitness witnesses))
   where
     selectByronWitness :: Witness Byron -> Byron.TxInWitness
@@ -937,79 +956,181 @@ makeSignedTransaction witnesses (ShelleyTxBody txbody) =
       Shelley.Tx
         txbody
         Shelley.WitnessSet {
-          Shelley.bootWits = Set.empty, -- byron key witnesses
-          Shelley.addrWits = Set.fromList keyWits,
-          Shelley.msigWits = Map.fromList [ (Shelley.hashScript sw, sw)
-                                          | sw <- scriptWits ]
+          Shelley.bootWits = Set.fromList
+                               [ w | ShelleyBootstrapWitness w <- witnesses ],
+          Shelley.addrWits = Set.fromList
+                               [ w | ShelleyKeyWitness w <- witnesses ],
+          Shelley.msigWits = Map.fromList
+                               [ (Shelley.hashScript sw, sw)
+                               | ShelleyScriptWitness sw <- witnesses ]
         }
         Shelley.SNothing -- (Shelley.maybeToStrictMaybe txmetadata)
-  where
-    (keyWits, scriptWits) = partitionEithers (map selectShelleyWitness witnesses)
 
-    selectShelleyWitness :: Witness Shelley
-                         -> Either (Shelley.WitVKey ShelleyCrypto Shelley.Witness)
-                                   (Shelley.MultiSig ShelleyCrypto)
-    selectShelleyWitness (ShelleyKeyWitness    w) = Left w
-    selectShelleyWitness (ShelleyScriptWitness w) = Right w
+data WitnessSigningKey era where
+
+     WitnessByronKey
+       :: SigningKey ByronKey
+       -> NetworkId
+       -> WitnessSigningKey era
+
+     WitnessPaymentKey
+       :: SigningKey PaymentKey
+       -> WitnessSigningKey Shelley
+
+     WitnessStakeKey
+       :: SigningKey StakeKey
+       -> WitnessSigningKey Shelley
+
+     WitnessStakePoolKey
+       :: SigningKey StakePoolKey
+       -> WitnessSigningKey Shelley
+
+     WitnessGenesisDelegateKey
+       :: SigningKey GenesisDelegateKey
+       -> WitnessSigningKey Shelley
+
+     WitnessGenesisUTxOKey
+       :: SigningKey GenesisUTxOKey
+       -> WitnessSigningKey Shelley
 
 
-byronKeyWitness :: NetworkId -> SigningKey ByronKey -> TxBody Byron -> Witness Byron
-byronKeyWitness nw (ByronSigningKey sk) (ByronTxBody txbody) =
+makeKeyWitness :: TxBody era -> WitnessSigningKey era -> Witness era
+makeKeyWitness (ByronTxBody txbody) (WitnessByronKey (ByronSigningKey sk) nw) =
     ByronKeyWitness $
       Byron.VKWitness
         (Byron.toVerification sk)
-        (Byron.sign pm Byron.SignTx sk (Byron.TxSigData txHash))
+        (Byron.sign pm Byron.SignTx sk (Byron.TxSigData txhash))
   where
-    txHash :: Byron.Hash Byron.Tx
-    txHash = Byron.serializeCborHash txbody
+    txhash :: Byron.Hash Byron.Tx
+    txhash = Byron.hashDecoded txbody
 
     pm :: Byron.ProtocolMagicId
     pm = toByronProtocolMagicId nw
 
-{-
-byronKeyWitness nw (ByronSigningKey sk) (ShelleyTxBody txbody) =
+makeKeyWitness (ShelleyTxBody txbody) (WitnessByronKey (ByronSigningKey sk) nw) =
     ShelleyBootstrapWitness $
-      Shelley.BootstrapWitness
-        { Shelley.bwKey       = svk
-        , Shelley.bwSig       = Crypto.signedDSIGN () txHash sk
-        , Shelley.bwChainCode = chainCode
-        , Shelley.bwPadding   = Shelley.KeyPadding "prefix" "suffix"
-        }
+      -- Byron era witnesses were weird. This reveals all that weirdness.
+      Shelley.BootstrapWitness {
+        Shelley.bwKey       = vk,
+        Shelley.bwSig       = signature,
+        Shelley.bwChainCode = chainCode,
+        Shelley.bwPadding   = padding
+      }
   where
-    -- TODO: have to convert the signing key from Byron to Shelley
-    -- and the verification key, with chain code
-    (svk, chainCode) = Shelley.unpackByronKey (Byron.toVerification sk)
+    -- Starting with the easy bits: we /can/ convert the Byron verification key
+    -- to a the pair of a Shelley verification key plus the chain code.
+    --
+    (vk, chainCode) = Shelley.unpackByronVKey (Byron.toVerification sk)
 
-    txHash :: Crypto.Hash Byron.Blake2b_256 (Shelley.TxBody ShelleyCrypto)
-    txHash = Crypto.hashRaw CBOR.recoverBytes txbody
+    -- Now the hairy bits.
+    --
+    -- We start with a Byron era /extended/ signing key. This /cannot/ be
+    -- converted to a plain (non-extended) signing key. So we have to produce a
+    -- signature using this extended signing key directly.
+    --
+    signature :: Shelley.SignedDSIGN ShelleyCrypto
+                  (Shelley.Hash ShelleyCrypto (Shelley.TxBody ShelleyCrypto))
+    signature = fromByronSignature $
+                  Byron.sign
+                    (toByronProtocolMagicId nw)
+                    Byron.SignTx
+                    sk
+                    (Byron.TxSigData (toByronHash txhash))
 
-    --TODO: the padding can be a constant, for constant attributes
-    --padding = Shelley.getPadding
--}
+    txhash :: Shelley.Hash ShelleyCrypto (Shelley.TxBody ShelleyCrypto)
+    txhash = Crypto.hash txbody
 
-shelleyKeyWitness :: SigningKey keyrole -> TxBody era -> Witness Shelley
-shelleyKeyWitness = undefined
--- this may need some class constraint on the keyrole, we can sign with:
---
---  ByronKey     -- for utxo inputs at byron addresses
---  PaymentKey   -- for utxo inputs, including multi-sig
---  StakeKey     -- for stake addr withdrawals and retiring and pool owners
---  StakePoolKey -- for stake pool ops
---  GenesisDelegateKey -- for update proposals, MIR etc
+    -- The Byron crypto signing code wants a Byron hash of a Byron tx body
+    -- but we have a Shelley hash of a Shelley tx body. Of course the
+    -- representations are the same, but we have to do some type conversions.
+    --
+    toByronHash :: Shelley.Hash ShelleyCrypto (Shelley.TxBody ShelleyCrypto)
+                -> Byron.Hash Byron.Tx
+    toByronHash = Byron.unsafeAbstractHashFromBytes
+                . Crypto.getHash
+                . Crypto.castHash
+
+    -- The crypto lib types are different so we also have to convert the type
+    -- of the resulting signature.
+    --
+    fromByronSignature :: Byron.Signature a
+                       -> Shelley.SignedDSIGN ShelleyCrypto b
+    fromByronSignature =
+        Crypto.SignedDSIGN
+      . fromMaybe impossible
+      . Crypto.rawDeserialiseSigDSIGN
+      . Byron.Crypto.Wallet.unXSignature
+      . (\(Byron.Signature sig) -> sig)
+
+    impossible =
+      error "fromByronSignature: byron and shelley signature sizes do not match"
+
+    padding = Shelley.byronVerKeyAddressPadding $
+                Byron.mkAttributes Byron.AddrAttributes {
+                  Byron.aaVKDerivationPath = Nothing,
+                  Byron.aaNetworkMagic     = toByronNetworkMagic nw
+                }
+
+makeKeyWitness (ShelleyTxBody txbody)
+               (WitnessPaymentKey (PaymentSigningKey sk)) =
+    ShelleyKeyWitness $
+      makeShelleyKeyWitness txbody sk
+
+makeKeyWitness (ShelleyTxBody txbody)
+               (WitnessStakeKey (StakeSigningKey sk)) =
+    ShelleyKeyWitness $
+      makeShelleyKeyWitness txbody sk
+
+makeKeyWitness (ShelleyTxBody txbody)
+               (WitnessStakePoolKey (StakePoolSigningKey sk)) =
+    ShelleyKeyWitness $
+      makeShelleyKeyWitness txbody sk
+
+makeKeyWitness (ShelleyTxBody txbody)
+               (WitnessGenesisUTxOKey (GenesisUTxOSigningKey sk)) =
+    ShelleyKeyWitness $
+      makeShelleyKeyWitness txbody sk
+
+makeKeyWitness (ShelleyTxBody txbody)
+               (WitnessGenesisDelegateKey (GenesisDelegateSigningKey sk)) =
+    ShelleyKeyWitness $
+      makeShelleyKeyWitness txbody sk
+
+
+makeShelleyKeyWitness :: Shelley.TxBody ShelleyCrypto
+                      -> Shelley.SignKeyDSIGN ShelleyCrypto
+                      -> Shelley.WitVKey ShelleyCrypto Shelley.Witness
+
+makeShelleyKeyWitness txbody sk =
+    Shelley.WitVKey vk signature
+  where
+    vk = Shelley.VKey (Crypto.deriveVerKeyDSIGN sk)
+
+    signature :: Shelley.SignedDSIGN ShelleyCrypto
+                  (Shelley.Hash ShelleyCrypto (Shelley.TxBody ShelleyCrypto))
+    signature = Crypto.signedDSIGN () txhash sk
+
+    txhash :: Shelley.Hash ShelleyCrypto (Shelley.TxBody ShelleyCrypto)
+    txhash = Crypto.hash txbody
 
 
 -- order of signing keys must match txins
-signByronTransaction :: NetworkId
-                     -> TxBody Byron
-                     -> [SigningKey ByronKey]
+signByronTransaction :: TxBody Byron
+                     -> [WitnessSigningKey Byron]
                      -> Tx Byron
-signByronTransaction = undefined
+signByronTransaction txbody sks =
+    makeSignedTransaction witnesses txbody
+  where
+    witnesses = map (makeKeyWitness txbody) sks
 
 -- signing keys is a set
 signShelleyTransaction :: TxBody Shelley
-                       -> [SigningKey Shelley]
+                       -> [WitnessSigningKey Shelley]
                        -> Tx Shelley
-signShelleyTransaction = undefined
+signShelleyTransaction txbody sks =
+    makeSignedTransaction witnesses txbody
+  where
+    witnesses = map (makeKeyWitness txbody) sks
 
 
 -- ----------------------------------------------------------------------------
@@ -1021,8 +1142,8 @@ data Script
 newtype instance Hash Script = ScriptHash (Shelley.ScriptHash ShelleyCrypto)
 
 
-shelleyScriptWitness :: Script -> TxId -> Witness Shelley
-shelleyScriptWitness = undefined
+makeScriptWitness :: TxBody Shelley -> Script -> Witness Shelley
+makeScriptWitness = undefined
 
 
 -- ----------------------------------------------------------------------------
