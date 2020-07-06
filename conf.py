fc020376826b5b3ac4c7679846bf9c7859b3605d
Rob Cohen <rob@robcohen.dev>
2020-07-06 15:08:56 -0600

    fix

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-master_doc = 'index'
+master_doc = 'doc/index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
