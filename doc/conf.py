2d2d089eaeb1a692a3073850a3dc0d674642836e
Rob Cohen <rob@robcohen.dev>
2020-07-10 10:30:03 -0600

    fix

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
-master_doc = '../index'
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -47,8 +47,8 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-html_static_path = ['_static']
+templates_path = ['.sphinx/_templates']
+html_static_path = ['.sphinx/_static']
 
 source_suffix = {
     '.rst': 'restructuredtext',
