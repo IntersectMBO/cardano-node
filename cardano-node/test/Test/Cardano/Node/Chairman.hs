3d4ab281963672d6acb060976ac9e66beff07bc5
John Ky <john.ky@iohk.io>
2020-08-04 08:49:22 +1000

    Stuff

--- cardano-node/test/Test/Cardano/Node/Chairman.hs
+++ cardano-node/test/Test/Cardano/Node/Chairman.hs
@@ -23,13 +23,13 @@ prop_spawnOneNode = H.propertyOnce $ do
 
   (_mIn, _mOut, _mErr, hProcess) <- H.createProcess =<< H.procNode
     [ "run"
-    , "--database-path", base <> "/db/node-2/"
-    , "--socket-path", base <> "/socket/node-2-socket"
+    , "--database-path", base <> "/chairman/db/node-2"
+    , "--socket-path", base <> "/chairman/socket/node-2-socket"
     , "--port", "3002"
-    , "--topology", base <> "/configuration/defaults/simpleview/topology-node-2.json"
-    , "--config", base <> "/configuration/defaults/simpleview/config-2.yaml"
-    , "--signing-key", base <> "/configuration/defaults/simpleview/genesis/delegate-keys.002.key"
-    , "--delegation-certificate", base <> "/configuration/defaults/simpleview/genesis/delegation-cert.002.json"
+    , "--topology", base <> "/chairman/configuration/defaults/simpleview/topology-node-2.json"
+    , "--config", base <> "/chairman/configuration/defaults/simpleview/config-2.yaml"
+    , "--signing-key", base <> "/chairman/configuration/defaults/simpleview/genesis/delegate-keys.002.key"
+    , "--delegation-certificate", base <> "/chairman/configuration/defaults/simpleview/genesis/delegation-cert.002.json"
     ]
 
   H.threadDelay 10000000
--- cardano-node/test/Test/Common/Base.hs
+++ cardano-node/test/Test/Common/Base.hs
@@ -2,24 +2,36 @@ module Test.Common.Base
   ( propertyOnce
   , failWithCustom
   , threadDelay
+  , workspace
+  , moduleWorkspace
   ) where
 
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Bool
 import           Data.Either (Either (..))
 import           Data.Function (($), (.))
+import           Data.Functor
 import           Data.Int
-import           Data.Maybe (Maybe (..))
+import           Data.Maybe (Maybe (..), fromMaybe, listToMaybe)
 import           Data.Monoid (Monoid (..))
+import           Data.Semigroup (Semigroup (..))
 import           Data.String (String)
-import           GHC.Stack (CallStack)
+import           Data.Tuple
+import           GHC.Stack (CallStack, HasCallStack, callStack, getCallStack)
 import           Hedgehog (MonadTest)
 import           Hedgehog.Internal.Property (Diff, liftTest, mkTest)
 import           Hedgehog.Internal.Source (getCaller)
-import           System.IO (IO)
+import           System.IO (FilePath, IO)
 
 import qualified Control.Concurrent as IO
+import qualified GHC.Stack as GHC
 import qualified Hedgehog as H
 import qualified Hedgehog.Internal.Property as H
+import qualified System.Directory as IO
+import qualified System.IO.Temp as IO
+
+cardanoCliPath :: FilePath
+cardanoCliPath = "cardano-cli"
 
 propertyOnce :: H.PropertyT IO () -> H.Property
 propertyOnce = H.withTests 1 . H.property
@@ -30,3 +42,32 @@ threadDelay = liftIO . IO.threadDelay
 -- | Takes a 'CallStack' so the error can be rendered at the appropriate call site.
 failWithCustom :: MonadTest m => CallStack -> Maybe Diff -> String -> m a
 failWithCustom cs mdiff msg = liftTest $ mkTest (Left $ H.Failure (getCaller cs) msg mdiff, mempty)
+
+-- | Create a workspace directory which will exist for at least the duration of
+-- the supplied block.
+--
+-- The directory will have the supplied prefix but contain a generated random
+-- suffix to prevent interference between tests
+--
+-- The directory will be deleted if the block succeeds, but left behind if
+-- the block fails.
+workspace :: HasCallStack => FilePath -> (FilePath -> H.PropertyT IO ()) -> H.PropertyT IO ()
+workspace prefixPath f = GHC.withFrozenCallStack $ do
+  liftIO $ IO.createDirectoryIfMissing True prefixPath
+  ws <- liftIO $ IO.createTempDirectory prefixPath "test"
+  H.annotate $ "Workspace: " <> cardanoCliPath <> "/" <> ws
+  f ws
+  liftIO $ IO.removeDirectoryRecursive ws
+
+-- | Create a workspace directory which will exist for at least the duration of
+-- the supplied block.
+--
+-- The directory will have the prefix as "$prefixPath/$moduleName" but contain a generated random
+-- suffix to prevent interference between tests
+--
+-- The directory will be deleted if the block succeeds, but left behind if
+-- the block fails.
+moduleWorkspace :: HasCallStack => FilePath -> (FilePath -> H.PropertyT IO ()) -> H.PropertyT IO ()
+moduleWorkspace prefixPath f = GHC.withFrozenCallStack $ do
+  let srcModule = fromMaybe "UnknownModule" (fmap (GHC.srcLocModule . snd) (listToMaybe (getCallStack callStack)))
+  workspace (prefixPath <> "/" <> srcModule) f
