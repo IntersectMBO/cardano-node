2057e0a05d289f1d454fbb3b2e3e6353884304e3
John Ky <john.ky@iohk.io>
2020-08-04 15:39:02 +1000

    Spawn cardano-node in a test for 10 seconds.

--- cardano-node/test/cardano-node-test.hs
+++ cardano-node/test/cardano-node-test.hs
@@ -3,10 +3,12 @@ import           Cardano.Prelude
 
 import           Hedgehog.Main (defaultMain)
 
+import qualified Test.Cardano.Node.Chairman
 import qualified Test.Cardano.Node.Json
 
 main :: IO ()
 main =
   defaultMain
-    [ Test.Cardano.Node.Json.tests
+    [ Test.Cardano.Node.Chairman.tests
+    , Test.Cardano.Node.Json.tests
     ]
