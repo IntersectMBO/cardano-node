ff2cbd7a8c92d713de9e2f5afa0cfe49e5c1d971
Erik de Castro Lopo <erikd@mega-nerd.com>
2020-08-12 12:45:20 +1000

    Reorganize cardano-cli tests
    Split a single test into a pair of tests (golden and cli) that are easier
    to run separately. Also rearrange the module heirarchy so that the module
    names indicate which test the module belongs to.
    
    Apart from the reorg, the only other change is changing some tests from
    sequential to parallel execution.

---
+++ cardano-cli/test/Test/Golden/Byron/TextEnvelope/Tx.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Byron.TextEnvelope.Tx
+  ( golden_byronTx
+  ) where
+
+import           Cardano.Prelude
+
+import           Hedgehog (Property)
+
+
+golden_byronTx :: Property
+golden_byronTx = panic "TODO"
