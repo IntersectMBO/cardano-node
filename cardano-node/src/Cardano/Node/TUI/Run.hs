bf1fe79372b7a244ded46378601297a5d1105ed2
John Ky <john.ky@iohk.io>
2020-08-17 13:41:50 +1000

    Enable hlint rule replace case with maybe

--- cardano-node/src/Cardano/Node/TUI/Run.hs
+++ cardano-node/src/Cardano/Node/TUI/Run.hs
@@ -50,11 +50,7 @@ liveViewPostSetup lvbe ncli nc = do
     -- an ID. We don't even have a port number that we know if we're given our
     -- listening socket via systemd socket activation.
     nodeId :: Text
-    nodeId =
-        Text.pack $ "Port: " <>
-        case naPort <$> nodeAddr ncli of
-          Nothing -> "-"
-          Just port -> show port
+    nodeId = Text.pack $ "Port: " <> maybe "-" show (naPort <$> nodeAddr ncli)
 
 setNodeThread :: NFData a => LiveViewBackend blk a -> Async.Async () -> IO ()
 setNodeThread lvbe nodeThr =
