a047f2520e38e1d0066768b154f2dae3bb233adc
Rob Cohen <rob.cohen@iohk.io>
2020-08-20 12:09:36 -0600

    Logo change

--- doc/conf.py
+++ doc/conf.py
@@ -81,7 +81,7 @@ html_theme_options = {
     'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'style_nav_header_background': '#fcfcfc',
+    'style_nav_header_background': '#0635a7',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
