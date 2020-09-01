799dd726b90652e2f8fa611c98602213011b7c65
John Ky <john.ky@iohk.io>
2020-09-01 17:39:45 +1000

    Pure Haskell Shelley-only chairman test.  The test makes the following assertions:
    * It opens the correct port
    * It creates a socket file
    * The text "until genesis start time at" appears in the log output
    * The text "Chain extended, new tip" appears in the log output
    
    Additional changes:
    
    * New `createFileLink` function for creating symbolic links in tests
    * New `noteShow_`, `noteShowM_`, `noteShowIO_` functions, which are `()` returning equivalents
    * New `readFile`, `writeFile`, `lbsReadFile`, `lbsWriteFile` functions for reading and writing files
    * New `rewriteJson` functions for rewriting JSON configuration files
    * New `forceM` function for forcing the return values of computations.  It is used for reading of files (which is lazy by default).
    * New `release` function for releasing resources early.  Used to shutdown the node process early.
    * New `fileContains` function for testing if a file contains a string.  For making assertions on the log file output.
    * New `strip` function, which is `String` version of standard `Text` function.
    * Existing file IO related test functions now annotate the filename that is involved.

--- cardano-node-chairman/src/Chairman/Base.hs
+++ cardano-node-chairman/src/Chairman/Base.hs
@@ -9,23 +9,35 @@ module Chairman.Base
   , moduleWorkspace
   , createDirectoryIfMissing
   , copyFile
+  , createFileLink
   , noteShow
+  , noteShow_
   , noteShowM
+  , noteShowM_
   , noteShowIO
+  , noteShowIO_
   , noteTempFile
   , assertByDeadlineIO
   , showUTCTimeSeconds
   , listDirectory
+  , readFile
   , writeFile
+  , lbsReadFile
+  , lbsWriteFile
+  , rewriteJson
   , assertM
   , assertIO
   , Integration
+  , forceM
+  , release
   ) where
 
+import           Control.DeepSeq
 import           Control.Monad
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Monad.Morph (hoist)
-import           Control.Monad.Trans.Resource (ResourceT, runResourceT)
+import           Control.Monad.Trans.Resource (ReleaseKey, ResourceT, runResourceT)
+import           Data.Aeson
 import           Data.Bool
 import           Data.Either (Either (..))
 import           Data.Eq
@@ -47,6 +59,8 @@ import           System.IO (FilePath, IO)
 import           Text.Show
 
 import qualified Control.Concurrent as IO
+import qualified Control.Monad.Trans.Resource as IO
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Time.Clock as DTC
 import qualified Data.Time.Clock.POSIX as DTC
 import qualified GHC.Stack as GHC
@@ -100,19 +114,14 @@ moduleWorkspace prefixPath f = GHC.withFrozenCallStack $ do
   let srcModule = maybe "UnknownModule"  (GHC.srcLocModule . snd) (listToMaybe (getCallStack callStack))
   workspace (prefixPath <> "/" <> srcModule) f
 
-createDirectoryIfMissing :: HasCallStack => FilePath -> Integration ()
-createDirectoryIfMissing filePath = H.evalM . liftIO $ IO.createDirectoryIfMissing True filePath
-
-copyFile :: HasCallStack => FilePath -> FilePath -> Integration ()
-copyFile src dst = GHC.withFrozenCallStack $ do
-  H.annotate $ "Copy from " <> show src <> " to " <> show dst
-  H.evalM . liftIO $ IO.copyFile src dst
-
 noteShow :: (HasCallStack, Show a) => a -> Integration a
 noteShow a = GHC.withFrozenCallStack $ do
   !b <- H.eval a
   H.annotateShow b
   return b
+
+noteShow_ :: (HasCallStack, Show a) => a -> Integration ()
+noteShow_ = void . noteShow
 
 noteShowM :: (HasCallStack, Show a) => Integration a -> Integration a
 noteShowM a = GHC.withFrozenCallStack $ do
@@ -120,11 +129,17 @@ noteShowM a = GHC.withFrozenCallStack $ do
   H.annotateShow b
   return b
 
+noteShowM_ :: (HasCallStack, Show a) => Integration a -> Integration ()
+noteShowM_ = void . noteShowM
+
 noteShowIO :: (HasCallStack, Show a) => IO a -> Integration a
 noteShowIO a = GHC.withFrozenCallStack $ do
   !b <- H.evalM . liftIO $ a
   H.annotateShow b
   return b
+
+noteShowIO_ :: (HasCallStack, Show a) => IO a -> Integration ()
+noteShowIO_ = void . noteShowIO
 
 -- | Return the test file path after annotating it relative to the project root directory
 noteTempFile :: (Monad m, HasCallStack) => FilePath -> FilePath -> H.PropertyT m FilePath
@@ -153,14 +168,64 @@ assertByDeadlineIO deadline f = GHC.withFrozenCallStack $ do
 showUTCTimeSeconds :: UTCTime -> String
 showUTCTimeSeconds time = show @Int64 (floor (DTC.utcTimeToPOSIXSeconds time))
 
+createDirectoryIfMissing :: HasCallStack => FilePath -> Integration ()
+createDirectoryIfMissing filePath = GHC.withFrozenCallStack $ do
+  H.annotate $ "Creating directory if missing: " <> filePath
+  H.evalIO $ IO.createDirectoryIfMissing True filePath
+
+copyFile :: HasCallStack => FilePath -> FilePath -> Integration ()
+copyFile src dst = GHC.withFrozenCallStack $ do
+  H.annotate $ "Copying from " <> show src <> " to " <> show dst
+  H.evalM . liftIO $ IO.copyFile src dst
+
+createFileLink :: HasCallStack => FilePath -> FilePath -> Integration ()
+createFileLink src dst = GHC.withFrozenCallStack $ do
+  H.annotate $ "Creating link from " <> show dst <> " to " <> show src
+  H.evalM . liftIO $ IO.copyFile src dst
+
 listDirectory :: (MonadIO m, HasCallStack) => FilePath -> H.PropertyT m [FilePath]
-listDirectory = GHC.withFrozenCallStack . H.evalIO . IO.listDirectory
+listDirectory p = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Listing directory: " <> p
+  H.evalIO $ IO.listDirectory p
 
 writeFile :: (MonadIO m, HasCallStack) => FilePath -> String -> H.PropertyT m ()
-writeFile filePath contents = GHC.withFrozenCallStack . H.evalIO $ IO.writeFile filePath contents
+writeFile filePath contents = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Writing file: " <> filePath
+  H.evalIO $ IO.writeFile filePath contents
+
+readFile :: (MonadIO m, HasCallStack) => FilePath -> H.PropertyT m String
+readFile filePath = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Reading file: " <> filePath
+  H.evalIO $ IO.readFile filePath
+
+lbsWriteFile :: (MonadIO m, HasCallStack) => FilePath -> LBS.ByteString -> H.PropertyT m ()
+lbsWriteFile filePath contents = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Writing file: " <> filePath
+  H.evalIO $ LBS.writeFile filePath contents
+
+lbsReadFile :: (MonadIO m, HasCallStack) => FilePath -> H.PropertyT m LBS.ByteString
+lbsReadFile filePath = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Reading file: " <> filePath
+  H.evalIO $ LBS.readFile filePath
+
+rewriteJson :: (MonadIO m, HasCallStack) => FilePath -> (Value -> Either String Value) -> H.PropertyT m ()
+rewriteJson filePath f = GHC.withFrozenCallStack $ do
+  void . H.annotate $ "Rewriting JSON file: " <> filePath
+  lbs <- forceM $ lbsReadFile filePath
+  case eitherDecode lbs of
+    Right iv -> case f iv of
+      Right ov -> lbsWriteFile filePath (encode ov)
+      Left msg -> failWithCustom GHC.callStack Nothing msg
+    Left msg -> failWithCustom GHC.callStack Nothing msg
 
 assertM :: (MonadIO m, HasCallStack) => H.PropertyT m Bool -> H.PropertyT m ()
 assertM = (>>= H.assert)
 
 assertIO :: (MonadIO m, HasCallStack) => IO Bool -> H.PropertyT m ()
 assertIO f = H.evalIO f >>= H.assert
+
+forceM :: (Monad m, NFData a) => m a -> m a
+forceM = (force <$!>)
+
+release :: MonadIO m => ReleaseKey -> H.PropertyT m ()
+release = H.evalIO . IO.release
