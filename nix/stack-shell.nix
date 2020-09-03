47a2dc48d85b7408e124dd83d8bace6a29a054c7
Alexander Diemand <codieplusplus@apax.net>
2020-09-03 23:23:47 +0200

    CAD-1765 stack on 1.19.1
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- nix/stack-shell.nix
+++ nix/stack-shell.nix
@@ -3,6 +3,6 @@ with import ./. {};
 
 haskell.lib.buildStackProject {
   name = "stack-env";
-  buildInputs = with pkgs; [ zlib openssl gmp libffi git systemd haskellPackages.happy ];
+  buildInputs = with pkgs; [ zlib openssl gmp libffi libsodium pkg-config git systemd haskellPackages.happy ];
   ghc = (import ../shell.nix {inherit pkgs;}).ghc;
 }
