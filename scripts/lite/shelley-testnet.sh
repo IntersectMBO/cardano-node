80f5609419888b967ccd2f0626564d5cbcaf2a81
Jordan Millar <jordan.millar@iohk.io>
2020-09-15 08:59:40 +0100

    Add simple mainnet node script

--- scripts/lite/shelley-testnet.sh
+++ scripts/lite/shelley-testnet.sh
@@ -95,11 +95,7 @@ for i in 1 2 3; do
       --cold-signing-key-file                      "${data_dir}/node-$i/hotkey.skey" \
       --operational-certificate-issue-counter-file "${data_dir}/node-$i/counterFile.counter" \
       --out-file                                   "${data_dir}/node-$i/opcert"
-done
 
-rmdir "${data_dir}/genesis/delegate-keys"
-
-for i in 1 2 3; do
   # Launch a node
   cabal run exe:cardano-node -- run \
     --database-path "${db_dir}" \
@@ -113,7 +109,7 @@ for i in 1 2 3; do
     | sed "s|^|${esc}[$((31+$i))m[node-$i]${esc}[0m |g" &
 done
 
-
+rmdir "${data_dir}/genesis/delegate-keys"
 
 function cleanup()
 {
