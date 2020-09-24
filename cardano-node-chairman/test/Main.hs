5270aa52eced83c9cce31a1a9f7141301953cf45
John Ky <john.ky@iohk.io>
2020-09-24 18:08:18 +1000

    Not working

--- cardano-node-chairman/test/Main.hs
+++ cardano-node-chairman/test/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 
 module Main
   ( main
@@ -6,23 +7,29 @@ module Main
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource
+import           Data.Bool
 import           Data.Function
 import           Hedgehog
 import           System.IO (IO)
 
 import qualified Control.Concurrent as IO
+import qualified Control.Concurrent.STM as STM
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Test.Base as H
 import qualified System.IO as IO
 
 main :: IO ()
 main = do
+  tvDone <- STM.newTVarIO @Bool False
+
   void . check $ H.propertyOnce . H.workspace "chairman" $ \_ -> do
     void . register . liftIO $ IO.appendFile "logs.txt" "Cleanup\n"
 
     void . liftResourceT . resourceForkIO $ do
       liftIO $ IO.appendFile "logs.txt" "Forked\n"
-      void . forever . liftIO $ IO.threadDelay 100000000
+      void . forever . liftIO . STM.atomically $ do
+        done <- STM.readTVar tvDone
+        unless done STM.retry
       liftIO $ IO.appendFile "logs.txt" "Thread done\n"
 
       liftIO $ IO.appendFile "logs.txt" "Done\n"
