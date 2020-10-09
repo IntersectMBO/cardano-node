36e7c9d29fb4a8f58dceed8274cb13fa8c72bd94
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-10-08 20:45:39 -0400

    Replace panic with fail in JSON parsers in Cardano.Node.Orphans

--- cardano-node/src/Cardano/Node/Orphans.hs
+++ cardano-node/src/Cardano/Node/Orphans.hs
@@ -6,7 +6,7 @@
 module Cardano.Node.Orphans () where
 
 import           Cardano.Prelude
-import qualified Prelude
+import           Prelude (fail)
 
 import           Data.Aeson (FromJSON (..), Value (..))
 import qualified Data.Text as Text
@@ -19,15 +19,15 @@ instance FromJSON TracingVerbosity where
     "MinimalVerbosity" -> pure MinimalVerbosity
     "MaximalVerbosity" -> pure MaximalVerbosity
     "NormalVerbosity" -> pure NormalVerbosity
-    err -> panic $ "Parsing of TracingVerbosity failed, "
-                 <> err <> " is not a valid TracingVerbosity"
-  parseJSON invalid  = panic $ "Parsing of TracingVerbosity failed due to type mismatch. "
-                             <> "Encountered: " <> Text.pack (Prelude.show invalid)
+    err -> fail $ "Parsing of TracingVerbosity failed, "
+                <> Text.unpack err <> " is not a valid TracingVerbosity"
+  parseJSON invalid  = fail $ "Parsing of TracingVerbosity failed due to type mismatch. "
+                           <> "Encountered: " <> show invalid
 
 deriving instance Show TracingVerbosity
 
 instance FromJSON Update.ApplicationName where
   parseJSON (String x) = pure $ Update.ApplicationName x
   parseJSON invalid  =
-    panic $ "Parsing of application name failed due to type mismatch. "
-    <> "Encountered: " <> Text.pack (Prelude.show invalid)
+    fail $ "Parsing of application name failed due to type mismatch. "
+    <> "Encountered: " <> show invalid
