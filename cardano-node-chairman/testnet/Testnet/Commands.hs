0db51f2ffe96fb2eecdcf8f6ec322499da00d6d0
John Ky <john.ky@iohk.io>
2020-10-29 17:10:49 +1100

    Add version command to cardano-testnet

--- cardano-node-chairman/testnet/Testnet/Commands.hs
+++ cardano-node-chairman/testnet/Testnet/Commands.hs
@@ -7,15 +7,21 @@ import           System.IO (IO)
 import           Testnet.Commands.Byron
 import           Testnet.Commands.ByronShelley
 import           Testnet.Commands.Shelley
+import           Testnet.Commands.Version
 
 {- HLINT ignore "Monoid law, left identity" -}
 
 commands :: Parser (IO ())
-commands = commandsGeneral
+commands = commandsTestnet <|> commandsGeneral
 
-commandsGeneral :: Parser (IO ())
-commandsGeneral = subparser $ mempty
-  <>  commandGroup "Commands:"
+commandsTestnet :: Parser (IO ())
+commandsTestnet = subparser $ mempty
+  <>  commandGroup "Testnets:"
   <>  cmdByron
   <>  cmdByronShelley
   <>  cmdShelley
+
+commandsGeneral :: Parser (IO ())
+commandsGeneral = subparser $ mempty
+  <>  commandGroup "General:"
+  <>  cmdVersion
