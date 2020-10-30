dabf9c9d5207032f2e052017e0520f640fc95d24
John Ky <john.ky@iohk.io>
2020-10-30 16:34:54 +1100

    Add version command to cardano-node-chairman

--- cardano-node-chairman/app/cardano-node-chairman.hs
+++ cardano-node-chairman/app/cardano-node-chairman.hs
@@ -1,41 +1,20 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
 
-import           Cardano.Chairman (chairmanTest)
-import           Cardano.Chairman.Options
-import           Cardano.Node.Configuration.POM (parseNodeConfigurationFP, pncProtocol)
-import           Cardano.Prelude hiding (option)
-import           Control.Tracer (stdoutTracer)
+import           Cardano.Chairman.Commands
+import           Control.Monad
+import           Data.Function
+import           Data.Semigroup
 import           Options.Applicative
+import           System.IO (IO)
 
 main :: IO ()
-main = do
-  ChairmanArgs
-    { caRunningTime
-    , caMinProgress
-    , caSocketPaths
-    , caConfigYaml
-    , caSlotLength
-    , caSecurityParam
-    , caNetworkMagic
-    } <- execParser opts
-
-  partialNc <- liftIO . parseNodeConfigurationFP $ Just caConfigYaml
-
-  ptcl <- case pncProtocol partialNc of
-            Left err -> panic $ "Chairman error: " <> err
-            Right protocol -> return protocol
-
-  let someNodeClientProtocol = mkNodeClientProtocol ptcl
-
-  chairmanTest
-    stdoutTracer
-    caSlotLength
-    caSecurityParam
-    caRunningTime
-    caMinProgress
-    caSocketPaths
-    someNodeClientProtocol
-    caNetworkMagic
+main = join
+  . customExecParser
+    ( prefs (showHelpOnEmpty <> showHelpOnError)
+    )
+  $ info (commands <**> helper)
+    (  fullDesc
+    <> progDesc "Chairman checks Cardano clusters for progress and consensus."
+    <> header "Chairman sits in a room full of Shelley nodes, and checks \
+              \if they are all behaving ..."
+    )
