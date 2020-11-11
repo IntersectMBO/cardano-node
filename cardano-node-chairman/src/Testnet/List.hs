40fb6b8bb08c4b6d2d3179ed4a9c9c09cc6f06e3
John Ky <john.ky@iohk.io>
2020-11-12 09:55:05 +1100

    Fix chairman config for Byron testnet:
    * Fix implementation of dropNth which failed to drop the nth element.
    * Bump up the expected number of blocks
    * Change PBftSignatureThreshold to 0.4

--- cardano-node-chairman/src/Testnet/List.hs
+++ cardano-node-chairman/src/Testnet/List.hs
@@ -8,5 +8,5 @@ import           GHC.Num
 -- | Drop the zero-based n-th element from the list.
 dropNth :: Int -> [a] -> [a]
 dropNth _ [] = []
-dropNth 0 as = as
+dropNth 0 (_:as) = as
 dropNth i (a:as) = a:dropNth (i - 1) as
