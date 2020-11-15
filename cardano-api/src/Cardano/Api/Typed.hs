c8c4b3eb75df90d0df45ba749bb53eb41258a5c6
Duncan Coutts <duncan@well-typed.com>
2020-11-15 01:13:22 +0000

    Split OperationalCertificate module out of Cardano.Api.Typed

--- cardano-api/src/Cardano/Api/Typed.hs
+++ cardano-api/src/Cardano/Api/Typed.hs
@@ -389,7 +389,6 @@ module Cardano.Api.Typed (
 import           Prelude
 
 import           Data.Void (Void)
-import           Data.Word
 
 import qualified Data.ByteString.Lazy as LBS
 
@@ -401,8 +400,6 @@ import           Control.Tracer (nullTracer)
 --
 -- Common types, consensus, network
 --
-import qualified Shelley.Spec.Ledger.Serialization as CBOR (CBORGroup (..))
-
 import           Cardano.Slotting.Slot (EpochNo (..), EpochSize (..), SlotNo (..))
 
 -- TODO: it'd be nice if the network imports needed were a bit more coherent
@@ -480,6 +477,7 @@ import           Cardano.Api.KeysByron
 import           Cardano.Api.KeysPraos
 import           Cardano.Api.KeysShelley
 import           Cardano.Api.NetworkId
+import           Cardano.Api.OperationalCertificate
 import           Cardano.Api.ProtocolParameters
 import           Cardano.Api.Script
 import           Cardano.Api.SerialiseBech32
@@ -511,122 +509,6 @@ import           Cardano.Api.Value
 -- distinguish the key /role/. These are type level distinctions, so each of
 -- these roles is a type level tag.
 --
-
-
--- ----------------------------------------------------------------------------
--- Operational certificates
---
-
-data OperationalCertificate =
-     OperationalCertificate
-       !(Shelley.OCert StandardCrypto)
-       !(VerificationKey StakePoolKey)
-  deriving (Eq, Show)
-  deriving anyclass SerialiseAsCBOR
-
-data OperationalCertificateIssueCounter =
-     OperationalCertificateIssueCounter
-       !Word64
-       !(VerificationKey StakePoolKey) -- For consistency checking
-  deriving (Eq, Show)
-  deriving anyclass SerialiseAsCBOR
-
-instance ToCBOR OperationalCertificate where
-    toCBOR (OperationalCertificate ocert vkey) =
-      toCBOR (CBOR.CBORGroup ocert, vkey)
-
-instance FromCBOR OperationalCertificate where
-    fromCBOR = do
-      (CBOR.CBORGroup ocert, vkey) <- fromCBOR
-      return (OperationalCertificate ocert vkey)
-
-instance ToCBOR OperationalCertificateIssueCounter where
-    toCBOR (OperationalCertificateIssueCounter counter vkey) =
-      toCBOR (counter, vkey)
-
-instance FromCBOR OperationalCertificateIssueCounter where
-    fromCBOR = do
-      (counter, vkey) <- fromCBOR
-      return (OperationalCertificateIssueCounter counter vkey)
-
-instance HasTypeProxy OperationalCertificate where
-    data AsType OperationalCertificate = AsOperationalCertificate
-    proxyToAsType _ = AsOperationalCertificate
-
-instance HasTypeProxy OperationalCertificateIssueCounter where
-    data AsType OperationalCertificateIssueCounter = AsOperationalCertificateIssueCounter
-    proxyToAsType _ = AsOperationalCertificateIssueCounter
-
-instance HasTextEnvelope OperationalCertificate where
-    textEnvelopeType _ = "NodeOperationalCertificate"
-
-instance HasTextEnvelope OperationalCertificateIssueCounter where
-    textEnvelopeType _ = "NodeOperationalCertificateIssueCounter"
-
-data OperationalCertIssueError =
-       -- | The stake pool verification key expected for the
-       -- 'OperationalCertificateIssueCounter' does not match the signing key
-       -- supplied for signing.
-       --
-       -- Order: pool vkey expected, pool skey supplied
-       --
-       OperationalCertKeyMismatch (VerificationKey StakePoolKey)
-                                  (VerificationKey StakePoolKey)
-  deriving Show
-
-instance Error OperationalCertIssueError where
-    displayError (OperationalCertKeyMismatch _counterKey _signingKey) =
-      "Key mismatch: the signing key does not match the one that goes with the counter"
-      --TODO: include key ids
-
-issueOperationalCertificate :: VerificationKey KesKey
-                            -> Either (SigningKey StakePoolKey)
-                                      (SigningKey GenesisDelegateExtendedKey)
-                               --TODO: this may be better with a type that
-                               -- captured the three (four?) choices, stake pool
-                               -- or genesis delegate, extended or normal.
-                            -> Shelley.KESPeriod
-                            -> OperationalCertificateIssueCounter
-                            -> Either OperationalCertIssueError
-                                      (OperationalCertificate,
-                                      OperationalCertificateIssueCounter)
-issueOperationalCertificate (KesVerificationKey kesVKey)
-                            skey
-                            kesPeriod
-                            (OperationalCertificateIssueCounter counter poolVKey)
-    | poolVKey /= poolVKey'
-    = Left (OperationalCertKeyMismatch poolVKey poolVKey')
-
-    | otherwise
-    = Right (OperationalCertificate ocert poolVKey,
-            OperationalCertificateIssueCounter (succ counter) poolVKey)
-  where
-    poolVKey' :: VerificationKey StakePoolKey
-    poolVKey' = either getVerificationKey (convert . getVerificationKey) skey
-      where
-        convert :: VerificationKey GenesisDelegateExtendedKey
-                -> VerificationKey StakePoolKey
-        convert = (castVerificationKey :: VerificationKey GenesisDelegateKey
-                                       -> VerificationKey StakePoolKey)
-                . (castVerificationKey :: VerificationKey GenesisDelegateExtendedKey
-                                       -> VerificationKey GenesisDelegateKey)
-
-    ocert     :: Shelley.OCert StandardCrypto
-    ocert     = Shelley.OCert kesVKey counter kesPeriod signature
-
-    signature :: Shelley.SignedDSIGN
-                   StandardCrypto
-                   (Shelley.OCertSignable StandardCrypto)
-    signature = makeShelleySignature
-                  (Shelley.OCertSignable kesVKey counter kesPeriod)
-                  skey'
-      where
-        skey' :: ShelleySigningKey
-        skey' = case skey of
-                  Left (StakePoolSigningKey poolSKey) ->
-                    ShelleyNormalSigningKey poolSKey
-                  Right (GenesisDelegateExtendedSigningKey delegSKey) ->
-                    ShelleyExtendedSigningKey delegSKey
 
 
 -- ----------------------------------------------------------------------------
