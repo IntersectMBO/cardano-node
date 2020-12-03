445b592aea8301c0cdb6e608c6508f597127a832
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2020-12-03 17:09:47 +0100

    Fix edgeNode test with retry and timeout. Fix #2075
     also use cardano-ping to test connectivity.

--- nix/nixos/tests/cardano-node-edge.nix
+++ nix/nixos/tests/cardano-node-edge.nix
@@ -14,6 +14,10 @@ with pkgs;
         port = 3001;
         hostAddr = "127.0.0.1";
         environment = "mainnet";
+        topology = commonLib.mkEdgeTopology {
+          port = 3001;
+          edgeNodes = [ "127.0.0.1" ];
+        };
         cardanoNodePkgs = pkgs;
         nodeConfig = config.services.cardano-node.environments.${config.services.cardano-node.environment}.nodeConfig // {
           hasPrometheus = [ config.services.cardano-node.hostAddr 12798 ];
@@ -37,13 +41,14 @@ with pkgs;
   testScript = ''
     start_all()
     machine.wait_for_unit("cardano-node.service")
-    machine.sleep(5)
-    machine.succeed("systemctl status cardano-node")
     machine.succeed("stat /run/cardano-node")
     machine.succeed("stat /run/cardano-node/node.socket")
-    machine.sleep(25)
-    machine.succeed("nc -z 127.0.0.1 12798")
-    machine.succeed("nc -z 127.0.0.1 3001")
+    machine.wait_for_open_port(12798)
+    machine.wait_for_open_port(3001)
+    machine.succeed("systemctl status cardano-node")
+    machine.succeed(
+        "${cardano-ping}/bin/cardano-ping -h 127.0.0.1 -c 1 -q --json | ${jq}/bin/jq -c"
+    )
   '';
 
 }
