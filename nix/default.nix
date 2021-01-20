88ab9981e879583864c764ec65383fc099720ea8
Michael Fellinger <michael.fellinger@iohk.io>
2021-01-20 17:10:08 +0100

    nix flake

--- nix/default.nix
+++ nix/default.nix
@@ -1,37 +1,35 @@
 { system ? builtins.currentSystem
 , crossSystem ? null
 , config ? {}
-, sourcesOverride ? {}
-, gitrev ? null
-}:
-let
-  sources = import ./sources.nix { inherit pkgs; }
-    // sourcesOverride;
-  iohkNixMain = import sources.iohk-nix {};
-  haskellNix = (import sources."haskell.nix" { inherit system sourcesOverride; }).nixpkgsArgs;
   # use our own nixpkgs if it exists in our sources,
   # otherwise use iohkNix default nixpkgs.
-  nixpkgs = if (sources ? nixpkgs)
+, nixpkgs ? if (sources ? nixpkgs)
     then (builtins.trace "Not using IOHK default nixpkgs (use 'niv drop nixpkgs' to use default for better sharing)"
       sources.nixpkgs)
-    else iohkNixMain.nixpkgs;
-
+    else iohkNix.nixpkgs
+, sourcesOverride ? {}
+, sources ? import ./sources.nix { pkgs = import nixpkgs { inherit system; }; } // sourcesOverride
+, gitrev ? null
+, iohkNix ? import sources.iohk-nix { inherit system; }
+, haskellNix ? (import sources."haskell.nix" { inherit system sourcesOverride; }).nixpkgsArgs
+}:
+let
   # for inclusion in pkgs:
   overlays =
     # Haskell.nix (https://github.com/input-output-hk/haskell.nix)
     haskellNix.overlays
     # haskell-nix.haskellLib.extra: some useful extra utility functions for haskell.nix
-    ++ iohkNixMain.overlays.haskell-nix-extra
-    ++ iohkNixMain.overlays.crypto
+    ++ iohkNix.overlays.haskell-nix-extra
+    ++ iohkNix.overlays.crypto
     # iohkNix: nix utilities and niv:
-    ++ iohkNixMain.overlays.iohkNix
+    ++ iohkNix.overlays.iohkNix
     # our own overlays:
     ++ [
       (pkgs: _: with pkgs; {
         inherit gitrev;
 
         # commonLib: mix pkgs.lib with iohk-nix utils and our own:
-        commonLib = lib // iohkNix // iohkNix.cardanoLib
+        commonLib = lib // pkgs.iohkNix // pkgs.iohkNix.cardanoLib
           // import ./util.nix { inherit haskell-nix; }
           # also expose our sources, nixpkgs and overlays
           // { inherit overlays sources nixpkgs; };
