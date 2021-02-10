c0a680cea43825716e308f78ba19a883971ceb53
Yupanqui <jnf@arcor.de>
2021-02-10 08:33:39 +0100

    Dependency, doc and started Config

--- trace-dispatcher/src/Cardano/Logging/Types.hs
+++ trace-dispatcher/src/Cardano/Logging/Types.hs
@@ -4,6 +4,7 @@
 module Cardano.Logging.Types where
 
 import           Control.Tracer
+import           Data.Map (Map)
 import           Data.Text (Text)
 import           Katip
 
@@ -21,6 +22,12 @@ data LoggingContextKatip = LoggingContextKatip {
   , lkLogEnv :: LogEnv
 }
 
+-- | Formerly known as verbosity
+data DetailLevel =
+      DBrief
+    | DRegular
+    | DDetailed
+    deriving (Show, Eq, Ord, Bounded, Enum)
 
 emptyLoggingContext :: LoggingContext
 emptyLoggingContext = LoggingContext [] Nothing Nothing Nothing
@@ -44,3 +51,42 @@ data SeverityF
 
 newtype Folding a b = Folding b
   deriving (ToObject)
+
+-- Configuration options
+data ConfigOption = ConfigOption {
+    -- | Severity level for a message
+    coSeverity    :: SeverityF
+    -- | Detail level for a message
+  , coDetailLevel :: DetailLevel
+}
+
+
+data TraceConfiguration = TraceConfiguration {
+     -- | Options taken, if no more specific options are available
+     tcDefaultOptions :: ConfigOption
+     -- | Options specific to a certain namespace
+  ,  tcOptions        :: Map Namespace ConfigOption
+
+  --  Forwarder:
+     -- Can their only be one forwarder? Use one of:
+
+     --  Forward messages to the following address
+--  ,  tcForwardTo :: RemoteAddr
+
+     --  Forward messages to the following address
+--  ,  tcForwardTo :: Map TracerName RemoteAddr
+
+  --  ** Katip:
+
+--  ,  tcDefaultScribe :: ScribeDefinition
+
+--  ,  tcScripes :: Map TracerName -> ScribeDefinition
+
+  --  EKG:
+     --  Port for EKG server
+--  ,  tcPortEKG :: Int
+
+  -- Prometheus:
+    --  Host/port to bind Prometheus server at
+--  ,  tcBindAddrPrometheus :: Maybe (String,Int)
+}
