f303d458cfb8f602847479d597984a4387276356
John Ky <john.ky@iohk.io>
2021-02-18 17:59:40 +1100

    Fixes

--- cardano-submit-api/src/Cardano/TxSubmit/JsonOrphans.hs
+++ cardano-submit-api/src/Cardano/TxSubmit/JsonOrphans.hs
@@ -6,11 +6,9 @@
 
 module Cardano.TxSubmit.JsonOrphans () where
 
-import Cardano.Api.TxBody ( TxId (..) )
 import Cardano.Crypto.Hash.Class as Cryptos
 import Data.Aeson
 
 import qualified Shelley.Spec.Ledger.TxBody as Shelley
 
-deriving newtype instance ToJSON TxId
 deriving newtype instance ToJSON (Shelley.TxId c)
