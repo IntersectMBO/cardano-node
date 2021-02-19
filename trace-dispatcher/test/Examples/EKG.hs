6af1cdea6a21ee727c1d78575cbd5f1b565b2a79
Yupanqui <jnf@arcor.de>
2021-02-19 15:29:44 +0100

    Adopting EKG

--- trace-dispatcher/test/Examples/EKG.hs
+++ trace-dispatcher/test/Examples/EKG.hs
@@ -3,14 +3,22 @@ module Examples.EKG where
 import           Cardano.Logging
 import           Control.Concurrent
 import           Control.Monad (liftM)
+import           Data.Text (pack)
 import           System.Remote.Monitoring (forkServer)
+
+instance Logging Int where
+  asMetrics i = [IntM Nothing i]
+
+instance Humanise Int where
+  humanise i = (pack . show) i
 
 testEKG :: IO ()
 testEKG = do
     server <- forkServer "localhost" 8000
-    tracer <- ekgTracer' server
+    tracer <- ekgTracer (Right server)
     loop (appendName "ekg1" tracer) 1
   where
+    loop :: Trace IO Int -> Int -> IO ()
     loop tracer count = do
       if count == 1000
         then pure ()
