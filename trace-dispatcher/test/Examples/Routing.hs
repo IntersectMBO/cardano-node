b0e1f8db31a554bbaad67832d4cea4ecbfb722b7
Yupanqui <jnf@arcor.de>
2021-03-02 10:59:30 +0100

    With compiling examples

--- trace-dispatcher/test/Examples/Routing.hs
+++ trace-dispatcher/test/Examples/Routing.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Examples.Routing where
 
 import           Control.Monad (liftM)
@@ -8,31 +9,33 @@ import           Katip.Scribes.Handle (ioLogEnv)
 import           Cardano.Logging
 import           Examples.TestObjects
 
-tracer1 :: MonadIO m => m (Trace m LO)
-tracer1  = liftM (appendName "tracer1") stdoutObjectKatipTracer
+tracer1 :: (LogFormatting (TraceForgeEvent blk), MonadIO m) =>
+  m (Trace m (TraceForgeEvent blk))
+tracer1  = fmap (appendName "tracer1") stdoutObjectKatipTracer
 
-tracer2 :: MonadIO m => m (Trace m LO)
-tracer2  = liftM (appendName "tracer2") stdoutJsonKatipTracer
+tracer2 :: (LogFormatting (TraceForgeEvent blk), MonadIO m) =>
+  m (Trace m (TraceForgeEvent blk))
+tracer2  = fmap (appendName "tracer2") stdoutJsonKatipTracer
 
 routingTracer1 :: (Monad m)
-  => Trace m LO
-  -> Trace m LO
-  -> Trace m LO
+  => Trace m (TraceForgeEvent LogBlock)
+  -> Trace m (TraceForgeEvent LogBlock)
+  -> Trace m (TraceForgeEvent LogBlock)
 routingTracer1 t1 t2 = routingTrace routingf (t1 <> t2)
   where
-    routingf LO1 {} = t1
-    routingf LO2 {} = t2
+    routingf TraceStartLeadershipCheck {} = t1
+    routingf _                            = t2
 
 routingTracer2 :: (Monad m)
-  => Trace m LO
-  -> Trace m LO
-  -> Trace m LO
+  => Trace m (TraceForgeEvent LogBlock)
+  -> Trace m (TraceForgeEvent LogBlock)
+  -> Trace m (TraceForgeEvent LogBlock)
 routingTracer2 t1 t2 = t1 <> t2
 
 testRouting :: IO ()
 testRouting = do
     t1 <- tracer1
     t2 <- tracer2
-    traceWith (routingTracer1 t1 t2) logObject1
-    traceWith (routingTracer1 t1 t2) logObject2
-    traceWith (t1 <> t2) logObject3
+    traceWith (routingTracer1 t1 t2) message1
+    traceWith (routingTracer1 t1 t2) message2
+    traceWith (t1 <> t2) message3
