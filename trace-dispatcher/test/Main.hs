ca2e3b2eb272b8769b9e89f22313de99c84f755d
Yupanqui <jnf@arcor.de>
2021-03-04 16:26:47 +0100

    DocuGenerator (Markdown)

--- trace-dispatcher/test/Main.hs
+++ trace-dispatcher/test/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import           Cardano.Logging
+import           Data.Text (Text)
 import           Examples.Aggregation
 import           Examples.Configuration
+import           Examples.Documentation
 import           Examples.EKG
 import           Examples.FrequencyLimiting
 import           Examples.Routing
@@ -12,10 +14,11 @@ import           Examples.Trivial
 
 main :: IO ()
 main = do
-    test1
-    test2
-    testAggregation
-    testRouting
-    testConfig
+    -- test1
+    -- test2
+    -- testAggregation
+    -- testRouting
+    -- testConfig
     -- testEKG
     -- testLimiting
+    docTracer
