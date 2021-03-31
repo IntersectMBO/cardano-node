425adaae6cfed4579c56c987b1190e20744971d2
Yupanqui <jnf@arcor.de>
2021-03-31 16:49:54 +0200

    More ChainDB messages

--- cardano-node/src/Cardano/TraceDispatcher/OrphanInstances/Network.hs
+++ cardano-node/src/Cardano/TraceDispatcher/OrphanInstances/Network.hs
@@ -17,11 +17,25 @@ import           Data.Aeson (Value (String), toJSON, (.=))
 
 import           Cardano.Logging (LogFormatting (forMachine), mkObject)
 import           Cardano.Prelude ()
-import           Cardano.Slotting.Slot (SlotNo (..))
-import           Cardano.TraceDispatcher.Render ()
+import           Cardano.TraceDispatcher.Render (renderHeaderHashForDetails)
+
+import           Ouroboros.Consensus.Block (ConvertRawHash (..), Proxy(..))
+import           Ouroboros.Network.Block
 
 
 instance LogFormatting SlotNo where
   forMachine _dtal slot =
     mkObject [ "kind" .= String "SlotNo"
                , "slot" .= toJSON (unSlotNo slot) ]
+
+instance ConvertRawHash blk
+      => LogFormatting (Point blk) where
+  forMachine _dtal GenesisPoint =
+    mkObject
+      [ "kind" .= String "GenesisPoint" ]
+  forMachine dtal (BlockPoint slot h) =
+    mkObject
+      [ "kind" .= String "BlockPoint"
+      , "slot" .= toJSON (unSlotNo slot)
+      , "headerHash" .= renderHeaderHashForDetails (Proxy @blk) dtal h
+      ]
