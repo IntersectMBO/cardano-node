a632fd14715a2e30a39ec2ed85b7a450fa5c74ed
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-04-27 19:08:44 +0400

    CAD-2902: change peer based on limits.

--- trace-forward/src/Trace/Forward/Protocol/Acceptor.hs
+++ trace-forward/src/Trace/Forward/Protocol/Acceptor.hs
@@ -9,21 +9,13 @@
 --
 -- For execution, a conversion into the typed protocol is provided.
 --
-module Trace.Forward.Protocol.Acceptor (
-    TraceAcceptor(..)
+module Trace.Forward.Protocol.Acceptor
+  ( TraceAcceptor(..)
   , traceAcceptorPeer
-  , byteLimitsTraceForward
-  , timeLimitsTraceForward
   ) where
-
-import           Data.Time.Clock (DiffTime)
 
 import           Network.TypedProtocol.Core (Peer (..), PeerHasAgency (..),
                                              PeerRole (..))
-import           Ouroboros.Network.Driver.Limits (ProtocolSizeLimits (..),
-                                                  ProtocolTimeLimits (..))
-import           Ouroboros.Network.Protocol.Limits (largeByteLimit, shortWait,
-                                                    smallByteLimit, waitForever)
 
 import           Trace.Forward.Protocol.Type
 
@@ -78,35 +70,3 @@ traceAcceptorPeer = \case
     Effect $
       Yield (ClientAgency TokIdle) MsgDone . Done TokDone
         <$> getResult
-
--- | Byte Limits.
-byteLimitsTraceForward
-  :: forall bytes lo.
-     (bytes -> Word)
-  -> ProtocolSizeLimits (TraceForward lo) bytes
-byteLimitsTraceForward = ProtocolSizeLimits stateToLimit
- where
-  stateToLimit
-    :: forall lo
-              (pr :: PeerRole)
-              (st :: TraceForward lo).
-       PeerHasAgency pr st
-    -> Word
-  stateToLimit (ServerAgency (TokBusy TokBlocking))    = largeByteLimit
-  stateToLimit (ServerAgency (TokBusy TokNonBlocking)) = largeByteLimit
-  stateToLimit (ClientAgency TokIdle)                  = smallByteLimit
-
--- | Time Limits.
-timeLimitsTraceForward
-  :: forall lo. ProtocolTimeLimits (TraceForward lo)
-timeLimitsTraceForward = ProtocolTimeLimits stateToLimit
- where
-  stateToLimit
-    :: forall lo
-              (pr :: PeerRole)
-              (st :: TraceForward lo).
-       PeerHasAgency pr st
-    -> Maybe DiffTime
-  stateToLimit (ServerAgency (TokBusy TokBlocking))    = waitForever
-  stateToLimit (ServerAgency (TokBusy TokNonBlocking)) = shortWait
-  stateToLimit (ClientAgency TokIdle)                  = waitForever
