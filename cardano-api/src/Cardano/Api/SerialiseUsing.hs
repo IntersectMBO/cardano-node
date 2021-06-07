cb9b0b7e86ad1374dea5590be2ec7b65c0456e13
Duncan Coutts <duncan@well-typed.com>
2021-06-08 00:00:56 +0100

    Add a UsingBech32 type, similar to UsingRawBytesHex
    This provides the same instances as UsingRawBytesHex but based on the
    bech32 encoding rather than hex encoding.

--- cardano-api/src/Cardano/Api/SerialiseUsing.hs
+++ cardano-api/src/Cardano/Api/SerialiseUsing.hs
@@ -5,6 +5,7 @@
 module Cardano.Api.SerialiseUsing
   ( UsingRawBytes(..)
   , UsingRawBytesHex(..)
+  , UsingBech32(..)
   ) where
 
 import           Prelude
@@ -12,13 +13,16 @@ import           Prelude
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Char8 as BSC
 import           Data.String (IsString (..))
+import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import           Data.Typeable
 
 import           Data.Aeson (ToJSONKey(..), FromJSONKey(..))
 import qualified Data.Aeson.Types as Aeson
 
+import           Cardano.Api.Error
 import           Cardano.Api.HasTypeProxy
+import           Cardano.Api.SerialiseBech32
 import           Cardano.Api.SerialiseCBOR
 import           Cardano.Api.SerialiseJSON
 import           Cardano.Api.SerialiseRaw
@@ -88,3 +92,41 @@ instance (SerialiseAsRawBytes a, Typeable a) => FromJSON (UsingRawBytesHex a) wh
 instance SerialiseAsRawBytes a => ToJSONKey (UsingRawBytesHex a)
 instance (SerialiseAsRawBytes a, Typeable a) => FromJSONKey (UsingRawBytesHex a)
 
+
+-- | For use with @deriving via@, to provide instances for any\/all of 'Show',
+-- 'IsString', 'ToJSON', 'FromJSON', 'ToJSONKey', FromJSONKey' using a bech32
+-- encoding, based on the 'SerialiseAsBech32' instance.
+--
+-- > deriving (Show, IsString) via (UsingBech32 Blah)
+-- > deriving (ToJSON, FromJSON) via (UsingBech32 Blah)
+-- > deriving (ToJSONKey, FromJSONKey) via (UsingBech32 Blah)
+--
+newtype UsingBech32 a = UsingBech32 a
+
+instance SerialiseAsBech32 a => Show (UsingBech32 a) where
+    show (UsingBech32 x) = show (serialiseToBech32 x)
+
+instance SerialiseAsBech32 a => IsString (UsingBech32 a) where
+    fromString str =
+      case deserialiseFromBech32 ttoken (Text.pack str) of
+        Right x  -> UsingBech32 x
+        Left  e -> error ("fromString: " ++ show str ++ ": " ++ displayError e)
+      where
+        ttoken :: AsType a
+        ttoken = proxyToAsType Proxy
+
+instance SerialiseAsBech32 a => ToJSON (UsingBech32 a) where
+    toJSON (UsingBech32 x) = toJSON (serialiseToBech32 x)
+
+instance (SerialiseAsBech32 a, Typeable a) => FromJSON (UsingBech32 a) where
+    parseJSON =
+      Aeson.withText tname $ \str ->
+        case deserialiseFromBech32 ttoken str of
+          Right x -> return (UsingBech32 x)
+          Left  e -> fail (show str ++ ": " ++ displayError e)
+      where
+        ttoken = proxyToAsType (Proxy :: Proxy a)
+        tname  = (tyConName . typeRepTyCon . typeRep) (Proxy :: Proxy a)
+
+instance SerialiseAsBech32 a => ToJSONKey (UsingBech32 a)
+instance (SerialiseAsBech32 a, Typeable a) => FromJSONKey (UsingBech32 a)
