04a3157a8aabef861f76080a52eda2cbaf60cc57
John Ky <john.ky@iohk.io>
2021-06-18 15:29:52 +1000

    New script to sync hlint rules

---
+++ scripts/reconfigure-hlint.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# This file extracts HLINT rules in '*.hs' files and injects them into the .hlint.yaml file
+# to work around the hlint issue with haskell-language-server documented here:
+#
+#     https://github.com/haskell/haskell-language-server/issues/638
+#
+# To use, simply run the script from the project's top-level directory.
+
+extract_rules() {
+  for x in $(find . -type f -name '*.hs' | grep -v dist); do
+    module="$(grep '^module' $x | cut -d ' ' -f 2 | head -n 1)"
+    grep '{- HLINT ignore "' $x | cut -d '"' -f 2 | \
+    sed "s|^|$module,|g"
+  done
+}
+
+gen_rules() {
+  extract_rules | sort | while read line; do
+    module="$(echo "$line" | cut -d ',' -f 1)"
+    rule="$(echo "$line" | cut -d ',' -f 2)"
+    echo "- ignore:"
+    echo "    name: \"$rule\""
+    echo "    within: $module"
+  done
+}
+
+(
+  echo "# This file is generated from .hlint.template.yaml by scripts/reconfigure-hlint.sh"
+  echo ""
+  cat .hlint.template.yaml \
+    | sed '/^# BEGIN-GENERATED/,/^# END-GENERATED/{/^# BEGIN-GENERATED/!{/^# END-GENERATED/!d}}' \
+    | gsed -e "/^# BEGIN-GENERATED/ r "<(gen_rules)
+) > ".hlint.yaml"
