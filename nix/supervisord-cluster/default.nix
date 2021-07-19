b3a00ca2218e1ce87194343c73734fcc64d1450b
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-07-19 23:16:14 +0300

    workbench | start-cluster:  add --help

--- nix/supervisord-cluster/default.nix
+++ nix/supervisord-cluster/default.nix
@@ -134,6 +134,20 @@ let
     ++ optionals (!workbenchDevMode)
     [ workbench.workbench ];
 
+  startClusterUsage = ''
+Usage:
+   start-cluster [FLAGS..]
+
+   Flags:
+
+      --batch-name NAME               Override the batch name (default: ${batchName})
+      --no-generator | --no-gen       Don't auto-start the tx-generator
+
+      --trace | --debug               Trace the start-cluster script
+      --trace-wb | --trace-workbench  Trace the workbench script
+      --help                          This help message
+'';
+
   start = pkgs.writeScriptBin "start-cluster" ''
     set -euo pipefail
 
@@ -147,6 +161,10 @@ let
 
         --trace | --debug )              set -x;;
         --trace-wb | --trace-workbench ) export WORKBENCH_EXTRA_FLAGS=--trace;;
+        --help )                         cat <<EOF
+${startClusterUsage}
+EOF
+                                         exit 1;;
         * ) break;; esac; shift; done
 
     workbench-prebuild-executables
