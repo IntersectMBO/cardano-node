72f6fb289f0e7ed7bc50a3d60844af79d874706e
John Ky <john.ky@iohk.io>
2021-08-02 22:08:01 +1000

    Improved formatting:
    * Switch to prettyprinter library
    * Tracing
    * Command for printing all help
    * Option ordering note on build-raw command

---
+++ cardano-cli/src/Cardano/CLI/Render.hs
@@ -0,0 +1,48 @@
+module Cardano.CLI.Render
+  ( customRenderHelp
+  ) where
+
+import           Cardano.Prelude
+import           Data.Function (id)
+import           Options.Applicative
+import           Options.Applicative.Help.Ann
+import           Options.Applicative.Help.Types (helpText)
+import           Prelude (String)
+import           Prettyprinter
+import           Prettyprinter.Render.Util.SimpleDocTree
+
+import qualified Data.Text as T
+import qualified System.Environment as IO
+import qualified System.IO.Unsafe as IO
+
+cliHelpTraceEnabled :: Bool
+cliHelpTraceEnabled = IO.unsafePerformIO $ do
+  mValue <- IO.lookupEnv "CLI_HELP_TRACE"
+  return $ mValue == Just "1"
+{-# NOINLINE cliHelpTraceEnabled #-}
+
+-- | Convert a help text to 'String'.  When the CLI_HELP_TRACE environment variable is set
+-- to '1', the output will be in HTML so that it can be viewed in a browser where developer
+-- tools can be used to inspect tracing that aids in describing the structure of the output
+-- document.
+customRenderHelp :: Int -> ParserHelp -> String
+customRenderHelp cols
+  = T.unpack
+  . wrapper
+  . renderSimplyDecorated id renderElement
+  . treeForm
+  . layoutSmart (LayoutOptions (AvailablePerLine cols 1.0))
+  . helpText
+  where
+    renderElement = if cliHelpTraceEnabled
+      then \(AnnTrace _ name) x -> "<span name=" <> show name <> ">" <> x <> "</span>"
+      else flip const
+    wrapper = if cliHelpTraceEnabled
+      then id
+        . ("<html>\n" <>)
+        . ("<body>\n" <>)
+        . ("<pre>\n" <>)
+        . (<> "\n</html>")
+        . (<> "\n</body>")
+        . (<> "\n</pre>")
+      else id
