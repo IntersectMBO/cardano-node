657f804393299b50b977a6eb6902b117e132d5ae
Jordan Millar <jordan.millar@iohk.io>
2021-09-02 09:02:49 -0400

    Expose toLedger/fromLedgerPParams from cardano-api

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -584,6 +584,8 @@ module Cardano.Api (
     NetworkMagic(..),
 
     -- ** Conversions
+    toLedgerPParams,
+    fromLedgerPParams,
     --TODO: arrange not to export these
     toNetworkMagic,
     --TODO: Remove after updating cardano-node-chairman with new IPC
