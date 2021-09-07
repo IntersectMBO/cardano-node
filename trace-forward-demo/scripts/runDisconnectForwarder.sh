709a663bd4bb32a1d62fa96960c726be2589fe4d
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-09-07 11:35:56 +0400

    trace-forward-demo:  update

--- trace-forward-demo/scripts/runDisconnectForwarder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./demo-forwarder-mux 127.0.0.1 3010 --dc 25 > /dev/null 2>&1 &
-
-PID=$(ps aux | grep demo-forwarder-mux | grep -v 'grep' | awk '{ print $2 }')
-
-watch -- "lsof -p ${PID} | grep TCP | wc -l"
