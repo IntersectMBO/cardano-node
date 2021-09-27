8fe423752f0d97af5a1075be75b599d5d4ba54f6
Jordan Millar <jordan.millar@iohk.io>
2021-09-27 15:42:03 -0400

    Add script equivalence test to cardano-testnet

--- cardano-testnet/test/Main.hs
+++ cardano-testnet/test/Main.hs
@@ -11,6 +11,7 @@ import qualified Test.Tasty as T
 import qualified Test.Tasty.Hedgehog as H
 import qualified Test.Tasty.Ingredients as T
 
+import qualified Spec.Plutus.Direct.ScriptContextEquality
 import qualified Spec.Plutus.Direct.TxInLockingPlutus
 import qualified Spec.Plutus.Script.TxInLockingPlutus
 import qualified Spec.Plutus.SubmitApi.TxInLockingPlutus
@@ -22,6 +23,7 @@ tests = do
       [ H.testProperty "Spec.Plutus.Direct.TxInLockingPlutus" Spec.Plutus.Direct.TxInLockingPlutus.hprop_plutus
       , H.testProperty "Spec.Plutus.Script.TxInLockingPlutus" Spec.Plutus.Script.TxInLockingPlutus.hprop_plutus
       , H.testProperty "Spec.Plutus.SubmitApi.TxInLockingPlutus" Spec.Plutus.SubmitApi.TxInLockingPlutus.hprop_plutus
+      , H.testProperty "Spec.Plutus.Direct.ScriptContextEquality"  Spec.Plutus.Direct.ScriptContextEquality.hprop_plutus_script_context_equality
       ]
     ]
 
