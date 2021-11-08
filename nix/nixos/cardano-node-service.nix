90a249e3febed70fb2ba38e267d78fdb85238c3b
Wessel Nieboer <wessel.nieboer@onior.com>
2021-11-08 15:23:24 +0100

    Suggestion to use the compacting collector in the nix build
    Will reduce memory usage, might be bad for block-producing nodes without
    enough CPU.

--- nix/nixos/cardano-node-service.nix
+++ nix/nixos/cardano-node-service.nix
@@ -404,7 +404,7 @@ in {
 
       rtsArgs = mkOption {
         type = types.listOf types.str;
-        default = [ "-N2" "-A16m" "-qg" "-qb" "--disable-delayed-os-memory-return" ];
+        default = [ "-N2" "-A16m" "-c" "-qg" "-qb" "--disable-delayed-os-memory-return" ];
         apply = args: if (args != [] || cfg.profilingArgs != []) then
           ["+RTS"] ++ cfg.profilingArgs ++ args ++ ["-RTS"]
           else [];
