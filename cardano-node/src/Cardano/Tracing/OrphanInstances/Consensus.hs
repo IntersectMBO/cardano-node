9b80b0e4973d833a519a98f553d0fbdea35f4fbb
Marcin Szamotulski <profunctor@pm.me>
2021-11-09 11:02:15 +0100

    consensus trace instances: trace termination reason
    Co-authored-by: Armando Santos <bolt12@users.noreply.github.com>
    Co-authored-by: Karl Knutsson <karknu@users.noreply.github.com>
    Co-authored-by: Marcin Szamotulski <coot@users.noreply.github.com>

--- cardano-node/src/Cardano/Tracing/OrphanInstances/Consensus.hs
+++ cardano-node/src/Cardano/Tracing/OrphanInstances/Consensus.hs
@@ -1013,8 +1013,9 @@ instance (ConvertRawHash blk, LedgerSupportsProtocol blk)
                , "exception" .= String (pack $ show exc) ]
     TraceFoundIntersection _ _ _ ->
       mkObject [ "kind" .= String "ChainSyncClientEvent.TraceFoundIntersection" ]
-    TraceTermination _ ->
-      mkObject [ "kind" .= String "ChainSyncClientEvent.TraceTermination" ]
+    TraceTermination reason ->
+      mkObject [ "kind" .= String "ChainSyncClientEvent.TraceTermination"
+               , "reason" .= String (pack $ show reason) ]
 
 instance ConvertRawHash blk
       => ToObject (TraceChainSyncServerEvent blk) where
