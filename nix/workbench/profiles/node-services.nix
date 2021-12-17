a463bdd5229b1862cad1a2364b04fc0f082e3011
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-12-17 15:09:10 +0300

    workbench:  integrate trace-dispatcher

--- nix/workbench/profiles/node-services.nix
+++ nix/workbench/profiles/node-services.nix
@@ -51,10 +51,39 @@ let
                Protocol             = "Cardano";
                RequiresNetworkMagic = "RequiresMagic";
 
+               UseTraceDispatcher   = true;
+
                TracingVerbosity     = "NormalVerbosity";
                minSeverity          = "Debug";
 
                TraceMempool         = true;
+
+               TraceOptionSeverity  = [
+                 {ns = ""; severity = "Debug";}
+                 {ns = "Node.Resources"; severity = "Debug";}
+                 ## {ns = "Node.Mempool"; severity = "Debug";}
+                 {ns = "Node.ChainDB"; severity = "Debug";}
+                 ## {ns = "Node.ChainDB.ImmutableDBEvent"; severity = "Warning";}
+               ];
+
+               TraceOptionDetail = [
+                 {ns = ""; detail = "DNormal";}
+                 ## {ns = "Node.BlockFetchClient"; detail = "DMinimal";}
+               ];
+
+               TraceOptionBackend = [
+                 {ns = ""; backends = ["Stdout MachineFormat" "EKGBackend" "Forwarder"];}
+                 ## {ns = "Node.ChainDB"; backends = ["Stdout HumanFormatColoured"];}
+               ];
+
+               TraceOptionLimiter = [
+                 {ns = "Node.ChainDB.OpenEvent"; limiterName = "ChainDB open limiter"; limiterFrequency = 0.1;}
+               ];
+
+               TraceOptionForwarder = {filePath = "/tmp/forwarder-${toString i}.sock";};
+
+               TraceOptionForwardQueueSize = 700;
+
                TraceTxInbound       = true;
 
                defaultScribes = [
