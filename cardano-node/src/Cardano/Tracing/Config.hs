3110fb3df6967d9c1806d045d6e119bb0a220da5
Marcin Szamotulski <profunctor@pm.me>
2022-01-14 12:58:29 +0100

    connection-manager & inbound governor transition tracers

--- cardano-node/src/Cardano/Tracing/Config.hs
+++ cardano-node/src/Cardano/Tracing/Config.hs
@@ -47,6 +47,7 @@ type TraceChainSyncHeaderServer = ("TraceChainSyncHeaderServer" :: Symbol)
 type TraceChainSyncProtocol = ("TraceChainSyncProtocol" :: Symbol)
 type TraceConnectionManager = ("TraceConnectionManager" :: Symbol)
 type TraceConnectionManagerCounters = ("TraceConnectionManagerCounters" :: Symbol)
+type TraceConnectionManagerTransitions = ("TraceConnectionManagerTransitions" :: Symbol)
 type DebugPeerSelectionInitiator = ("DebugPeerSelectionInitiator" :: Symbol)
 type DebugPeerSelectionInitiatorResponder = ("DebugPeerSelectionInitiatorResponder" :: Symbol)
 type TraceDiffusionInitialization = ("TraceDiffusionInitialization" :: Symbol)
@@ -80,6 +81,7 @@ type TracePublicRootPeers = ("TracePublicRootPeers" :: Symbol)
 type TraceServer = ("TraceServer" :: Symbol)
 type TraceInboundGovernor = ("TraceInboundGovernor" :: Symbol)
 type TraceInboundGovernorCounters = ("TraceInboundGovernorCounters" :: Symbol)
+type TraceInboundGovernorTransitions = ("TraceInboundGovernorTransitions" :: Symbol)
 type TraceTxInbound = ("TraceTxInbound" :: Symbol)
 type TraceTxOutbound = ("TraceTxOutbound" :: Symbol)
 type TraceTxSubmissionProtocol = ("TraceTxSubmissionProtocol" :: Symbol)
@@ -113,6 +115,7 @@ data TraceSelection
   , traceChainSyncProtocol :: OnOff TraceChainSyncProtocol
   , traceConnectionManager :: OnOff TraceConnectionManager
   , traceConnectionManagerCounters :: OnOff TraceConnectionManagerCounters
+  , traceConnectionManagerTransitions :: OnOff TraceConnectionManagerTransitions
   , traceDebugPeerSelectionInitiatorTracer :: OnOff DebugPeerSelectionInitiator
   , traceDebugPeerSelectionInitiatorResponderTracer :: OnOff DebugPeerSelectionInitiatorResponder
   , traceDiffusionInitialization :: OnOff TraceDiffusionInitialization
@@ -124,6 +127,7 @@ data TraceSelection
   , traceHandshake :: OnOff TraceHandshake
   , traceInboundGovernor :: OnOff TraceInboundGovernor
   , traceInboundGovernorCounters :: OnOff TraceInboundGovernorCounters
+  , traceInboundGovernorTransitions :: OnOff TraceInboundGovernorTransitions
   , traceIpSubscription :: OnOff TraceIpSubscription
   , traceKeepAliveClient :: OnOff TraceKeepAliveClient
   , traceLedgerPeers :: OnOff TraceLedgerPeers
@@ -184,6 +188,8 @@ traceConfigParser v =
       connectionManager = OnOff False
       connectionManagerCounters :: OnOff TraceConnectionManagerCounters
       connectionManagerCounters = OnOff True
+      connectionManagerTransitions :: OnOff TraceConnectionManagerTransitions
+      connectionManagerTransitions = OnOff False
       debugPeerSelectionInitiator :: OnOff DebugPeerSelectionInitiator
       debugPeerSelectionInitiator = OnOff False
       debugPeerSelectionInitiatorResponder :: OnOff DebugPeerSelectionInitiatorResponder
@@ -206,6 +212,8 @@ traceConfigParser v =
       inboundGovernor = OnOff False
       inboundGovernorCounters :: OnOff TraceInboundGovernorCounters
       inboundGovernorCounters = OnOff True
+      inboundGovernorTransitions :: OnOff TraceInboundGovernorTransitions
+      inboundGovernorTransitions = OnOff False
       ipSubscription :: OnOff TraceIpSubscription
       ipSubscription = OnOff True
       keepAliveClient :: OnOff TraceKeepAliveClient
@@ -276,6 +284,7 @@ traceConfigParser v =
     <*> v .:? getName chainSyncProtocol .!= chainSyncProtocol
     <*> v .:? getName connectionManager .!= connectionManager
     <*> v .:? getName connectionManagerCounters .!= connectionManagerCounters
+    <*> v .:? getName connectionManagerTransitions .!= connectionManagerTransitions
     <*> v .:? getName debugPeerSelectionInitiator
                        .!= debugPeerSelectionInitiator
     <*> v .:? getName debugPeerSelectionInitiatorResponder
@@ -289,6 +298,7 @@ traceConfigParser v =
     <*> v .:? getName handshake .!= handshake
     <*> v .:? getName inboundGovernor .!= inboundGovernor
     <*> v .:? getName inboundGovernorCounters .!= inboundGovernorCounters
+    <*> v .:? getName inboundGovernorTransitions .!= inboundGovernorTransitions
     <*> v .:? getName ipSubscription .!= ipSubscription
     <*> v .:? getName keepAliveClient .!= keepAliveClient
     <*> v .:? getName ledgerPeers .!= ledgerPeers
