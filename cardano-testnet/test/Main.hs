8922ece8c9dd1aa06fb57655df91c7668087df83
Nicholas Clarke <nick@topos.org.uk>
2022-02-11 12:10:22 +0100

    Disable Plutus related testing because this introduces a  cyclical dependency between cardano-node and plutus-apps. This is ok because cardano-node-tests covers all of the Plutus related tests that node does. With the exception of one test that is currently flakey in any case. Co-authored-by: Jordan Millar <jordan.millar@iohk.io>

--- cardano-testnet/test/Main.hs
+++ cardano-testnet/test/Main.hs
@@ -8,16 +8,16 @@ import           Prelude
 
 import           Test.Tasty (TestTree)
 
-import qualified Spec.Plutus.Direct.CertifyingAndWithdrawingPlutus
-import qualified Spec.Plutus.Direct.ScriptContextEquality
-import qualified Spec.Plutus.Direct.ScriptContextEqualityMint
-import qualified Spec.Plutus.Direct.TxInLockingPlutus
-import qualified Spec.Plutus.Script.TxInLockingPlutus
-import qualified Spec.Plutus.SubmitApi.TxInLockingPlutus
+-- TODO: Move to plutus-apps
+-- import qualified Spec.Plutus.Direct.CertifyingAndWithdrawingPlutus
+-- import qualified Spec.Plutus.Direct.ScriptContextEquality
+-- import qualified Spec.Plutus.Direct.ScriptContextEqualityMint
+-- import qualified Spec.Plutus.Direct.TxInLockingPlutus
+-- import qualified Spec.Plutus.Script.TxInLockingPlutus
+-- import qualified Spec.Plutus.SubmitApi.TxInLockingPlutus
 import qualified Spec.Shutdown
 import qualified System.Environment as E
 import qualified Test.Tasty as T
-import qualified Test.Tasty.Hedgehog as H
 import qualified Test.Tasty.Ingredients as T
 import qualified Test.Util as H
 
@@ -25,16 +25,17 @@ tests :: IO TestTree
 tests = do
   pure $ T.testGroup "test/Spec.hs"
     [ T.testGroup "Spec"
-      [ -- Fails to meet deadline on MacOS for an unknown reason
-        H.ignoreOnMacAndWindows "Plutus.Direct.CertifyingAndWithdrawingPlutus" Spec.Plutus.Direct.CertifyingAndWithdrawingPlutus.hprop_plutus_certifying_withdrawing
-      , H.testProperty "Plutus.Direct.TxInLockingPlutus" Spec.Plutus.Direct.TxInLockingPlutus.hprop_plutus
-        -- This hangs on Windows for an unknown reason
-      , H.ignoreOnWindows "Plutus.Script.TxInLockingPlutus" Spec.Plutus.Script.TxInLockingPlutus.hprop_plutus
-      , H.testProperty "Plutus.SubmitApi.TxInLockingPlutus" Spec.Plutus.SubmitApi.TxInLockingPlutus.hprop_plutus
-      , H.ignoreOnWindows "Plutus.Direct.ScriptContextEquality"  Spec.Plutus.Direct.ScriptContextEquality.hprop_plutus_script_context_equality
-      , H.ignoreOnWindows "Plutus.Direct.ScriptContextEqualityMint" Spec.Plutus.Direct.ScriptContextEqualityMint.hprop_plutus_script_context_mint_equality
+      [ -- TODO: Move to plutus-apps
+        -- Fails to meet deadline on MacOS for an unknown reason
+     --    H.ignoreOnMacAndWindows "Plutus.Direct.CertifyingAndWithdrawingPlutus" Spec.Plutus.Direct.CertifyingAndWithdrawingPlutus.hprop_plutus_certifying_withdrawing
+     --  , H.testProperty "Plutus.Direct.TxInLockingPlutus" Spec.Plutus.Direct.TxInLockingPlutus.hprop_plutus
+     --    -- This hangs on Windows for an unknown reason
+     --  , H.ignoreOnWindows "Plutus.Script.TxInLockingPlutus" Spec.Plutus.Script.TxInLockingPlutus.hprop_plutus
+     --  , H.testProperty "Plutus.SubmitApi.TxInLockingPlutus" Spec.Plutus.SubmitApi.TxInLockingPlutus.hprop_plutus
+     --  , H.ignoreOnWindows "Plutus.Direct.ScriptContextEquality"  Spec.Plutus.Direct.ScriptContextEquality.hprop_plutus_script_context_equality
+     --  , H.ignoreOnWindows "Plutus.Direct.ScriptContextEqualityMint" Spec.Plutus.Direct.ScriptContextEqualityMint.hprop_plutus_script_context_mint_equality
         -- There is a blocking call on Windows that prevents graceful shutdown and we currently aren't testing the shutdown IPC flag.
-      , H.ignoreOnWindows "Shutdown" Spec.Shutdown.hprop_shutdown
+        H.ignoreOnWindows "Shutdown" Spec.Shutdown.hprop_shutdown
       ]
     ]
 
