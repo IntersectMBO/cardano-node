a3fce3962adb94efe83c63d4247ccb477c52a54e
MarcFontaine <MarcFontaine@users.noreply.github.com>
2022-04-25 16:24:28 +0200

    tx-generator remove legacy mode

--- nix/nixos/tx-generator-service.nix
+++ nix/nixos/tx-generator-service.nix
@@ -26,110 +26,7 @@ let
                   nodeConfigFile
                   localNodeSocketPath;
      };
-  ## Standard, simplest possible value transaction workload.
-  ##
-  ## For definitions of the cfg attributes referred here,
-  ## please see the 'defServiceModule.extraOptionDecls' attrset below.
-  lowLevelTxGenScript =
-    cfg: with cfg; with pkgs.lib;
-    [
-      { InitWallet = defaultWallet; }
-      { Set.SNumberOfInputsPerTx   = inputs_per_tx; }
-      { Set.SNumberOfOutputsPerTx  = outputs_per_tx; }
-      { Set.SNumberOfTxs           = tx_count; }
-      { Set.STxAdditionalSize      = add_tx_size; }
-      { Set.SMinValuePerUTxO       = min_utxo_value; }
-      { Set.SFee                   = tx_fee; }
-      { Set.STTL                   = 1000000; }
-      { StartProtocol              = nodeConfigFile; }
-      { Set.SEra                   = capitalise era; }
-      { Set.STargets               = targetNodesList cfg.targetNodes;}
-      { Set.SLocalSocket  = localNodeSocketPath; }
-      { ReadSigningKey    = [ "pass-partout" sigKey]; }
-      { ImportGenesisFund = [ defaultWallet { LocalSocket = []; } "pass-partout"  "pass-partout" ]; }
-      { Delay             = init_cooldown; }
-    ]
-    ++
-    ( let
-        ## hard-code mainnet cost model
-        ## scriptFees = executionMemory * 577 / 10000 + executionSteps  * 721 / 10000000;
-        scriptFees = 5000000;
-        collateralPercentage = 200;
-
-        totalFee = if plutusMode
-                   then tx_fee + scriptFees * inputs_per_tx
-                   else tx_fee;
-        safeCollateral = max ((scriptFees + tx_fee) * collateralPercentage / 100) min_utxo_value;
-        minTotalValue = min_utxo_value * outputs_per_tx + totalFee;
-        minValuePerInput = minTotalValue / inputs_per_tx + 1;
-      in
-        if !plutusMode
-          then createChangeRecursive cfg minValuePerInput (tx_count * inputs_per_tx)
-        else
-          [
-          # this is a hack !
-          # PayToCollateral will create outputs which are internally tagged as collateral and not available for splitting etc.
-          # If PayToCollateral returns a change value that value will also be tagged as collateral and lost.
-          # Therefore this first creates a matching regular output
-          # and turns that into a collateral right in the next step.
-            { CreateChange = [
-                defaultWallet
-                defaultWallet
-                { LocalSocket = []; }
-                { PayToAddr = "pass-partout"; }
-                (safeCollateral + tx_fee)
-                1
-              ];
-            }
-            { CreateChange = [
-                defaultWallet
-                defaultWallet
-                { LocalSocket = []; }
-                { PayToCollateral = "pass-partout"; }
-                safeCollateral
-                1
-              ];
-            }
-          ]
-          ++ createChangePlutus cfg minValuePerInput (tx_count * inputs_per_tx)
-    )
-    ++
-    [
-      { RunBenchmark = [
-          defaultWallet
-          ( if !debugMode
-            then { NodeToNode = []; }
-            else { LocalSocket = []; }
-          )
-          ( if plutusAutoMode
-            then { SpendAutoScript = plutusScriptFile cfg "loop.plutus"; }
-            else if plutusMode
-            then { SpendScript = [
-                     (plutusScript cfg)
-                     ( if debugMode
-                       then { CheckScriptBudget = { memory = executionMemory; steps = executionSteps; }; }
-                       else { StaticScriptBudget = { memory = executionMemory; steps = executionSteps; }; }
-                     )
-                     plutusData
-                     plutusRedeemer
-                   ]; }
-            else { SpendOutput = []; }
-          )
-          "tx-submit-benchmark"
-          tx_count
-          tps
-        ];
-      }
-    ]
-    ++
-    (
-      if !debugMode
-      then [ { WaitBenchmark = "tx-submit-benchmark"; } ]
-      else [ ]
-    )
-  ;
-  defaultWallet = "defaultWallet";
-  defaultGeneratorScriptFn = lowLevelTxGenScript;
+  defaultGeneratorScriptFn = cleanNixServiceOptions;
   ## The standard decision procedure for the run script:
   ##
   ##  - if the config explicitly specifies a script, take that,
@@ -142,43 +39,6 @@ let
          else runScriptFn cfg);
     
   capitalise = x: (pkgs.lib.toUpper (__substring 0 1 x)) + __substring 1 99999 x;
-
-  createChangeScript = cfg: value: count:
-    [ { CreateChange = [
-          defaultWallet
-          defaultWallet
-          { LocalSocket = []; }
-          { PayToAddr = "pass-partout"; }
-          value
-          count
-        ];
-      }
-      { Delay = cfg.init_cooldown; }
-    ];
-
-  createChangeScriptPlutus = cfg: value: count:
-    [ { CreateChange = [
-          defaultWallet
-          defaultWallet
-          { LocalSocket = []; }
-          { PayToScript = if cfg.plutusAutoMode
-                          then [ (plutusScriptFile cfg "loop.plutus") 0 ]
-                          else [ (plutusScript cfg) cfg.plutusData ];
-          }
-          value
-          count
-        ];
-      }
-      { Delay = cfg.init_cooldown; }
-    ];
-
-  createChangeRecursive = cfg: value: count: if count <= 30
-    then createChangeScript cfg value count
-    else createChangeRecursive cfg (value * 30 + cfg.tx_fee) (count / 30 + 1) ++ createChangeScript cfg value count;
-
-  createChangePlutus = cfg: value: count: if count <= 30
-    then createChangeScriptPlutus cfg value count
-    else createChangeRecursive cfg (value * 30 + cfg.tx_fee) (count / 30 + 1) ++ createChangeScriptPlutus cfg value count;
 
   plutusScript = cfg: plutusScriptFile cfg cfg.plutusScript;
   plutusScriptFile = cfg: filename: "${pkgs.plutus-scripts}/generated-plutus-scripts/${filename}";
@@ -223,7 +83,7 @@ in pkgs.commonLib.defServiceModule
         tps             = opt int 100        "Strength of generated load, in TPS.";
         init_cooldown   = opt int 50         "Delay between init and main submissions.";
         min_utxo_value  = opt int 10000000   "Minimum value allowed per UTxO entry";
-        runScriptFn     = opt (functionTo (listOf attrs)) defaultGeneratorScriptFn
+        runScriptFn     = opt (functionTo attrs) defaultGeneratorScriptFn
           "Function accepting this service config and producing the generator run script (a list of command attrsets).  Takes effect unless runScript or runScriptFile are specified.";
         runScript       = mayOpt (listOf attrs)
           "Generator run script (a list of command attrsets).  Takes effect unless runScriptFile is specified.";
@@ -256,7 +116,7 @@ in pkgs.commonLib.defServiceModule
       
       configExeArgsFn = cfg: [
           "json_highlevel"
-          "${pkgs.writeText "tx-gen-config.json" (__toJSON (cleanNixServiceOptions cfg))}"
+          "${pkgs.writeText "tx-gen-config.json" (cfg.decideRunScript cfg)}"
         ];
 
       configSystemdExtraConfig = _: {};
