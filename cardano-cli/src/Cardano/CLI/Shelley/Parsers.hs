690c460f8509aeb86783e6229b12ba0d9d3541ee
Jordan Millar <jordan.millar@iohk.io>
2022-04-27 11:36:26 -0500

    Intoduce an additional constructor for LocalNodeClientParams to encompass the different requirements for the CardanoMode vs the single era modes.

--- cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -1993,7 +1993,10 @@ pCardanoEra = asum
       (  Opt.long "alonzo-era"
       <> Opt.help "Specify the Alonzo era"
       )
-
+  , Opt.flag' (AnyCardanoEra BabbageEra)
+      (  Opt.long "babbage-era"
+      <> Opt.help "Specify the Babbage era"
+      )
     -- Default for now:
   , pure (AnyCardanoEra AlonzoEra)
   ]
