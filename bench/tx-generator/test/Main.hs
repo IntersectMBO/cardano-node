9942950276fc4de4cda999947aeb247f3e7bef67
MarcFontaine <MarcFontaine@users.noreply.github.com>
2022-04-27 13:59:57 +0200

    tx-generator updates for Babbage
    sized Metadate : tests for Alonzo & Babbage
    rebase on #3818 and reuse code from Cardano.Api.InMode
    fixes in SubmissionClient
    workbench:  add babbage era

--- bench/tx-generator/test/Main.hs
+++ bench/tx-generator/test/Main.hs
@@ -18,11 +18,15 @@ tests =  testGroup "cardano-tx-generator"
   ]
 
 sizedMetadata = testGroup "properties of the CBOR encoding relevant for generating sized metadat"
-  [ testCase "Shelley metadata map costs" $ assertBool "metadata map costs" prop_mapCostsShelley
-  , testCase "Shelley metadata ByteString costs" $ assertBool "metadata ByteString costs" prop_bsCostsShelley
-  , testCase "Allegra metadata map costs" $ assertBool "metadata map costs" prop_mapCostsAllegra
-  , testCase "Allegra metadata ByteString costs" $ assertBool "metadata ByteString costs" prop_bsCostsAllegra
-  , testCase "Mary metadata map costs"    $ assertBool "metadata map costs" prop_mapCostsMary
-  , testCase "Marymetadata ByteString costs"    $ assertBool "metadata ByteString costs" prop_bsCostsMary
+  [ testCase "Shelley metadata map costs"          $ assertBool "metadata map costs" prop_mapCostsShelley
+  , testCase "Shelley metadata ByteString costs"   $ assertBool "metadata ByteString costs" prop_bsCostsShelley
+  , testCase "Allegra metadata map costs"          $ assertBool "metadata map costs" prop_mapCostsAllegra
+  , testCase "Allegra metadata ByteString costs"   $ assertBool "metadata ByteString costs" prop_bsCostsAllegra
+  , testCase "Mary metadata map costs"             $ assertBool "metadata map costs" prop_mapCostsMary
+  , testCase "Mary metadata ByteString costs"      $ assertBool "metadata ByteString costs" prop_bsCostsMary
+  , testCase "Alonzo metadata map costs"           $ assertBool "metadata map costs" prop_mapCostsAlonzo
+  , testCase "Alonzo metadata ByteString costs"    $ assertBool "metadata ByteString costs" prop_bsCostsAlonzo
+  , testCase "Babbage metadata map costs"           $ assertBool "metadata map costs" prop_mapCostsBabbage
+  , testCase "Babbage metadata ByteString costs"    $ assertBool "metadata ByteString costs" prop_bsCostsBabbage
   , testCase "Test mkMetadata" $ assertBool "" True --WIP
   ]
