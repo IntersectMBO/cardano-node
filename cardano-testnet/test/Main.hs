47ce6adbbead5d9600d0d78db6bb4f1f4b22cec5
Jordan Millar <jordan.millar@iohk.io>
2022-05-13 15:33:15 -0500

    Temporarily disable leadership, protocol-state and kes-info commands. Update readFileTx and acceptTxCDDLSerialisation functions for Babbage Disable the kes-period-info command test temporarily

--- cardano-testnet/test/Main.hs
+++ cardano-testnet/test/Main.hs
@@ -1,28 +1,29 @@
-{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 
 module Main
   ( main
   ) where
 
-import Prelude
-import Spec.Cli.KesPeriodInfo qualified
-import Spec.Node.Shutdown qualified
-import Spec.ShutdownOnSlotSynced qualified
-import System.Environment qualified as E
-import Test.Tasty (TestTree)
-import Test.Tasty qualified as T
-import Test.Tasty.Ingredients qualified as T
-import Test.Util qualified as H
+import           Prelude
+-- import qualified Spec.Cli.KesPeriodInfo
+import qualified Spec.Node.Shutdown
+import qualified Spec.ShutdownOnSlotSynced
+import qualified System.Environment as E
+import           Test.Tasty (TestTree)
+import qualified Test.Tasty as T
+import qualified Test.Tasty.Ingredients as T
+import qualified Test.Util as H
 
 tests :: IO TestTree
 tests = pure $ T.testGroup "test/Spec.hs"
   [ T.testGroup "Spec"
-    [ H.ignoreOnWindows "Shutdown" Spec.Node.Shutdown.hprop_shutdown
+    [  H.ignoreOnWindows "Shutdown" Spec.Node.Shutdown.hprop_shutdown
     , H.ignoreOnWindows "ShutdownOnSlotSynced" Spec.ShutdownOnSlotSynced.hprop_shutdownOnSlotSynced
       -- Ignored on Windows due to <stdout>: commitBuffer: invalid argument (invalid character)
       -- as a result of the kes-period-info output to stdout.
-      , H.ignoreOnWindows "kes-period-info" Spec.Cli.KesPeriodInfo.hprop_kes_period_info
+      -- TODO: Babbage temporarily ignored due to broken protocol-state query
+      -- H.ignoreOnWindows "kes-period-info" Spec.Cli.KesPeriodInfo.hprop_kes_period_info
     ]
   ]
 
