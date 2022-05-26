9c90a443a1fba1973bf46fe1e71b69b9a7a2c43f
Yuriy Syrovetskiy <yuriy.syrovetskiy@iohk.io>
2022-05-26 18:00:58 +0200

    cardano-api: Add ToJSON instances for protocol update types

--- cardano-api/src/Cardano/Api/ProtocolParameters.hs
+++ cardano-api/src/Cardano/Api/ProtocolParameters.hs
@@ -74,10 +74,11 @@ module Cardano.Api.ProtocolParameters (
 
 import           Control.Applicative ((<|>))
 import           Control.Monad
-import           Data.Aeson (FromJSON (..), ToJSON (..), object, withObject, (.!=), (.:), (.:?),
-                   (.=))
+import           Data.Aeson (FromJSON (..), ToJSON (..), defaultOptions, fieldLabelModifier,
+                   genericToJSON, object, withObject, (.!=), (.:), (.:?), (.=))
 import           Data.Bifunctor (bimap, first)
 import           Data.ByteString (ByteString)
+import           Data.Char (toLower)
 import           Data.Either.Combinators (maybeToRight)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -541,7 +542,7 @@ data ProtocolParametersUpdate =
        -- /Introduced in Babbage.  Supercedes 'protocolUpdateUTxOCostPerWord'/
        protocolUpdateUTxOCostPerByte :: Maybe Lovelace
     }
-  deriving (Eq, Show)
+  deriving (Eq, Generic, Show)
 
 instance Semigroup ProtocolParametersUpdate where
     ppu1 <> ppu2 =
@@ -675,6 +676,15 @@ instance FromCBOR ProtocolParametersUpdate where
         <*> fromCBOR
         <*> fromCBOR
         <*> fromCBOR
+
+instance ToJSON ProtocolParametersUpdate where
+  toJSON =
+    genericToJSON defaultOptions{fieldLabelModifier = lowerFirst . drop 14}
+    where
+      lowerFirst s = case s of
+        "" -> ""
+        'U':'T':'x':'O':xs -> "utxo" ++ xs
+        x:xs -> toLower x : xs
 
 
 -- ----------------------------------------------------------------------------
@@ -848,7 +858,7 @@ data UpdateProposal =
      UpdateProposal
        !(Map (Hash GenesisKey) ProtocolParametersUpdate)
        !EpochNo
-    deriving stock (Eq, Show)
+    deriving stock (Eq, Generic, Show)
     deriving anyclass SerialiseAsCBOR
 
 instance HasTypeProxy UpdateProposal where
@@ -870,6 +880,13 @@ instance FromCBOR UpdateProposal where
       UpdateProposal
         <$> fromCBOR
         <*> fromCBOR
+
+instance ToJSON UpdateProposal where
+  toJSON (UpdateProposal updates epoch) =
+    object
+      [ "updates" .= Map.mapKeys serialiseToRawBytesHexText updates
+      , "epoch" .= epoch
+      ]
 
 makeShelleyUpdateProposal :: ProtocolParametersUpdate
                           -> [Hash GenesisKey]
