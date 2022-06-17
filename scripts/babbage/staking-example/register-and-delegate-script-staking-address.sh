5a2b2d09a2bece5c1d05ebd1a129162a0a9e7449
Jordan Millar <jordan.millar@iohk.io>
2022-06-17 13:00:37 -0500

    Adjust register-and-deglegate-script-staking-address.sh to use a plutus certifying script

--- scripts/babbage/staking-example/register-and-delegate-script-staking-address.sh
+++ scripts/babbage/staking-example/register-and-delegate-script-staking-address.sh
@@ -13,7 +13,7 @@ export TESTNET_MAGIC="${TESTNET_MAGIC:-42}"
 export UTXO_VKEY1="${UTXO_VKEY1:-example/utxo-keys/utxo1.vkey}"
 export UTXO_SKEY1="${UTXO_SKEY1:-example/utxo-keys/utxo1.skey}"
 export UTXO_STAKING_VKEY2="${UTXO_STAKING_VKEY2:=example/utxo-keys/utxo2-stake.vkey}"
-export PV=v1 # Plutus Script Version
+export PV=v2 # Plutus Script Version
 
 mkdir -p "$WORK"
 nodepool1dir=example/node-spo1
@@ -52,15 +52,46 @@ echo ""
 txin=$(jq -r 'keys[]' "$WORK/utxo-1.json")
 lovelaceattxin=$(jq -r ".[\"$txin\"].value.lovelace" "$WORK/utxo-1.json")
 lovelaceattxindiv3=$((lovelaceattxin / 3))
-scriptpaymentaddrwithstakecred=$(cardano-cli address build --payment-verification-key-file "$UTXO_VKEY1"  --stake-script-file "scripts/plutus/scripts/$PV/guess-42-stake.plutus" --testnet-magic 42)
+certifyingscript="scripts/plutus/scripts/v2/stake-script.plutus"
+scriptpaymentaddrwithstakecred=$(cardano-cli address build --payment-verification-key-file "$UTXO_VKEY1"  --stake-script-file $certifyingscript --testnet-magic 42)
+dummyaddress=addr_test1vpqgspvmh6m2m5pwangvdg499srfzre2dd96qq57nlnw6yctpasy4
 
 # Node Pool 1
 poolcoldkey="example/pools/cold1.vkey"
 
-# Delegate Plutus staking script address to stake pool
+# Delegate Plutus staking script address to stake pool.
 
-# Update UTxO again
+cardano-cli query protocol-parameters --testnet-magic "$TESTNET_MAGIC" --out-file "$WORK/pparams.json"
 
+# Step 1: Create certifying reference script at tx out
+
+cardano-cli transaction build \
+  --babbage-era \
+  --cardano-mode \
+  --testnet-magic "$TESTNET_MAGIC" \
+  --change-address "$utxoaddr" \
+  --tx-in "$txin" \
+  --tx-out "$dummyaddress+$lovelaceattxindiv3" \
+  --tx-out-reference-script-file "$certifyingscript" \
+  --protocol-params-file "$WORK/pparams.json" \
+  --out-file "$WORK/create-stake-reference-script.body"
+
+cardano-cli transaction sign \
+  --tx-body-file "$WORK/create-stake-reference-script.body" \
+  --testnet-magic "$TESTNET_MAGIC" \
+  --signing-key-file "$UTXO_SKEY1" \
+  --out-file "$WORK/create-stake-reference-script.tx"
+
+cardano-cli transaction submit \
+  --tx-file "$WORK/create-stake-reference-script.tx" \
+  --testnet-magic "$TESTNET_MAGIC"
+
+echo ""
+echo "Wait 5 seconds..."
+echo ""
+sleep 5
+
+# Step 2: Create and submit registration certificate for the staking script
 cardano-cli query utxo \
   --address "$utxoaddr" \
   --cardano-mode \
@@ -68,29 +99,23 @@ cardano-cli query utxo \
   --out-file "$WORK/utxo-2.json"
 
 cat "$WORK/utxo-2.json"
-
-txinupdated2=$(jq -r 'keys[0]' "$WORK/utxo-2.json")
-echo ""
-echo "Selected txin: $txinupdated2"
-# Step 1: Create registration certificate for the staking script
-
 # We also create collateral.
 
-txin=$(jq -r 'keys[]' "$WORK/utxo-2.json")
-lovelaceattxin=$(jq -r ".[\"$txin\"].value.lovelace" "$WORK/utxo-2.json")
-lovelaceattxindiv3=$((lovelaceattxin / 3))
+txin2=$(jq -r 'keys[0]' "$WORK/utxo-2.json")
+lovelaceattxin2=$(jq -r ".[\"$txin2\"].value.lovelace" "$WORK/utxo-2.json")
+lovelaceattxin2div3=$(($lovelaceattxin2 / 3))
 
 cardano-cli stake-address registration-certificate \
-  --stake-script-file "scripts/plutus/scripts/$PV/guess-42-stake.plutus" \
+  --stake-script-file "$certifyingscript" \
   --out-file "$WORK/script.regcert"
 
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic "$TESTNET_MAGIC" \
   --change-address "$utxoaddr" \
-  --tx-in "$txin" \
+  --tx-in "$txin2" \
   --tx-out "$scriptpaymentaddrwithstakecred+999978" \
-  --tx-out "$utxoaddr+$lovelaceattxindiv3" \
+  --tx-out "$utxoaddr+$lovelaceattxin2div3" \
   --witness-override 3 \
   --certificate-file "$WORK/script.regcert" \
   --out-file "$WORK/script-registration-cert.txbody"
@@ -105,7 +130,7 @@ cardano-cli transaction submit \
   --tx-file "$WORK/script-registration-cert.tx" \
   --testnet-magic "$TESTNET_MAGIC"
 
-stakingscriptaddr=$(cardano-cli stake-address build --stake-script-file scripts/plutus/scripts/$PV/guess-42-stake.plutus --testnet-magic 42)
+stakingscriptaddr=$(cardano-cli stake-address build --stake-script-file $certifyingscript --testnet-magic 42)
 
 echo ""
 echo "Staking script address"
@@ -126,12 +151,18 @@ echo "$registeredscr"
 
 # We need to delegate the script staking address
 
+# Retrieve plutus reference script tx in
+cardano-cli query utxo --address "$dummyaddress" --cardano-mode --testnet-magic "$TESTNET_MAGIC" --out-file $WORK/dummy-address-ref-script.json
+cat $WORK/dummy-address-ref-script.json
+# Get reference script txin
+plutusreferencescripttxin=$(jq -r 'keys[0]' $WORK/dummy-address-ref-script.json)
+
+
 cardano-cli stake-address delegation-certificate \
-  --stake-script-file "scripts/plutus/scripts/$PV/guess-42-stake.plutus" \
+  --stake-script-file "$certifyingscript" \
   --cold-verification-key-file "$poolcoldkey" \
   --out-file "$WORK/script.delegcert"
 
-cardano-cli query protocol-parameters --testnet-magic "$TESTNET_MAGIC" --out-file "$WORK/pparams.json"
 
 # We also need collateral
 
@@ -146,7 +177,7 @@ cat "$WORK/utxo-2.json"
 txinupdated3=$(jq -r 'keys[0]' "$WORK/utxo-2.json")
 txincollateral=$(jq -r 'keys[1]' "$WORK/utxo-2.json")
 echo ""
-echo "Selected txin: $txinupdated2"
+echo "Selected txin: $txinupdated3"
 
 cardano-cli transaction build \
   --babbage-era \
@@ -157,8 +188,9 @@ cardano-cli transaction build \
   --tx-out "$scriptpaymentaddrwithstakecred+999978" \
   --witness-override 3 \
   --certificate-file "$WORK/script.delegcert" \
-  --certificate-script-file "scripts/plutus/scripts/$PV/guess-42-stake.plutus" \
-  --certificate-redeemer-file "scripts/plutus/data/42.redeemer" \
+  --certificate-tx-in-reference "$plutusreferencescripttxin" \
+  --certificate-plutus-script-v2 \
+  --certificate-reference-tx-in-redeemer-file "scripts/plutus/data/42.redeemer" \
   --protocol-params-file "$WORK/pparams.json" \
   --out-file "$WORK/script-delegation-cert.txbody"
 
@@ -195,7 +227,7 @@ echo "$delegatedscript"
 echo ""
 echo "Staking script payment address"
 echo "$scriptpaymentaddrwithstakecred"
-echoi ""
+echo ""
 echo "If a staking script address is displayed after this message, the registration of the Plutus staking script was successful"
 echo "$stakingscriptaddr"
 echo ""
