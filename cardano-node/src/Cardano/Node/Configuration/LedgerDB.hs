8aa86e590256bb1fde4f4f89c97b8bf8a6f63c28
Joris Dral <joris@well-typed.com>
2022-06-21 13:29:06 +0200

    Configure default LMDB limits.
    Changes include:
    * The default LMDB limits are now included in the node configuration. A
    custom `lmdbMapSize` can still be set through manual configuration.
    * Make `--lmdb-mapsize` CLI flag optional. If the flag is
    omitted, the node will uses a default mapsize value that should be
    sufficient for a node running production.

--- cardano-node/src/Cardano/Node/Configuration/LedgerDB.hs
+++ cardano-node/src/Cardano/Node/Configuration/LedgerDB.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Cardano.Node.Configuration.LedgerDB (
     BackingStoreSelectorFlag(..)
+  , defaultLMDBLimits
   ) where
 
 import           Prelude
+
+import           Ouroboros.Consensus.Storage.LedgerDB.HD.LMDB (LMDBLimits (..))
 
 -- | Choose the LedgerDB Backend
 --
@@ -20,3 +25,26 @@ data BackingStoreSelectorFlag =
                      -- default of 12Gi will be used.
   | InMemory
   deriving (Eq, Show)
+
+-- | Recommended settings for the LMDB backing store.
+--
+-- The default @'LMDBLimits'@ uses an @'lmdbMapSize'@ of @16_000_000_000@
+-- bytes, or 16 GigaBytes. @'lmdbMapSize'@ sets the size of the memory map
+-- that is used internally by the LMDB backing store, and is also the
+-- maximum size of the on-disk database. 16 GB should be sufficient for the
+-- medium term, i.e., it is sufficient until a more performant alternative to
+-- the LMDB backing store is implemented, which will probably replace the LMDB
+-- backing store altogether.
+--
+-- Note(jdral): It is recommended not to set the @'lmdbMapSize'@ to a value
+-- that is much smaller than 16 GB through manual configuration: the node will
+-- die with a fatal error as soon as the database size exceeds the
+-- @'lmdbMapSize'@. If this fatal error were to occur, we would expect that
+-- the node can continue normal operation if it is restarted with a higher
+-- @'lmdbMapSize'@ configured. Nonetheless, this situation should be avoided.
+defaultLMDBLimits :: LMDBLimits
+defaultLMDBLimits = LMDBLimits {
+    lmdbMapSize = 16_000_000_000
+  , lmdbMaxDatabases = 10
+  , lmdbMaxReaders = 16
+  }
