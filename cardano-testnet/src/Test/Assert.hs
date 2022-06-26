de31768a24749d70315f9a9667e4c540d26d6bc5
John Ky <john.ky@iohk.io>
2022-06-26 10:40:37 +1000

    New Alonzo leadership schedule test

--- cardano-testnet/src/Test/Assert.hs
+++ cardano-testnet/src/Test/Assert.hs
@@ -1,23 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
 
+{- HLINT ignore "Redundant return" -}
+
 module Test.Assert
   ( readJsonLines
   , assertChainExtended
+  , getRelevantLeaderSlots
   ) where
 
-import           Control.Lens ((^.))
+import           Control.Lens (to, (^.), (^?))
 import           Control.Monad (Monad (..))
 import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Trans.Reader (ReaderT)
+import           Control.Monad.Trans.Resource (ResourceT)
 import           Data.Aeson (Value)
 import           Data.Bool (Bool (..))
 import           Data.Eq (Eq (..))
 import           Data.Function (($), (.))
 import           Data.Functor ((<$>))
+import           Data.Int (Int)
 import           Data.Maybe (mapMaybe)
+import           Data.Ord (Ord (..))
 import           Data.Word (Word8)
 import           GHC.Stack (HasCallStack)
 import           Hedgehog (MonadTest)
+import           Hedgehog.Extras.Internal.Test.Integration (IntegrationState)
+import           Prelude (fromIntegral)
 import           System.FilePath (FilePath)
 import           System.IO (IO)
 import           Test.Runtime (NodeLoggingFormat (..))
@@ -26,9 +35,11 @@ import qualified Data.Aeson as J
 import qualified Data.Aeson.Lens as J
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.List as L
+import qualified Data.Maybe as Maybe
 import qualified Data.Time.Clock as DTC
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Stock.IO.File as IO
+import qualified Hedgehog.Extras.Test.Base as H
 import qualified Test.Process as H
 
 newlineBytes :: Word8
@@ -54,3 +65,14 @@ assertChainExtended deadline nodeLoggingFormat nodeStdoutFile =
       NodeLoggingFormatAsText -> IO.fileContains "Chain extended, new tip" nodeStdoutFile
       NodeLoggingFormatAsJson -> fileJsonGrep nodeStdoutFile (\v -> v ^. J.key "data" . J.key "kind" . J._String == "")
 
+getRelevantLeaderSlots :: FilePath -> Int -> H.PropertyT (ReaderT IntegrationState (ResourceT IO)) [Int]
+getRelevantLeaderSlots poolNodeStdoutFile slotLowerBound = do
+  vs <- readJsonLines poolNodeStdoutFile
+  leaderSlots <- H.noteShow
+    $ Maybe.mapMaybe (\v -> v ^? J.key "data" . J.key "val" . J.key "slot" . J._Integer. to fromIntegral)
+    $ L.filter       (\v -> v ^. J.key "data" . J.key "val" . J.key "kind" . J._String == "TraceNodeIsLeader")
+    vs
+  relevantLeaderSlots <- H.noteShow
+    $ L.filter       (>= slotLowerBound)
+    leaderSlots
+  return relevantLeaderSlots
