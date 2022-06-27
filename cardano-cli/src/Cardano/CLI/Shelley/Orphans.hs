4590123edf25d1e17222c8cdffe216f24ce83c16
John Ky <john.ky@iohk.io>
2022-06-28 01:33:56 +1000

    Fix query protocol-state

--- cardano-cli/src/Cardano/CLI/Shelley/Orphans.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Orphans.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -13,38 +14,37 @@
 
 module Cardano.CLI.Shelley.Orphans () where
 
-import           Cardano.Prelude
-
-import           Control.SetAlgebra as SetAlgebra
-import           Data.Aeson
+import           Cardano.Api.Orphans ()
+import           Cardano.Ledger.AuxiliaryData (AuxiliaryDataHash (..))
+import qualified Cardano.Ledger.Credential as Ledger
+import qualified Cardano.Ledger.Crypto as CC (Crypto)
+import qualified Cardano.Ledger.Mary.Value as Ledger.Mary
+import           Cardano.Ledger.PoolDistr (PoolDistr (..))
+import qualified Cardano.Ledger.Shelley.EpochBoundary as Ledger
+import qualified Cardano.Ledger.Shelley.PoolRank as Ledger
+import           Cardano.Ledger.TxIn (TxId (..))
+import           Cardano.Prelude (Bool(True), Category((.)))
+import qualified Cardano.Protocol.TPraos.API as Ledger
+import           Cardano.Protocol.TPraos.BHeader (HashHeader (..))
+import qualified Cardano.Protocol.TPraos.Rules.Prtcl as Ledger
+import qualified Cardano.Protocol.TPraos.Rules.Tickn as Ledger
+import qualified Cardano.Slotting.Slot as Cardano
+import qualified Control.SetAlgebra as SetAlgebra (BiMap, forwards)
+import           Data.Aeson (FromJSON(..), KeyValue((.=)), ToJSON(..), ToJSONKey)
+import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Short as SBS
 import qualified Data.Text.Encoding as Text
-
-import           Cardano.Api.Orphans ()
-
+import qualified Data.VMap as VMap
 import           Ouroboros.Consensus.Byron.Ledger.Block (ByronHash (..))
 import           Ouroboros.Consensus.HardFork.Combinator (OneEraHash (..))
+import           Ouroboros.Consensus.Protocol.Praos (PraosState)
+import qualified Ouroboros.Consensus.Protocol.Praos as Consensus
+import           Ouroboros.Consensus.Protocol.TPraos (TPraosState)
+import qualified Ouroboros.Consensus.Protocol.TPraos as Consensus
 import           Ouroboros.Consensus.Shelley.Eras (StandardCrypto)
 import           Ouroboros.Consensus.Shelley.Ledger.Block (ShelleyHash (..))
 import           Ouroboros.Network.Block (BlockNo (..), HeaderHash, Tip (..))
-
-import           Cardano.Ledger.AuxiliaryData (AuxiliaryDataHash (..))
-import qualified Cardano.Ledger.Crypto as CC (Crypto)
-import           Cardano.Ledger.PoolDistr (PoolDistr (..))
-import           Cardano.Protocol.TPraos.BHeader (HashHeader (..))
-
-import qualified Cardano.Ledger.Credential as Ledger
-import qualified Cardano.Protocol.TPraos.API as Ledger
-import qualified Cardano.Ledger.Shelley.EpochBoundary as Ledger
-import           Cardano.Ledger.TxIn (TxId (..))
-import qualified Cardano.Protocol.TPraos.Rules.Prtcl as Ledger
-import qualified Cardano.Protocol.TPraos.Rules.Tickn as Ledger
-
-import qualified Cardano.Ledger.Mary.Value as Ledger.Mary
-
-import qualified Data.VMap as VMap
-import qualified Cardano.Ledger.Shelley.PoolRank as Ledger
 
 instance ToJSON (OneEraHash xs) where
   toJSON = toJSON
@@ -58,9 +58,9 @@ deriving newtype instance ToJSON ByronHash
 -- This instance is temporarily duplicated in cardano-config
 
 instance ToJSON (HeaderHash blk) => ToJSON (Tip blk) where
-  toJSON TipGenesis = object [ "genesis" .= True ]
+  toJSON TipGenesis = Aeson.object [ "genesis" .= True ]
   toJSON (Tip slotNo headerHash blockNo) =
-    object
+    Aeson.object
       [ "slotNo"     .= slotNo
       , "headerHash" .= headerHash
       , "blockNo"    .= blockNo
@@ -94,3 +94,26 @@ deriving newtype  instance ToJSON    (Ledger.Mary.PolicyID StandardCrypto)
 
 instance (ToJSONKey k, ToJSON v) => ToJSON (SetAlgebra.BiMap v k v) where
   toJSON = toJSON . SetAlgebra.forwards -- to normal Map
+
+instance ToJSON (TPraosState StandardCrypto) where
+  toJSON s = Aeson.object
+    [ "lastSlot" .= Consensus.tpraosStateLastSlot s
+    , "chainDepState" .= Consensus.tpraosStateChainDepState s
+    ]
+
+instance ToJSON (PraosState StandardCrypto) where
+  toJSON s = Aeson.object
+    [ "lastSlot" .= Consensus.praosStateLastSlot s
+    , "oCertCounters" .= Consensus.praosStateOCertCounters s
+    , "evolvingNonce" .= Consensus.praosStateEvolvingNonce s
+    , "candidateNonce" .= Consensus.praosStateCandidateNonce s
+    , "epochNonce" .= Consensus.praosStateEpochNonce s
+    , "labNonce" .= Consensus.praosStateLabNonce s
+    , "lastEpochBlockNonce" .= Consensus.praosStateLastEpochBlockNonce s
+    ]
+
+
+instance ToJSON (Cardano.WithOrigin Cardano.SlotNo) where
+  toJSON = \case
+    Cardano.Origin -> Aeson.String "origin"
+    Cardano.At (Cardano.SlotNo n) -> toJSON n
