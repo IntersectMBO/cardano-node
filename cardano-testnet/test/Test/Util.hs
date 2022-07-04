1126b3ce71362ef329b4b8744de5865de60dd29a
John Ky <john.ky@iohk.io>
2022-07-04 10:12:26 +1000

    Disable kes-period-info test with code rather than comments

--- cardano-testnet/test/Test/Util.hs
+++ cardano-testnet/test/Test/Util.hs
@@ -3,18 +3,20 @@ module Test.Util
   , ignoreOnWindows
   , ignoreOnMac
   , ignoreOnMacAndWindows
+  , disabled
   ) where
 
 import           Data.Bool (bool)
+import           Data.String (IsString (..))
 import           Hedgehog (Property)
 import           Hedgehog.Extras.Stock.OS (isWin32)
 import           Prelude
 import           Test.Tasty.ExpectedFailure (wrapTest)
 import           Test.Tasty.Providers (testPassed)
-import           Test.Tasty.Runners (TestTree, Result(resultShortDescription))
+import           Test.Tasty.Runners (Result (resultShortDescription), TestTree)
 
-import qualified Test.Tasty.Hedgehog as H
 import qualified System.Info as SYS
+import qualified Test.Tasty.Hedgehog as H
 
 type Os = String
 
@@ -38,3 +40,6 @@ ignoreOn os = wrapTest $ const $ return $
   (testPassed ("IGNORED on " <> os))
     { resultShortDescription = "IGNORED on " <> os
     }
+
+disabled :: String -> Property -> TestTree
+disabled pName prop = ignoreOn "Disabled" $ H.testPropertyNamed pName (fromString pName) prop
