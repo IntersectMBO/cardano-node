ffc035797dae215df0f86b7dc754cbf2f4bc480e
Michael Karg <michael.karg@iohk.io>
2022-08-31 17:34:01 +0200

    tx-generator: migrate ADTs (WIP4)

--- bench/tx-generator/src/Cardano/TxGenerator/Fund.hs
+++ bench/tx-generator/src/Cardano/TxGenerator/Fund.hs
@@ -50,7 +50,7 @@ getFundLovelace (Fund (InAnyCardanoEra _ a)) = case _fundVal a of
   TxOutAdaOnly _era l -> l
   TxOutValue _era v -> selectLovelace v
 
--- TODO: facilitate casting KeyWitnesses from one era to an other
+-- TODO: facilitate casting KeyWitnesses between eras -- Note [Era transitions]
 getFundWitness :: forall era. IsShelleyBasedEra era => Fund -> Witness WitCtxTxIn era
 getFundWitness fund = case (cardanoEra @ era, fund) of
   (ByronEra   , Fund (InAnyCardanoEra ByronEra   a)) -> _fundWitness a
@@ -60,7 +60,13 @@ getFundWitness fund = case (cardanoEra @ era, fund) of
   (AlonzoEra  , Fund (InAnyCardanoEra AlonzoEra  a)) -> _fundWitness a
   (BabbageEra , Fund (InAnyCardanoEra BabbageEra a)) -> _fundWitness a
   _                                                  -> error "getFundWitness: era mismatch"
--- background: getFundWitness *must* be partial
--- (e.g. it makes no sense to spend funds in an earlier era that doesn't support a specific feature);
--- it's implemented here simply by enforcing that both eras are the same. However, that is too constrained.
--- This approach rules out era transitions for transactions, which is a valid case.
+
+{-
+Note [Era transitions]
+~~~~~~~~~~~~~~~~~~~~~~
+getFundWitness *must* be a partial function since it makes no sense to spend funds in an earlier era that doesn't
+support a specific feature). Currently, it's implemented here simply by enforcing via the type system a fund witness
+only exists in the same era as the fund.
+However, that is too constrained: this approach rules out era transitions for transactions, which is 1) a valid case
+and 2) a potential use case for benchmarking.
+-}
