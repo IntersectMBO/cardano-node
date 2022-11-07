6cf921148896aee95c5f1d8e16f238592767fe1c
Joosep Jääger <joosep.jaager@iohk.io>
2022-11-07 13:42:13 +0200

    Copied changes from #4600

--- cardano-node/src/Cardano/Node/Run.hs
+++ cardano-node/src/Cardano/Node/Run.hs
@@ -622,10 +622,8 @@ mkP2PArguments NodeConfiguration {
       , P2P.daReadLocalRootPeers
       , P2P.daReadPublicRootPeers
       , P2P.daReadUseLedgerAfter
-      , P2P.daProtocolIdleTimeout   = ncProtocolIdleTimeout
-      , P2P.daTimeWaitTimeout       = ncTimeWaitTimeout
-      , P2P.daDeadlineChurnInterval = 3300
-      , P2P.daBulkChurnInterval     = 300
+      , P2P.daProtocolIdleTimeout = ncProtocolIdleTimeout
+      , P2P.daTimeWaitTimeout     = ncTimeWaitTimeout
       }
   where
     daPeerSelectionTargets = PeerSelectionTargets {
