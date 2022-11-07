9ea396a4e19e4ef57c59563c5a4c36b8a27165c7
Joosep Jääger <joosep.jaager@iohk.io>
2022-11-07 13:42:13 +0200

    Copied changes from #4600

--- cardano-api/src/Cardano/Api/Script.hs
+++ cardano-api/src/Cardano/Api/Script.hs
@@ -129,7 +129,6 @@ import           Numeric.Natural (Natural)
 import           Data.Aeson (Value (..), object, (.:), (.=))
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
-import qualified Data.Sequence.Strict as Seq
 import           Data.Vector (Vector)
 import qualified Data.Vector as Vector
 
@@ -169,6 +168,7 @@ import           Cardano.Api.SerialiseTextEnvelope
 import           Cardano.Api.SerialiseUsing
 import           Cardano.Api.TxIn
 import           Cardano.Api.Utils (failEitherWith)
+import qualified Data.Sequence.Strict as Seq
 
 -- ----------------------------------------------------------------------------
 -- Types for script language and version
