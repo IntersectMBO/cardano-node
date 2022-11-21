72c0e89b582973d8627c3a91e1b116a4ceccd70c
Moritz Angermann <moritz.angermann@gmail.com>
2022-11-21 12:18:38 +0800

    Create build-secp256k1.bash

---
+++ .github/workflows/build-secp256k1.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# I don't understand why this just vanishes.
+export PATH=/usr/bin:$PATH
+
+echo ========
+echo $PATH
+echo ========
+git clone https://github.com/bitcoin-core/secp256k1
+cd secp256k1
+git switch $SECP256K1_REF --detach
+./autogen.sh
+./configure $CI_SECP_FLAGS --enable-module-schnorrsig --enable-experimental
+make
+make check
+$CI_SECP_INSTALL_CMD make install
+cd ..
