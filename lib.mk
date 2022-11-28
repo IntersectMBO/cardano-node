0c11ffd1e0faa8ef406e20f29d2424da395797ce
Kosyrev Serge <serge.kosyrev@iohk.io>
2022-11-28 22:15:28 +0800

    Makefile: define workbench targets with the nomad backend

--- lib.mk
+++ lib.mk
@@ -1,23 +1,24 @@
-## define_proftarget :: target -> profile -> fullnixmode -> devmode -> autostart -> autostay -> profiled -> IO ()
-define define_profile_target
+## proftgt :: target -> profile -> fullnixmode -> devmode -> autostart -> autostay -> profiled -> backend -> IO ()
+define proftgt
 $(1): shell
 $(1): PROFILE = $(2)-${ERA}
-ifeq ($(3),true)
+$(1): BACKEND = $(8)
+ifeq ($(strip $(3)),true)
 $(1): ARGS += --arg 'useCabalRun' false
 endif
-ifeq ($(4),true)
+ifeq ($(strip $(4)),true)
 $(1): ARGS += --arg 'workbenchDevMode' true
 endif
-ifeq ($(7),true)
+ifeq ($(strip $(7)),true)
 $(1): ARGS += --arg 'profiled' true
 endif
-ifeq ($(5)$(6),truetrue)
+ifeq ($(strip $(5))$(strip $(6)),truetrue)
 $(1): CMD := start-cluster $(if ${ITER},--iterations ${ITER}); return
 endif
-ifeq ($(5)$(6),truefalse)
+ifeq ($(strip $(5))$(strip $(6)),truefalse)
 $(1): RUN := start-cluster $(if ${ITER},--iterations ${ITER})
 endif
-ifeq ($(3)$(4)$(5)$(6),falsetruefalsefalse)
+ifeq ($(strip $(3))$(strip $(4))$(strip $(5))$(strip $(6)),falsetruefalsefalse)
 define EXTRA_HELP +=
 $(1): ## Shell for profile \033[34m$(2)\033[0m  (also: \033[34m-auto -autostay -nix -autonix -prof\033[0m)\n
 endef
@@ -26,10 +27,12 @@ endif
 endef
 
 define define_profile_targets
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof),$$(prof),false,true,false,false,false)))
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof)-prof,$$(prof),false,true,false,false,true)))
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof)-auto,$$(prof),false,true,true,false,false)))
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof)-autostay,$$(prof),false,true,true,true,false)))
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof)-nix,$$(prof),true,false,false,false,false)))
-$$(foreach prof,$(1),$$(eval $$(call define_profile_target,$$(prof)-autonix,$$(prof),true,false,true,false,false)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof),           $$(prof),false, true,false,false,false, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-prof,      $$(prof),false, true,false,false, true, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-auto,      $$(prof),false, true, true,false,false, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-autostay,  $$(prof),false, true, true, true,false, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-nix,       $$(prof), true,false,false,false,false, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-autonix,   $$(prof), true,false, true,false,false, supervisor)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-nomad,     $$(prof), true,false,false,false,false, nomad)))
+$$(foreach prof,$(1),$$(eval $$(call proftgt,$$(prof)-autonomad, $$(prof), true,false, true,false,false, nomad)))
 endef
