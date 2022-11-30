f952850304ffd4931560ef56313fac1caf050d0c
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-11-30 09:31:35 +0100

    Export fromConsensusPointHF
    As toConsensusPointHF is also exported

--- cardano-api/src/Cardano/Api/Shelley.hs
+++ cardano-api/src/Cardano/Api/Shelley.hs
@@ -98,6 +98,7 @@ module Cardano.Api.Shelley
     toConsensusBlock,
     fromConsensusTip,
     fromConsensusPointInMode,
+    fromConsensusPointHF,
     toConsensusPointInMode,
     toConsensusPointHF,
 
