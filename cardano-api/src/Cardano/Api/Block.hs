3b01002f548a7fdbe847c7997cdcba20edb75c90
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-11-30 12:23:38 +0100

    Add LambdaCase pragma

--- cardano-api/src/Cardano/Api/Block.hs
+++ cardano-api/src/Cardano/Api/Block.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
