9487d1b240c3ce36afb95346433cc3aa9c5b8dd3
Jordan Millar <jordan.millar@iohk.io>
2022-12-14 13:16:04 -0400

    Remove Shelley era chairman test

--- cardano-node-chairman/test/Main.hs
+++ cardano-node-chairman/test/Main.hs
@@ -13,21 +13,18 @@ import qualified Test.Tasty.Hedgehog as H
 import qualified Test.Tasty.Ingredients as T
 
 import qualified Spec.Chairman.Cardano
-import qualified Spec.Chairman.Shelley
 import qualified Spec.Network
 
 tests :: IO T.TestTree
 tests = do
   let t0 = H.testPropertyNamed "isPortOpen False" (fromString "isPortOpen False") Spec.Network.hprop_isPortOpen_False
   let t1 = H.testPropertyNamed "isPortOpen True"  (fromString "isPortOpen True" ) Spec.Network.hprop_isPortOpen_True
-  let t3 = H.testPropertyNamed "chairman"         (fromString "chairman"        ) Spec.Chairman.Cardano.hprop_chairman
-  let t4 = H.testPropertyNamed "chairman"         (fromString "chairman"        ) Spec.Chairman.Shelley.hprop_chairman
+  let t2 = H.testPropertyNamed "chairman"         (fromString "chairman"        ) Spec.Chairman.Cardano.hprop_chairman
 
   pure $ T.testGroup "test/Spec.hs"
     [ T.testGroup "Spec"
       [ T.testGroup "Chairman"
-        [ T.testGroup "Cardano" [t3]
-        , T.testGroup "Shelley" [t4]
+        [ T.testGroup "Cardano" [t2]
         ]
       , T.testGroup "Network" [t0, t1]
       ]
