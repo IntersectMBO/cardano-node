32a3fe145aaea91560d3523d7c2c40fc992d6750
Kosyrev Serge <serge.kosyrev@iohk.io>
2023-01-21 04:06:03 +0800

    workbench:  relativise rundirs

--- nix/workbench/run.sh
+++ nix/workbench/run.sh
@@ -91,7 +91,7 @@ local op=${1:-$run_default_op}; test $# -gt 0 && shift
 
 case "$op" in
     get-rundir )
-        echo $global_rundir;;
+        realpath --relative-to "$(pwd)" "$global_rundir";;
 
     list-runs | runs | lsr )
         local usage="USAGE: wb run $op [--remote | -r]"
@@ -211,7 +211,7 @@ case "$op" in
     describe )
         local usage="USAGE: wb run $op RUN"
         local run=${1:?$usage}
-        local dir=$global_rundir/$run
+        local dir=$(run get $run)
 
         if ! run check "$run"
         then fatal "run fails sanity checks:  $run at $dir"; fi
@@ -271,7 +271,7 @@ EOF
     compute-path )
         if test -f "$1/meta.json"
         then echo -n "$1"
-        else echo -n "$global_rundir/$1"
+        else realpath --relative-to "$(pwd)" "$global_rundir/$1"
         fi;;
 
     check )
@@ -356,7 +356,7 @@ EOF
         ln -s $run "$global_rundir"/current;;
 
     current-run-path | current-path | path )
-        realpath "$global_rundir"/current;;
+        realpath --relative-to "$(pwd)" "$global_rundir"/current;;
 
     current-run-tag | current-tag | tag | current )
         basename "$(run current-path)";;
@@ -405,7 +405,7 @@ EOF
         progress "run | tag" "allocated run identifier (tag):  $(with_color white $run)"
 
         ## 4. allocate directory:
-        local dir=$global_rundir/$run
+        local dir=$(realpath --relative-to "$(pwd)" "$global_rundir/$run")
         local realdir=$(realpath --canonicalize-missing "$dir")
 
         test "$(dirname "$realdir")" = "$(realpath "$global_rundir")" ||
