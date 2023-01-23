e39075d1498ead461c5a55c37ebdb8b76fd7baf0
John Ky <john.ky@iohk.io>
2023-01-22 23:46:58 -0800

    Use oops to implement queryNodeLocalState.  The implementation is in queryNodeLocalState_ and queryNodeLocalState is rewritten to delegate to queryNodeLocalState_.

--- cardano-api/src/Cardano/Api/IPC/Monad.hs
+++ cardano-api/src/Cardano/Api/IPC/Monad.hs
@@ -1,20 +1,29 @@
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Cardano.Api.IPC.Monad
   ( LocalStateQueryExpr
   , executeLocalStateQueryExpr
+  , executeLocalStateQueryExpr_
   , queryExpr
   , determineEraExpr
+
+  , NodeToClientVersionOf (..)
   ) where
 
 import           Control.Concurrent.STM
 import           Control.Monad
 import           Control.Monad.IO.Class
+import           Control.Monad.Oops (CouldBe, Variant, runOopsInEither)
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Cont
 import           Control.Monad.Trans.Except (ExceptT (..), runExceptT)
+import qualified Data.Variant as DV
 
 import           Cardano.Ledger.Shelley.Scripts ()
 import qualified Ouroboros.Network.Protocol.LocalStateQuery.Client as Net.Query
@@ -50,44 +59,56 @@ executeLocalStateQueryExpr
   -> Maybe ChainPoint
   -> LocalStateQueryExpr (BlockInMode mode) ChainPoint (QueryInMode mode) () IO a
   -> IO (Either AcquiringFailure a)
-executeLocalStateQueryExpr connectInfo mpoint f = do
-  tmvResultLocalState <- newEmptyTMVarIO
+executeLocalStateQueryExpr connectInfo mpoint f =
+  runOopsInEither $ executeLocalStateQueryExpr_ connectInfo mpoint (lift f)
+
+-- | Execute a local state query expression.
+executeLocalStateQueryExpr_
+  :: forall e mode a . ()
+  => e `CouldBe` AcquiringFailure
+  => LocalNodeConnectInfo mode
+  -> Maybe ChainPoint
+  -> ExceptT (Variant e) (LocalStateQueryExpr (BlockInMode mode) ChainPoint (QueryInMode mode) () IO) a
+  -> ExceptT (Variant e) IO a
+executeLocalStateQueryExpr_ connectInfo mpoint f = do
+  tmvResultLocalState <- liftIO (newEmptyTMVarIO @(Either (Variant e) a))
   let waitResult = readTMVar tmvResultLocalState
 
-  connectToLocalNodeWithVersion
+  liftIO $ connectToLocalNodeWithVersion
     connectInfo
     (\ntcVersion ->
       LocalNodeClientProtocols
       { localChainSyncClient    = NoLocalChainSyncClient
-      , localStateQueryClient   = Just $ setupLocalStateQueryExpr waitResult mpoint tmvResultLocalState ntcVersion f
+      , localStateQueryClient   = Just $ setupLocalStateQueryExpr_ waitResult mpoint tmvResultLocalState ntcVersion f
       , localTxSubmissionClient = Nothing
       , localTxMonitoringClient = Nothing
       }
     )
 
-  atomically waitResult
+  ExceptT . return =<< liftIO (atomically waitResult)
 
 -- | Use 'queryExpr' in a do block to construct monadic local state queries.
-setupLocalStateQueryExpr ::
-     STM x
+setupLocalStateQueryExpr_ :: ()
+  => e `CouldBe` AcquiringFailure
+  => STM x
      -- ^ An STM expression that only returns when all protocols are complete.
      -- Protocols must wait until 'waitDone' returns because premature exit will
      -- cause other incomplete protocols to abort which may lead to deadlock.
   -> Maybe ChainPoint
-  -> TMVar (Either AcquiringFailure a)
+  -> TMVar (Either (Variant e) a)
   -> NodeToClientVersion
-  -> LocalStateQueryExpr (BlockInMode mode) ChainPoint (QueryInMode mode) () IO a
+  -> ExceptT (Variant e) (LocalStateQueryExpr (BlockInMode mode) ChainPoint (QueryInMode mode) () IO) a
   -> Net.Query.LocalStateQueryClient (BlockInMode mode) ChainPoint (QueryInMode mode) IO ()
-setupLocalStateQueryExpr waitDone mPointVar' resultVar' ntcVersion f =
+setupLocalStateQueryExpr_ waitDone mPointVar' resultVar' ntcVersion f =
   LocalStateQueryClient . pure . Net.Query.SendMsgAcquire mPointVar' $
     Net.Query.ClientStAcquiring
-    { Net.Query.recvMsgAcquired = runContT (runReaderT (runLocalStateQueryExpr f) ntcVersion) $ \result -> do
-        atomically $ putTMVar resultVar' (Right result)
+    { Net.Query.recvMsgAcquired = runContT (runReaderT (runLocalStateQueryExpr (runExceptT f)) ntcVersion) $ \result -> do
+        atomically $ putTMVar resultVar' result
         void $ atomically waitDone -- Wait for all protocols to complete before exiting.
         pure $ Net.Query.SendMsgRelease $ pure $ Net.Query.SendMsgDone ()
 
     , Net.Query.recvMsgFailure = \failure -> do
-        atomically $ putTMVar resultVar' (Left (toAcquiringFailure failure))
+        atomically $ putTMVar resultVar' (Left (DV.throw (toAcquiringFailure failure)))
         void $ atomically waitDone -- Wait for all protocols to complete before exiting.
         pure $ Net.Query.SendMsgDone ()
     }
