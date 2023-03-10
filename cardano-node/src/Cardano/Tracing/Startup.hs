2814303d4aad5fb273e61793b82da540b118d781
John Ky <john.ky@iohk.io>
2023-03-11 10:07:11 +1100

    Unrelated fixes

--- cardano-node/src/Cardano/Tracing/Startup.hs
+++ cardano-node/src/Cardano/Tracing/Startup.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Cardano.Tracing.Startup where
 
-import           Prelude
 import           Data.Text (Text)
+import           Prelude
 
 import           Cardano.Logging (LogFormatting (..))
 import           Cardano.Node.Startup
@@ -15,14 +15,12 @@ import           Cardano.Node.Tracing.Compat
 import           Cardano.Node.Tracing.Tracers.Startup
 import           Cardano.Tracing.OrphanInstances.Network ()
 
-import           Cardano.BM.Tracing (HasPrivacyAnnotation (..),
-                   HasSeverityAnnotation (..), Severity (..), ToObject (..),
-                   Transformable (..))
-import           Cardano.BM.Data.Tracer (HasTextFormatter (..),
-                   trStructuredText)
+import           Cardano.BM.Data.Tracer (HasTextFormatter (..), trStructuredText)
+import           Cardano.BM.Tracing (HasPrivacyAnnotation (..), HasSeverityAnnotation (..),
+                   Severity (..), ToObject (..), Transformable (..))
 
-import           Ouroboros.Consensus.Node.NetworkProtocolVersion
-                   (BlockNodeToClientVersion, BlockNodeToNodeVersion)
+import           Ouroboros.Consensus.Node.NetworkProtocolVersion (BlockNodeToClientVersion,
+                   BlockNodeToNodeVersion)
 
 
 instance HasSeverityAnnotation (StartupTrace blk) where
@@ -43,10 +41,7 @@ instance ( Show (BlockNodeToNodeVersion blk)
       => Transformable Text IO (StartupTrace blk) where
   trTransformer = trStructuredText
 
-instance ( Show (BlockNodeToNodeVersion blk)
-         , Show (BlockNodeToClientVersion blk)
-         )
-        => HasTextFormatter (StartupTrace blk) where
+instance HasTextFormatter (StartupTrace blk) where
   formatText a _ = ppStartupInfoTrace a
 
 instance ( Show (BlockNodeToNodeVersion blk)
