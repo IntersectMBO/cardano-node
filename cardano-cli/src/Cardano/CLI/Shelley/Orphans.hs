2a820957d3257197195b5233d000b0ea52197f9f
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2023-03-15 03:57:09 +0300

    Brought back TxMetadata serialization

--- cardano-cli/src/Cardano/CLI/Shelley/Orphans.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Orphans.hs
@@ -17,20 +17,17 @@ module Cardano.CLI.Shelley.Orphans () where
 import           Cardano.Api.Orphans ()
 import qualified Cardano.Ledger.AuxiliaryData as Ledger
 import qualified Cardano.Ledger.Credential as Ledger
-import           Cardano.Ledger.Conway.Genesis (ConwayGenesis (..))
+import qualified Cardano.Ledger.EpochBoundary as Ledger
 import qualified Cardano.Ledger.Crypto as CC (Crypto)
 import qualified Cardano.Ledger.Mary.Value as Ledger.Mary
 import qualified Cardano.Ledger.PoolDistr as Ledger
-import qualified Cardano.Ledger.Shelley.EpochBoundary as Ledger
 import qualified Cardano.Ledger.Shelley.PoolRank as Ledger
-import           Cardano.Ledger.TxIn (TxId (..))
 import qualified Cardano.Protocol.TPraos.API as Ledger
 import           Cardano.Protocol.TPraos.BHeader (HashHeader (..))
 import qualified Cardano.Protocol.TPraos.Rules.Prtcl as Ledger
 import qualified Cardano.Protocol.TPraos.Rules.Tickn as Ledger
 import qualified Cardano.Slotting.Slot as Cardano
-import qualified Control.SetAlgebra as SetAlgebra (BiMap, forwards)
-import           Data.Aeson (FromJSON (..), KeyValue ((.=)), ToJSON (..), ToJSONKey)
+import           Data.Aeson (FromJSON (..), KeyValue ((.=)), ToJSON (..))
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Short as SBS
@@ -73,8 +70,6 @@ deriving newtype instance FromJSON BlockNo
 -- Simple newtype wrappers JSON conversion
 --
 
-deriving newtype instance CC.Crypto crypto => ToJSON (TxId crypto)
-
 deriving newtype instance CC.Crypto crypto => ToJSON (ShelleyHash crypto)
 deriving newtype instance CC.Crypto crypto => ToJSON (HashHeader crypto)
 
@@ -91,9 +86,6 @@ deriving instance ToJSON Ledger.TicknState
 deriving instance ToJSON (Ledger.ChainDepState StandardCrypto)
 
 deriving newtype  instance ToJSON    (Ledger.Mary.PolicyID StandardCrypto)
-
-instance (ToJSONKey k, ToJSON v) => ToJSON (SetAlgebra.BiMap v k v) where
-  toJSON = toJSON . SetAlgebra.forwards -- to normal Map
 
 instance ToJSON (TPraosState StandardCrypto) where
   toJSON s = Aeson.object
@@ -117,8 +109,3 @@ instance ToJSON (Cardano.WithOrigin Cardano.SlotNo) where
   toJSON = \case
     Cardano.Origin -> Aeson.String "origin"
     Cardano.At (Cardano.SlotNo n) -> toJSON n
-
--- This instance should be exported from ledger but is currently not,
-instance CC.Crypto c => ToJSON (ConwayGenesis c) where
-  toJSON (ConwayGenesis genDelegs) =
-    Aeson.object ["genDelegs" .= toJSON genDelegs]
