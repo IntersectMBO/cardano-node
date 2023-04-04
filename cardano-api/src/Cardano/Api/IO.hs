971156d415290b5be2f3c420bf2cdcc5636a4c96
John Ky <john.ky@iohk.io>
2023-04-04 19:12:47 +1000

    Generalise writeLazyByteStringFileWithOwnerPermissions to handleFileForWritingWithOwnerPermission

--- cardano-api/src/Cardano/Api/IO.hs
+++ cardano-api/src/Cardano/Api/IO.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Api.IO
   ( OutputFile(..)
@@ -17,8 +19,25 @@ module Cardano.Api.IO
   , writeLazyByteStringFileWithOwnerPermissions
   ) where
 
+#if !defined(mingw32_HOST_OS)
+#define UNIX
+#endif
+
+#ifdef UNIX
+import           Control.Exception (IOException, bracket, bracketOnError, try)
+import           System.Directory ()
+import           System.IO (hClose)
+import           System.Posix.Files (ownerModes, setFdOwnerAndGroup)
+import           System.Posix.IO (OpenMode (..), closeFd, defaultFileFlags, fdToHandle, openFd)
+import           System.Posix.User (getRealUserID)
+#else
+import           Control.Exception (bracketOnError)
+import           System.Directory (removeFile, renameFile)
+import           System.FilePath (splitFileName, (<.>))
+import           System.IO (hClose, openTempFile)
+#endif
+
 import           Cardano.Api.Error (FileError (..))
-import           Cardano.Api.IO.Compat (writeLazyByteStringFileWithOwnerPermissions)
 
 import           Control.Monad.Except (runExceptT)
 import           Control.Monad.IO.Class (MonadIO (..))
@@ -33,6 +52,51 @@ import           Data.String (IsString)
 import           Data.Text (Text)
 import qualified Data.Text.IO as Text
 import           GHC.Generics (Generic)
+import           System.IO (Handle)
+
+handleFileForWritingWithOwnerPermission
+  :: FilePath
+  -> (Handle -> IO ())
+  -> IO (Either (FileError ()) ())
+handleFileForWritingWithOwnerPermission path f = do
+#ifdef UNIX
+  -- On a unix based system, we grab a file descriptor and set ourselves as owner.
+  -- Since we're holding the file descriptor at this point, we can be sure that
+  -- what we're about to write to is owned by us if an error didn't occur.
+  user <- getRealUserID
+  ownedFile <- try $
+    -- We only close the FD on error here, otherwise we let it leak out, since
+    -- it will be immediately turned into a Handle (which will be closed when
+    -- the Handle is closed)
+    bracketOnError
+      (openFd path WriteOnly (Just ownerModes) defaultFileFlags)
+      closeFd
+      (\fd -> setFdOwnerAndGroup fd user (-1) >> pure fd)
+  case ownedFile of
+    Left (err :: IOException) -> do
+      pure $ Left $ FileIOError path err
+    Right fd -> do
+      bracket
+        (fdToHandle fd)
+        hClose
+        (runExceptT . handleIOExceptT (FileIOError path) . f)
+#else
+  -- On something other than unix, we make a _new_ file, and since we created it,
+  -- we must own it. We then place it at the target location. Unfortunately this
+  -- won't work correctly with pseudo-files.
+  bracketOnError
+    (openTempFile targetDir $ targetFile <.> "tmp")
+    (\(tmpPath, h) -> do
+      hClose h >> removeFile tmpPath
+      return . Left $ FileErrorTempFile path tmpPath h)
+    (\(tmpPath, h) -> do
+        f h
+        hClose h
+        renameFile tmpPath path
+        return $ Right ())
+  where
+    (targetDir, targetFile) = splitFileName path
+#endif
 
 newtype OutputFile = OutputFile
   { unOutputFile :: FilePath
@@ -69,3 +133,11 @@ writeTextOutput mOutput t = runExceptT $
   case mOutput of
     Just fp -> handleIOExceptT (FileIOError fp) $ Text.writeFile fp t
     Nothing -> liftIO $ Text.putStr t
+
+writeLazyByteStringFileWithOwnerPermissions
+  :: FilePath
+  -> LBS.ByteString
+  -> IO (Either (FileError ()) ())
+writeLazyByteStringFileWithOwnerPermissions fp lbs =
+  handleFileForWritingWithOwnerPermission fp $ \h ->
+    LBS.hPut h lbs
