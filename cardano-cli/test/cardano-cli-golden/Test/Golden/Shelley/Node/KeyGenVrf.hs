aa6510b07f51737b85400535ef5366343d288564
John Ky <john.ky@iohk.io>
2023-04-05 20:37:10 +1000

    Test for new --key-format-output flag.

--- cardano-cli/test/cardano-cli-golden/Test/Golden/Shelley/Node/KeyGenVrf.hs
+++ cardano-cli/test/cardano-cli-golden/Test/Golden/Shelley/Node/KeyGenVrf.hs
@@ -2,6 +2,8 @@
 
 module Test.Golden.Shelley.Node.KeyGenVrf
   ( golden_shelleyNodeKeyGenVrf
+  , golden_shelleyNodeKeyGenVrf_bech32
+  , golden_shelleyNodeKeyGenVrf_te
   ) where
 
 import           Control.Monad (void)
@@ -29,3 +31,36 @@ golden_shelleyNodeKeyGenVrf = propertyOnce . H.moduleWorkspace "tmp" $ \tempDir 
 
   H.assertEndsWithSingleNewline verificationKey
   H.assertEndsWithSingleNewline signingKey
+
+golden_shelleyNodeKeyGenVrf_te :: Property
+golden_shelleyNodeKeyGenVrf_te = propertyOnce . H.moduleWorkspace "tmp" $ \tempDir -> do
+  verificationKey <- noteTempFile tempDir "kes.vkey"
+  signingKey <- noteTempFile tempDir "kes.skey"
+
+  void $ execCardanoCLI
+    [ "node","key-gen-VRF"
+    , "--key-output-format", "text-envelope"
+    , "--verification-key-file", verificationKey
+    , "--signing-key-file", signingKey
+    ]
+
+  H.assertFileOccurences 1 "VRF Verification Key" verificationKey
+  H.assertFileOccurences 1 "VRF Signing Key" signingKey
+
+  H.assertEndsWithSingleNewline verificationKey
+  H.assertEndsWithSingleNewline signingKey
+
+golden_shelleyNodeKeyGenVrf_bech32 :: Property
+golden_shelleyNodeKeyGenVrf_bech32 = propertyOnce . H.moduleWorkspace "tmp" $ \tempDir -> do
+  verificationKey <- noteTempFile tempDir "kes.vkey"
+  signingKey <- noteTempFile tempDir "kes.skey"
+
+  void $ execCardanoCLI
+    [ "node","key-gen-VRF"
+    , "--key-output-format", "bech32"
+    , "--verification-key-file", verificationKey
+    , "--signing-key-file", signingKey
+    ]
+
+  H.assertFileOccurences 1 "vrf_vk" verificationKey
+  H.assertFileOccurences 1 "vrf_sk" signingKey
