d6a1f638ab4fa772ee5109e0500b6dfdac3fa4db
KtorZ <matthias.benkort@gmail.com>
2023-04-06 16:13:38 +0200

    Write automated tests to cover newly introduced SPO on-chain poll commands
      Fixture keys were generated using the command-line itself. The set of tests cover quite extensively the various commands, as well as a few 'negative' test scenarios. It is more complicated to cover the interactive part of the 'answer-poll' command through those tests; and this is therefore left as manual test. Instructions for executing the sequence will also be provided with the introduction of the commands (e.g. in the description of [PR#5050](https://github.com/input-output-hk/cardano-node/pull/5050).

---
+++ cardano-cli/test/Test/Golden/Shelley/Governance/CreatePoll.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.Governance.CreatePoll
+  ( golden_shelleyGovernanceCreatePoll
+  , golden_shelleyGovernanceCreateLongPoll
+  ) where
+
+import           Control.Monad (void)
+import           Hedgehog (Property)
+import           Test.OptParse
+
+import qualified Hedgehog as H
+import qualified Hedgehog.Extras.Test.Base as H
+import qualified Hedgehog.Extras.Test.File as H
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyGovernanceCreatePoll :: Property
+golden_shelleyGovernanceCreatePoll =
+  propertyOnce . H.moduleWorkspace "tmp" $ \tempDir -> do
+    pollFile <- noteTempFile tempDir "poll.json"
+
+    stdout <- execCardanoCLI
+      [ "governance", "create-poll"
+      , "--question", "Pineapples on pizza?"
+      , "--answer", "yes"
+      , "--answer", "no"
+      , "--out-file", pollFile
+      ]
+
+    void $ H.readFile pollFile
+    noteInputFile "test/data/golden/shelley/governance/create.json"
+      >>= H.readFile
+      >>= (H.===) stdout
+    H.assertFileOccurences 1 "GovernancePoll" pollFile
+    H.assertEndsWithSingleNewline pollFile
+
+golden_shelleyGovernanceCreateLongPoll :: Property
+golden_shelleyGovernanceCreateLongPoll =
+  propertyOnce . H.moduleWorkspace "tmp" $ \tempDir -> do
+    pollFile <- noteTempFile tempDir "poll.json"
+
+    stdout <- execCardanoCLI
+      [ "governance", "create-poll"
+      , "--question", "What is the most adequate topping to put on a pizza (please consider all possibilities and take time to answer)?"
+      , "--answer", "pineapples"
+      , "--answer", "only traditional topics should go on a pizza, this isn't room for jokes"
+      , "--out-file", pollFile
+      ]
+
+    void $ H.readFile pollFile
+    noteInputFile "test/data/golden/shelley/governance/create-long.json"
+      >>= H.readFile
+      >>= (H.===) stdout
+    H.assertFileOccurences 1 "GovernancePoll" pollFile
+    H.assertEndsWithSingleNewline pollFile
