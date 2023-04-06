8ddeb6609f386daabc84fb13316bcc7461e603ca
KtorZ <matthias.benkort@gmail.com>
2023-04-06 16:13:38 +0200

    Write automated tests to cover newly introduced SPO on-chain poll commands
      Fixture keys were generated using the command-line itself. The set of tests cover quite extensively the various commands, as well as a few 'negative' test scenarios. It is more complicated to cover the interactive part of the 'answer-poll' command through those tests; and this is therefore left as manual test. Instructions for executing the sequence will also be provided with the introduction of the commands (e.g. in the description of [PR#5050](https://github.com/input-output-hk/cardano-node/pull/5050).

---
+++ cardano-cli/test/Test/Golden/Shelley/Governance/AnswerPoll.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.Governance.AnswerPoll
+  ( golden_shelleyGovernanceAnswerPollVrf
+  , golden_shelleyGovernanceAnswerPollCold
+  , golden_shelleyGovernanceAnswerPollInvalidAnswer
+  ) where
+
+import           Cardano.Prelude hiding (stdout)
+
+import           Hedgehog (Property)
+import           Test.OptParse
+
+import qualified Hedgehog as H
+import qualified Hedgehog.Extras.Test.File as H
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyGovernanceAnswerPollVrf :: Property
+golden_shelleyGovernanceAnswerPollVrf = propertyOnce $ do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/poll.json"
+  vrfKeyFile <- noteInputFile "test/data/golden/shelley/governance/vrf.sk"
+
+  stdout <- execCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--signing-key-file", vrfKeyFile
+    , "--answer", "0"
+    ]
+
+  noteInputFile "test/data/golden/shelley/governance/answer-vrf.json"
+    >>= H.readFile
+    >>= (H.===) stdout
+
+golden_shelleyGovernanceAnswerPollCold :: Property
+golden_shelleyGovernanceAnswerPollCold = propertyOnce $ do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/poll.json"
+  coldKeyFile <- noteInputFile "test/data/golden/shelley/governance/cold.sk"
+
+  stdout <- execCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--signing-key-file", coldKeyFile
+    , "--answer", "1"
+    ]
+
+  noteInputFile "test/data/golden/shelley/governance/answer-cold.json"
+    >>= H.readFile
+    >>= (H.===) stdout
+
+golden_shelleyGovernanceAnswerPollInvalidAnswer :: Property
+golden_shelleyGovernanceAnswerPollInvalidAnswer = propertyOnce $ do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/poll.json"
+  vrfKeyFile <- noteInputFile "test/data/golden/shelley/governance/vrf.sk"
+
+  result <- tryExecCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--signing-key-file", vrfKeyFile
+    , "--answer", "3"
+    ]
+
+  either (const H.success) (const H.failure) result
