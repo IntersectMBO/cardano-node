ded05979083757ebec34fe3b4aa7735daa8390e0
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2023-04-26 13:16:17 -0400

    Add option processing & help option.

--- bench/plutus-scripts-bench/app/gen-plutus.hs
+++ bench/plutus-scripts-bench/app/gen-plutus.hs
@@ -1,10 +1,37 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import Cardano.Benchmarking.PlutusScripts (findPlutusScript, encodePlutusScript)
-import qualified Data.ByteString.Lazy as LBS (putStr)
-import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy as LBS (hPut)
+import Options.Applicative
+import System.IO (IOMode(..), openFile, stdout)
+
+data Options = Options
+   { optOut :: Maybe FilePath
+   , optMod :: String
+   } deriving (Eq, Read, Show)
+
+opts :: Parser Options
+opts = Options
+       <$>
+        optional (strOption
+                   (  long "output"
+                   <> short 'o'
+                   <> metavar "FILE"
+                   <> help "Write output to FILE"
+                   )
+                 )
+       <*>
+        strArgument
+        (  metavar "SCRIPT"
+        <> help "Write SCRIPT to output"
+        )
 
 main :: IO ()
 main =
   do
-    a:_ <- getArgs
-    let Just s = findPlutusScript a
-    LBS.putStr $ encodePlutusScript s
+    Options {..} <- execParser (info opts fullDesc)
+    let Just s = findPlutusScript optMod
+    h <- case optOut of
+           Just file -> openFile file WriteMode
+           Nothing   -> pure stdout
+    LBS.hPut h $ encodePlutusScript s
