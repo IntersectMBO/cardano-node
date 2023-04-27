cdcfe3d58e6eaec8380fea4cd8eb0e06985f4d35
John Ky <john.ky@iohk.io>
2023-04-28 01:16:53 +1000

    Freeze callstack in integration and integrationRetryWorkspace functions

--- cardano-testnet/src/Testnet/Util/Base.hs
+++ cardano-testnet/src/Testnet/Util/Base.hs
@@ -7,16 +7,18 @@ module Testnet.Util.Base
 import           GHC.Stack (HasCallStack)
 import           System.Info (os)
 
+import qualified GHC.Stack as GHC
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Test.Base as H
 
 
 integration :: HasCallStack => H.Integration () -> H.Property
-integration = H.withTests 1 . H.propertyOnce
+integration f = GHC.withFrozenCallStack $ H.withTests 1 $ H.propertyOnce f
 
-integrationRetryWorkspace :: Int -> FilePath -> (FilePath -> H.Integration ()) -> H.Property
-integrationRetryWorkspace n workspaceName f = integration $ H.retry n $ \i ->
-  H.runFinallies $ H.workspace (workspaceName <> "-" <> show i) f
+integrationRetryWorkspace :: HasCallStack => Int -> FilePath -> (FilePath -> H.Integration ()) -> H.Property
+integrationRetryWorkspace n workspaceName f = GHC.withFrozenCallStack $
+  integration $ H.retry n $ \i ->
+    H.runFinallies $ H.workspace (workspaceName <> "-" <> show i) f
 
 isLinux :: Bool
 isLinux = os == "linux"
