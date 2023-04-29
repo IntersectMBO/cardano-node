44c4b76d3d5a106ecd625effd85ffe811db8f263
John Ky <john.ky@iohk.io>
2023-04-29 11:25:01 +1000

    Tidy up governance poll tests

--- cardano-cli/test/Test/Golden/Shelley/Governance/AnswerPoll.hs
+++ cardano-cli/test/Test/Golden/Shelley/Governance/AnswerPoll.hs
@@ -1,40 +1,81 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.Golden.Shelley.Governance.AnswerPoll
-  ( golden_shelleyGovernanceAnswerPoll
-  , golden_shelleyGovernanceAnswerPollInvalidAnswer
+  ( golden_shelleyGovernanceAnswerPollNeg1Invalid
+  , golden_shelleyGovernanceAnswerPoll0
+  , golden_shelleyGovernanceAnswerPollPos1
+  , golden_shelleyGovernanceAnswerPollPos2Invalid
   ) where
 
 import           Hedgehog (Property)
 import           Test.OptParse
 
+import           Control.Monad (void)
 import qualified Hedgehog as H
+import qualified Hedgehog.Extras.Test.Base as H
 import qualified Hedgehog.Extras.Test.File as H
+import qualified Test.Utilities as H
 
 {- HLINT ignore "Use camelCase" -}
 
-golden_shelleyGovernanceAnswerPoll :: Property
-golden_shelleyGovernanceAnswerPoll = propertyOnce $ do
+golden_shelleyGovernanceAnswerPollNeg1Invalid :: Property
+golden_shelleyGovernanceAnswerPollNeg1Invalid = propertyOnce . H.moduleWorkspace "governance-answer-poll" $ \tempDir -> do
   pollFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.json"
-
-  stdout <- execCardanoCLI
-    [ "governance", "answer-poll"
-    , "--poll-file", pollFile
-    , "--answer", "1"
-    ]
-
-  noteInputFile "test/data/golden/shelley/governance/answer/basic.json"
-    >>= H.readFile
-    >>= (H.===) stdout
-
-golden_shelleyGovernanceAnswerPollInvalidAnswer :: Property
-golden_shelleyGovernanceAnswerPollInvalidAnswer = propertyOnce $ do
-  pollFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.json"
+  outFile <- H.noteTempFile tempDir "answer-file.json"
 
   result <- tryExecCardanoCLI
     [ "governance", "answer-poll"
     , "--poll-file", pollFile
-    , "--answer", "3"
+    , "--answer", "-1"
+    , "--out-file", outFile
     ]
+
+  H.assertFileMissing outFile
+
+  either (const H.success) (const H.failure) result
+
+golden_shelleyGovernanceAnswerPoll0 :: Property
+golden_shelleyGovernanceAnswerPoll0 = propertyOnce . H.moduleWorkspace "governance-answer-poll" $ \tempDir -> do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.json"
+  expectedAnswerFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.answer.0.json"
+  outFile <- H.noteTempFile tempDir "answer-file.json"
+
+  void $ execCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--answer", "0"
+    , "--out-file", outFile
+    ]
+
+  H.diffFileVsGoldenFile outFile expectedAnswerFile
+
+golden_shelleyGovernanceAnswerPollPos1 :: Property
+golden_shelleyGovernanceAnswerPollPos1 = propertyOnce . H.moduleWorkspace "governance-answer-poll" $ \tempDir -> do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.json"
+  expectedAnswerFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.answer.1.json"
+  outFile <- H.noteTempFile tempDir "answer-file.json"
+
+  void $ execCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--answer", "1"
+    , "--out-file", outFile
+    ]
+
+  H.diffFileVsGoldenFile outFile expectedAnswerFile
+
+golden_shelleyGovernanceAnswerPollPos2Invalid :: Property
+golden_shelleyGovernanceAnswerPollPos2Invalid = propertyOnce . H.moduleWorkspace "governance-answer-poll" $ \tempDir -> do
+  pollFile <- noteInputFile "test/data/golden/shelley/governance/polls/basic.json"
+  outFile <- H.noteTempFile tempDir "answer-file.json"
+
+  result <- tryExecCardanoCLI
+    [ "governance", "answer-poll"
+    , "--poll-file", pollFile
+    , "--answer", "2"
+    , "--out-file", outFile
+    ]
+
+  H.assertFileMissing outFile
 
   either (const H.success) (const H.failure) result
