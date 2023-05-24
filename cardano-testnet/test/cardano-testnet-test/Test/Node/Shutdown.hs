3982ca922035cc46a321e71617010c10e5a8bcd6
Jordan Millar <jordan.millar@iohk.io>
2023-05-24 09:54:25 -0400

    Propagate the removal of the `base` record to hprop_shutdown Update hprop_shutdown to generate the requires genesis files and topology file Implement default mainnet topology file

--- cardano-testnet/test/cardano-testnet-test/Test/Node/Shutdown.hs
+++ cardano-testnet/test/cardano-testnet-test/Test/Node/Shutdown.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DisambiguateRecordFields #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -8,10 +7,14 @@ module Test.Node.Shutdown
   ( hprop_shutdown
   ) where
 
+import           Cardano.Api
 import           Control.Monad
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as LBS
 import           Data.Functor ((<&>))
 import qualified Data.List as L
 import           Data.Maybe
+import qualified Data.Time.Clock as DTC
 import           Hedgehog (Property, (===))
 import           Prelude
 import           System.FilePath ((</>))
@@ -19,46 +22,107 @@ import           System.FilePath ((</>))
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Stock.IO.Network.Socket as IO
 import qualified Hedgehog.Extras.Stock.IO.Network.Sprocket as IO
+import           Hedgehog.Extras.Stock.Time (formatIso8601)
 import qualified Hedgehog.Extras.Test.Base as H
 import qualified Hedgehog.Extras.Test.Concurrent as H
 import qualified Hedgehog.Extras.Test.File as H
 import qualified Hedgehog.Extras.Test.Process as H
-import qualified System.Directory as IO
 import qualified System.Exit as IO
 import qualified System.IO as IO
 import qualified System.Process as IO
 import qualified Testnet.Util.Base as H
 
 import           Cardano.Testnet
-import           Testnet.Util.Process (procNode)
+import           Testnet.Commands.Genesis
+import           Testnet.Options
+import           Testnet.Topology
+import           Testnet.Util.Process (execCli_, procNode)
+import           Testnet.Utils
 
 {- HLINT ignore "Redundant <&>" -}
 
 hprop_shutdown :: Property
 hprop_shutdown = H.integrationRetryWorkspace 2 "shutdown" $ \tempAbsBasePath' -> do
-  base <- H.note =<< H.noteIO . IO.canonicalizePath =<< H.getProjectBase
-  Conf { tempBaseAbsPath, tempAbsPath, logDir, socketDir } <- H.noteShowM $
-    mkConf (ProjectBase base) Nothing tempAbsBasePath' Nothing
-
+  conf <- H.noteShowM $
+    mkConf Nothing tempAbsBasePath' Nothing
+  let tempBaseAbsPath' = tempBaseAbsPath conf
+      tempAbsPath' = tempAbsPath conf
+      logDir' = logDir conf
+      socketDir' = socketDir conf
+      testnetMagic' = testnetMagic conf
   [port] <- H.noteShowIO $ IO.allocateRandomPorts 1
 
-  sprocket <- H.noteShow $ IO.Sprocket tempBaseAbsPath (socketDir </> "node")
+  sprocket <- H.noteShow $ IO.Sprocket tempBaseAbsPath' (socketDir' </> "node")
 
   H.diff (L.length (IO.sprocketArgumentName sprocket)) (<=) IO.maxSprocketArgumentNameLength
 
-  nodeStdoutFile <- H.noteTempFile logDir "node.stdout.log"
-  nodeStderrFile <- H.noteTempFile logDir "node.stderr.log"
+  nodeStdoutFile <- H.noteTempFile logDir' "node.stdout.log"
+  nodeStderrFile <- H.noteTempFile logDir' "node.stderr.log"
 
   hNodeStdout <- H.openFile nodeStdoutFile IO.WriteMode
   hNodeStderr <- H.openFile nodeStderrFile IO.WriteMode
 
+  -- We need to create the relevant genesis files to start the node
+  -- 1. Create Byron genesis
+
+  H.lbsWriteFile (tempAbsPath' </> "byron.genesis.spec.json")
+    . encode $ defaultByronProtocolParamsJsonValue
+
+  startTime <- H.noteShowIO DTC.getCurrentTime
+  createByronGenesis
+    testnetMagic'
+    startTime
+    babbageDefaultTestnetOptions
+    (tempAbsPath' </> "byron.genesis.spec.json")
+    (tempAbsPath' </> "byron")
+
+  shelleyDir <- H.createDirectoryIfMissing $ tempAbsPath' </> "shelley"
+
+  -- 2. Create Alonzo genesis
+  alonzoBabbageTestGenesisJsonTargetFile <- H.noteShow $ tempAbsPath' </> shelleyDir </> "genesis.alonzo.spec.json"
+  case defaultAlonzoGenesis of
+    Left e -> H.onFailure . H.noteIO_ . return $ displayError e
+    Right gen -> H.evalIO $ LBS.writeFile alonzoBabbageTestGenesisJsonTargetFile $ encode gen
+
+  -- 2. Create Conway genesis
+  conwayBabbageTestGenesisJsonTargetFile <- H.noteShow $ tempAbsPath' </> shelleyDir </> "genesis.conway.spec.json"
+  H.evalIO $ LBS.writeFile conwayBabbageTestGenesisJsonTargetFile $ encode defaultConwayGenesis
+
+  -- 4. Create Shelley genesis
+  execCli_
+    [ "genesis", "create"
+    , "--testnet-magic", show @Int testnetMagic'
+    , "--genesis-dir", shelleyDir
+    , "--start-time", formatIso8601 startTime
+    ]
+
+
+
+
+  byronGenesisHash <- getByronGenesisHash $ tempAbsPath' </> "byron/genesis.json"
+  shelleyGenesisHash <- getShelleyGenesisHash (tempAbsPath' </> "shelley/genesis.json") "ShelleyGenesisHash"
+  alonzoGenesisHash <- getShelleyGenesisHash (tempAbsPath' </> "shelley/genesis.alonzo.json") "AlonzoGenesisHash"
+
+  let finalYamlConfig :: LBS.ByteString
+      finalYamlConfig = encode . Object
+                                 $ mconcat [ byronGenesisHash
+                                           , shelleyGenesisHash
+                                           , alonzoGenesisHash
+                                           , defaultYamlHardforkViaConfig (AnyCardanoEra BabbageEra)]
+
+  H.evalIO $ LBS.writeFile (tempAbsPath' </> "configuration.yaml") finalYamlConfig
+
+  H.evalIO $ LBS.writeFile (tempAbsPath' </> "mainnet-topology.json")
+    $ encode defaultMainnetTopology
+
+  -- TODO: Stopped here
   -- Run cardano-node with pipe as stdin.  Use 0 file descriptor as shutdown-ipc
   (mStdin, _mStdout, _mStderr, pHandle, _releaseKey) <- H.createProcess =<<
     ( procNode
       [ "run"
-      , "--config", base </> "configuration/cardano/mainnet-config.json"
-      , "--topology", base </> "configuration/cardano/mainnet-topology.json"
-      , "--database-path", tempAbsPath </> "db"
+      , "--config", tempAbsPath' </> "configuration.yaml"
+      , "--topology", tempAbsPath' </> "mainnet-topology.json"
+      , "--database-path", tempAbsPath' </> "db"
       , "--socket-path", IO.sprocketArgumentName sprocket
       , "--host-addr", "127.0.0.1"
       , "--port", show @Int port
@@ -68,7 +132,7 @@ hprop_shutdown = H.integrationRetryWorkspace 2 "shutdown" $ \tempAbsBasePath' ->
         { IO.std_in = IO.CreatePipe
         , IO.std_out = IO.UseHandle hNodeStdout
         , IO.std_err = IO.UseHandle hNodeStderr
-        , IO.cwd = Just tempBaseAbsPath
+        , IO.cwd = Just tempBaseAbsPath'
         }
       )
     )
