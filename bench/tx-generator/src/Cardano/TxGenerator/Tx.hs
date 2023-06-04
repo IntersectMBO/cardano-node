af973ac488b966911d03e4450b96033fa106a778
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2023-06-04 14:39:40 -0400

    Document top-level functions in Tx.

--- bench/tx-generator/src/Cardano/TxGenerator/Tx.hs
+++ bench/tx-generator/src/Cardano/TxGenerator/Tx.hs
@@ -9,6 +9,7 @@ module  Cardano.TxGenerator.Tx
 
 import           Control.Monad.Trans.Except (ExceptT, except)
 import           Control.Monad.Trans (lift)
+import           Control.Arrow ((&&&))
 import           Data.Bifunctor (bimap)
 import qualified Data.ByteString as BS (length)
 import           Data.Function ((&))
@@ -22,11 +23,38 @@ import           Cardano.TxGenerator.Types
 import           Cardano.TxGenerator.UTxO (ToUTxOList)
 
 
+-- | 'CreateAndStore' is meant to represent building a transaction
+-- from a single number and presenting a function to carry out the
+-- needed side effects.
+-- This type alias is only used in "Cardano.Benchmarking.Wallet".
 type CreateAndStore m era           = Lovelace -> (TxOut CtxTx era, TxIx -> TxId -> m ())
+
+-- | 'CreateAndStoreList' is meant to represent building a transaction
+-- and presenting a function to carry out the needed side effects.
+-- This type alias is also only used in "Cardano.Benchmarking.Wallet".
+-- The @split@ parameter seems to actualy be used for not much more
+-- than lists and records containing lists.
 type CreateAndStoreList m era split = split -> ([TxOut CtxTx era], TxId -> m ())
+
 
 -- TODO: 'sourceToStoreTransaction' et al need to be broken up
 -- for the sake of maintainability.
+
+-- | 'sourceToStoreTransaction' builds a transaction out of several
+-- arguments. "Cardano.Benchmarking.Script.PureExample" is the sole caller.
+-- @txGenerator@ is just 'genTx' partially applied in all uses of all
+-- these functions.
+-- @inputFunds@ for this is a list of 'Lovelace' with some extra
+-- fields to throw away and coproducts maintaining distinctions that
+-- don't matter to these functions.
+-- The @inToOut@ argument seems to just sum and subtract the fee in
+-- seemingly all callers.
+-- @mkTxOut@ gets built from functions in "Cardano.TxGenerator.UTxO".
+-- The other functions take 'CreateAndStoreList' arguments and name
+-- them @valueSplitter@ and callers construct the argument from the
+-- mangling functions in 'Cardano.TxGenerator.Utils". 
+-- @fundToStore@ commits the single-threaded fund state in its sole
+-- caller with 'Control.Monad.State.put', using a @State@ monad.
 sourceToStoreTransaction ::
      Monad m
   => TxGenerator era
@@ -36,11 +64,26 @@ sourceToStoreTransaction ::
   -> FundToStoreList m                --inline to ToUTxOList
   -> ExceptT TxGenError m (Tx era)
 sourceToStoreTransaction txGenerator inputFunds inToOut mkTxOut fundToStore = do
+  -- 'getFundLovelace' unwraps the 'TxOutValue' in a fund field so it's
+  -- all just 'Lovelace' instead of a copruduct maintaining distinctions.
   (outputs, toFunds) <- fmap mkTxOut . inToOut $ map getFundLovelace inputFunds
   (tx, txId) <- except $ txGenerator inputFunds outputs
   lift . fundToStore $ toFunds txId
   return tx
 
+-- | 'sourceToStoreTransactionNew' builds a new transaction out of
+-- several things. 'Cardano.Benchmarking.Script.Core.evalGenerator' in
+-- "Cardano.Benchmarking.Script.Core" is the sole caller.
+-- @txGenerator@ is just 'genTx' partially applied in every use.
+-- @inputFunds@ for this is a list of 'Lovelace' with some extra
+-- fields to throw away and coproducts maintaining distinctions that
+-- don't matter to these functions.
+-- @valueSplitter@ is just 'Cardano.TxGenerator.Utils.includeChange' or
+-- 'Cardano.TxGenerator.Utils.inputsToOutputsWithFee' at every use,
+-- which just sum the inputs and subtract the fee.
+-- @toStore@ is just a partial application of either
+-- 'Cardano.Benchmarking.Wallet.mangleWithChange'
+-- or 'Cardano.Benchmarking.Wallet.mangle' at every use.
 sourceToStoreTransactionNew ::
      Monad m
   => TxGenerator era
@@ -54,8 +97,28 @@ sourceToStoreTransactionNew txGenerator inputFunds valueSplitter toStore = do
   lift $ storeAction txId
   return tx
 
--- just a preview of a transaction:
--- not intended to be submitted; funds remain unchanged
+-- | 'sourceTransactionPreview' is only used at one point in
+-- 'Cardano.Benchmarking.Script.Core.evalGenerator' within
+-- "Cardano.Benchmarking.Script.Core" to generate a hopefully pure
+-- transaction to examine.
+-- This only constructs a preview of a transaction not intended to be
+-- submitted. Funds remain unchanged by dint of a different method
+-- of wallet access.
+-- @txGenerator@ is the same 'genTx' partial application passed to other
+-- functions here.
+-- @inputFunds@ for this is a list of 'Lovelace' with some extra
+-- fields to throw away and coproducts maintaining distinctions that
+-- don't matter to these functions. This is the only argument that differs
+-- from 'sourceToStoreTransactionNew', being drawn from a use of
+-- 'Cardano.Benchmarking.Wallet.walletPreview'.
+-- @valueSplitter@ is just 'Cardano.TxGenerator.Utils.inputsToOutputsWithFee'
+-- at the sole use, with the same variable for monad lifting etc. as
+-- the other companion functions.
+-- @toStore@ is just a partial application of
+-- 'Cardano.Benchmarking.Wallet.mangle' at the sole use, with the same
+-- expression involving the same function returned as a product of
+-- 'Cardano.Benchmarking.Wallet.createAndStore' as the nearby invocation
+-- of 'sourceToStoreTransactionNew' in "Cardano.Benchmarking.Script.Core".
 sourceTransactionPreview ::
   Monad m
   => TxGenerator era
@@ -67,6 +130,17 @@ sourceTransactionPreview txGenerator inputFunds valueSplitter toStore = do
   (outputs, _) <- fmap toStore . valueSplitter $ map getFundLovelace inputFunds
   fmap fst . except $ txGenerator inputFunds outputs
 
+-- | 'genTx' seems to mostly be a wrapper for
+-- 'Cardano.Api.TxBody.createAndValidateTransactionBody', which uses
+-- the 'Either' convention in lieu of e.g.
+-- 'Control.Monad.Trans.Except.ExceptT'. Then the pure function
+-- 'Cardano.Api.Tx.makeSignedTransaction' is composed with it and
+-- the 'Cardano.Api.Error' is lifted to 'Cardano.TxGenerator.Types.TxGenError'
+-- as an 'Cardano.TxGenerator.Types.ApiError' case.
+-- The @txGenerator@ arguments of the rest of the functions in this
+-- module are all partial applications of this to its first 5 arguments.
+-- The 7th argument comes from 'TxGenerator' being a being a type alias
+-- for a function type -- of two arguments.
 genTx :: forall era. ()
   => IsShelleyBasedEra era
   => CardanoEra era
@@ -76,12 +150,12 @@ genTx :: forall era. ()
   -> TxMetadataInEra era
   -> TxGenerator era
 genTx _era protocolParameters (collateral, collFunds) fee metadata inFunds outputs
-  = bimap
-      ApiError
-      (\b -> (signShelleyTransaction b $ map WitnessPaymentKey allKeys, getTxId b))
-      (createAndValidateTransactionBody txBodyContent)
+  -- 'Data.Bifunctor.bimap` doesn't need composition with Left and
+  -- Right like 'Control.Arrow.(+++)' but otherwise does the same here.
+  = ApiError `bimap` (flip signShelleyTransaction allKeys &&& getTxId)
+        $ createAndValidateTransactionBody txBodyContent
  where
-  allKeys = mapMaybe getFundKey $ inFunds ++ collFunds
+  allKeys = mapMaybe (fmap WitnessPaymentKey . getFundKey) $ inFunds ++ collFunds
   txBodyContent = defaultTxBodyContent
     & setTxIns (map (\f -> (getFundTxIn f, BuildTxWith $ getFundWitness f)) inFunds)
     & setTxInsCollateral collateral
