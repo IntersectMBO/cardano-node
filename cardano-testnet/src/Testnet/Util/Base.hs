7540b910e3c87cf477ecf4e09a0925d434ca4bde
John Ky <john.ky@iohk.io>
2023-06-12 15:30:53 +1000

    Add DISABLE_RETRIES support

--- cardano-testnet/src/Testnet/Util/Base.hs
+++ cardano-testnet/src/Testnet/Util/Base.hs
@@ -11,7 +11,14 @@ import           System.Info (os)
 import qualified GHC.Stack as GHC
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras.Test.Base as H
+import qualified System.Environment as IO
+import qualified System.IO.Unsafe as IO
 
+disableRetries :: Bool
+disableRetries = IO.unsafePerformIO $ do
+  mValue <- IO.lookupEnv "DISABLE_RETRIES"
+  return $ mValue == Just "1"
+{-# NOINLINE disableRetries #-}
 
 integration :: HasCallStack => H.Integration () -> H.Property
 integration f = GHC.withFrozenCallStack $ H.withTests 1 $ H.propertyOnce f
@@ -20,8 +27,13 @@ integration f = GHC.withFrozenCallStack $ H.withTests 1 $ H.propertyOnce f
 -- This is created (and returned) via 'H.workspace'.
 integrationRetryWorkspace :: HasCallStack => Int -> FilePath -> (FilePath -> H.Integration ()) -> H.Property
 integrationRetryWorkspace n workspaceName f = GHC.withFrozenCallStack $
-  integration $ H.retry n $ \i ->
-    H.runFinallies $ H.workspace (workspaceName <> "-" <> show i) f
+  if disableRetries
+    then
+      integration $
+        H.runFinallies $ H.workspace (workspaceName <> "-no-retries") f
+    else
+      integration $ H.retry n $ \i ->
+        H.runFinallies $ H.workspace (workspaceName <> "-" <> show i) f
 
 -- | The 'FilePath' in '(FilePath -> H.Integration ())' is the work space directory.
 -- This is created (and returned) via 'H.workspace'.
