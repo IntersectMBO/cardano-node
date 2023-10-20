0ec0e4172ea9880e35c797dcebf87b2ac09256a5
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2023-10-19 20:25:31 -0400

    bench: run before/after selftests
    This bypasses the runCommand front end. Comparing the results may take
    some work.

--- bench/tx-generator/test/MonadicMain.hs
+++ bench/tx-generator/test/MonadicMain.hs
@@ -1,8 +1,12 @@
-import           Ouroboros.Network.NodeToClient (withIOManager)
-import           MonadicGen.Cardano.Benchmarking.Script.Selftest (runSelftest)
+import qualified Cardano.Benchmarking.Script.Selftest as C (runSelftest)
+import qualified MonadicGen.Cardano.Benchmarking.Script.Selftest as M (runSelftest)
+import           Ouroboros.Network.NodeToClient (IOManager, withIOManager)
 import           System.Exit (die, exitSuccess)
 
 main :: IO ()
-main = withIOManager
-     $ \iocp -> runSelftest iocp Nothing
-                     >>= either (die . show) (const exitSuccess)
+main = runSelftest' C.runSelftest >> runSelftest' M.runSelftest
+
+runSelftest' :: Show a => (IOManager -> Maybe FilePath -> IO (Either a ())) -> IO ()
+runSelftest' rst = do
+  withIOManager $ \iocp -> rst iocp Nothing
+                               >>= either (die . show) (const exitSuccess)
