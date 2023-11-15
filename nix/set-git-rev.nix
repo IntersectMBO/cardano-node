677711db13e9035951d5de750222ebc66dcc82d6
Moritz Angermann <moritz.angermann@gmail.com>
2023-11-15 13:29:13 +0800

    Add darwin.signingUtils
    And ensure we are re-siging files we potentially mutilated with
    git-set-rev.

--- nix/set-git-rev.nix
+++ nix/set-git-rev.nix
@@ -1,11 +1,19 @@
 { pkgs }:
 drv:
-pkgs.buildPackages.runCommand drv.name
+with pkgs;
+buildPackages.runCommand drv.name
 {
   inherit (drv) exeName exePath meta passthru;
-} ''
+  # this is to ensure we are re-signing macOS binaries that might have been
+  # mutilated by set-git-rev (e.g. patching in the git revision.)
+  nativeBuildInputs = lib.optionals hostPlatform.isDarwin [ darwin.signingUtils ];
+} (''
   mkdir -p $out
   cp --no-preserve=timestamps --recursive ${drv}/* $out/
   chmod -R +w $out/bin
-  ${pkgs.pkgsBuildBuild.haskellBuildUtils}/bin/set-git-rev "${pkgs.gitrev}" $out/bin/*
-''
+  ${pkgsBuildBuild.haskellBuildUtils}/bin/set-git-rev "${gitrev}" $out/bin/*
+'' + lib.optionalString hostPlatform.isDarwin ''
+  for exe in $out/bin/*; do
+    signIfRequired "$exe"
+  done
+'')
