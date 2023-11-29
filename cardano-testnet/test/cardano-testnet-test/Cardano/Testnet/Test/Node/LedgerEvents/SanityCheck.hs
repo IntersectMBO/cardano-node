ff60321ce9c97a91dba356c884759c7105aa458b
Jordan Millar <jordan.millar@iohk.io>
2023-11-29 09:18:27 -0400

    Rename Cardano.Testnet.Test.Node.LedgerEvents.hs to Cardano.Testnet.Test.Node.LedgerEvents.SanityCheck.hs

--- cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Node/LedgerEvents/SanityCheck.hs
+++ cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Node/LedgerEvents/SanityCheck.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Cardano.Testnet.Test.Node.LedgerEvents
+module Cardano.Testnet.Test.Node.LedgerEvents.SanityCheck
   ( hprop_ledger_events_sanity_check
   ) where
 
--- cardano-testnet/test/cardano-testnet-test/cardano-testnet-test.hs
+++ cardano-testnet/test/cardano-testnet-test/cardano-testnet-test.hs
@@ -11,7 +11,7 @@ import qualified Cardano.Testnet.Test.Cli.Babbage.Transaction
 import qualified Cardano.Testnet.Test.Cli.KesPeriodInfo
 import qualified Cardano.Testnet.Test.Cli.QuerySlotNumber
 import qualified Cardano.Testnet.Test.FoldBlocks
-import qualified Cardano.Testnet.Test.Node.LedgerEvents
+import qualified Cardano.Testnet.Test.Node.LedgerEvents.SanityCheck
 import qualified Cardano.Testnet.Test.Node.Shutdown
 import qualified Cardano.Testnet.Test.SubmitApi.Babbage.Transaction
 
@@ -31,7 +31,7 @@ tests = pure $ T.testGroup "test/Spec.hs"
   [ T.testGroup "Spec"
       [ T.testGroup "CLI"
         [ H.ignoreOnWindows "Shutdown" Cardano.Testnet.Test.Node.Shutdown.hprop_shutdown
-        , H.ignoreOnWindows "LedgerEvents" Cardano.Testnet.Test.Node.LedgerEvents.hprop_ledger_events_sanity_check
+        , H.ignoreOnWindows "LedgerEvents" Cardano.Testnet.Test.Node.LedgerEvents.SanityCheck.hprop_ledger_events_sanity_check
         , H.ignoreOnWindows "ShutdownOnSigint" Cardano.Testnet.Test.Node.Shutdown.hprop_shutdownOnSigint
         -- ShutdownOnSlotSynced FAILS Still. The node times out and it seems the "shutdown-on-slot-synced" flag does nothing
         -- , H.ignoreOnWindows "ShutdownOnSlotSynced" Cardano.Testnet.Test.Node.Shutdown.hprop_shutdownOnSlotSynced
