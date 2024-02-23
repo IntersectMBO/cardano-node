78618e45ac4b15d5e6e3330311c45287d70b68bd
John Lotoski <john.lotoski@iohk.io>
2024-02-22 18:43:52 -0600

    bump: iohk-nix for bootstrap opt, mithril, ci fix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     };
     utils.url = "github:numtide/flake-utils";
     iohkNix = {
-      url = "github:input-output-hk/iohk-nix";
+      url = "github:input-output-hk/iohk-nix/bootstrap-peers";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ops-lib = {
