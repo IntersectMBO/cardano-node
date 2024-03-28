d9ad053a08ff26c4c63da4842d8c83c44689cd62
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2024-03-28 15:55:34 +1300

    enableShared = true for cardano-cli too

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -342,10 +342,6 @@ let
               {
                 # Haddock not working and not needed for cross builds
                 doHaddock = false;
-                packages.cardano-cli.enableShared = false;
-                packages.cardano-cli.enableStatic = true;
-                # Needed for TH code in cardano-cli
-                packages.cardano-git-rev.enableShared = lib.mkForce true;
               };
           })
           ({ lib, pkgs, ... }: lib.mkIf (pkgs.stdenv.hostPlatform != pkgs.stdenv.buildPlatform) {
