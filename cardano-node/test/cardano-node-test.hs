5388f1e104e5d23f2928bbc739612d4aa8514d49
Neil Mayhew <neil.mayhew@iohk.io>
2024-04-09 17:32:01 -0600

    Add golden tests for Cardano.Tracing.OrphanInstances.Shelley

--- cardano-node/test/cardano-node-test.hs
+++ cardano-node/test/cardano-node-test.hs
@@ -14,6 +14,7 @@ import qualified Test.Cardano.Node.FilePermissions
 import qualified Test.Cardano.Node.Json
 import qualified Test.Cardano.Node.POM
 import qualified Test.Cardano.Tracing.OrphanInstances.HardFork
+import qualified Test.Cardano.Tracing.OrphanInstances.Shelley
 import qualified Test.Cardano.Tracing.NewTracing.Consistency
 
 import qualified Cardano.Crypto.Init as Crypto
@@ -35,5 +36,6 @@ main = do
       , Test.Cardano.Node.Json.tests
       , Test.Cardano.Node.POM.tests
       , Test.Cardano.Tracing.OrphanInstances.HardFork.tests
+      , Test.Cardano.Tracing.OrphanInstances.Shelley.tests
       , Test.Cardano.Tracing.NewTracing.Consistency.tests
       ]
