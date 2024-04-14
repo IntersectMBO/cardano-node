36c984ad907579b95e5fa75f19f713790e44f5b5
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2024-04-14 03:59:01 +0000

    Extract modules

--- bench/stdout-tools/app/stdout-tools.hs
+++ bench/stdout-tools/app/stdout-tools.hs
@@ -84,7 +84,7 @@ sys 0m46.550s
 
 -}
 
-{-- RTS params:
+{-- RTS params: +RTS -xc -s -l -hc
 
 -N:
 There are two ways to run a program on multiple processors: call
@@ -123,31 +123,18 @@ module Main (main) where
 
 -- base.
 import           Control.Applicative (some, (<|>))
-import           Control.Monad (when, foldM)
-import           Data.Foldable (toList)
-import           Data.Kind (Type)
-import           GHC.Generics
 -- package: time.
-import           Data.Time.Clock
-  ( UTCTime
-  , getCurrentTime
-  , NominalDiffTime
-  , diffUTCTime
-  )
--- package: containers.
-import qualified Data.Sequence as Seq
+import           Data.Time.Clock (getCurrentTime, diffUTCTime)
 -- package: text.
 import qualified Data.Text as Text
--- package: text-iso8601-0.1
-import qualified Data.Time.FromText as ParseTime
--- package: aeson.
-import qualified Data.Aeson as Aeson
 -- package: async.
 import qualified Control.Concurrent.Async as Async
 -- package: optparse-applicative.
 import qualified Options.Applicative as Opt
 -- library.
 import qualified Data.Log as Log
+import qualified Cardano.Tracer.Trace as Trace
+import qualified Cardano.Tracer.Reducer as Reducer
 
 --------------------------------------------------------------------------------
 
@@ -161,18 +148,18 @@ data CliOpts = CliOpts
   }
   deriving Show
 
-data ReducerElem = forall r. (Show r, Reducer r) => MkReducer r
+data ReducerElem = forall r. (Show r, Reducer.Reducer r) => MkReducer r
 
 instance Show ReducerElem where
   show (MkReducer r) = show r
 
 cliFilterReader :: String -> Either String ReducerElem
 cliFilterReader str = case str of
-  "count-lines"  -> Right $ MkReducer CountLines
-  "count-FLSLCP" -> Right $ MkReducer CountStartLeadershipCheckPlus
-  "heap-changes" -> Right $ MkReducer HeapChanges
-  "missed-slots" -> Right $ MkReducer MissedSlots
-  "1s-silences"  -> Right $ MkReducer OneSecondSilences
+  "count-lines"  -> Right $ MkReducer Reducer.CountLines
+  "count-FLSLCP" -> Right $ MkReducer Reducer.CountStartLeadershipCheckPlus
+  "heap-changes" -> Right $ MkReducer Reducer.HeapChanges
+  "missed-slots" -> Right $ MkReducer Reducer.MissedSlots
+  "1s-silences"  -> Right $ MkReducer Reducer.OneSecondSilences
   _ -> Left str
 
 main :: IO ()
@@ -183,120 +170,6 @@ main = do
        <> Opt.header "hello - a test for optparse-applicative"
     )
   run cliOpts
-
---------------------------------------------------------------------------------
-
-class Show r => Reducer r where
-  type family Accum r :: Type
-  initialOf :: r -> Accum r
-  reducerOf :: r -> Accum r -> Cursor -> Accum r
-  showAns   :: r -> Accum r -> String
-  printAns  :: r -> Accum r -> IO ()
-  printAns r acc = putStrLn $ showAns r acc
-
-data CountLines = CountLines
-  deriving Show
-
-data CountStartLeadershipCheckPlus = CountStartLeadershipCheckPlus
-  deriving Show
-
-data HeapChanges = HeapChanges
-  deriving Show
-
-data MissedSlots = MissedSlots
-  deriving Show
-
-data OneSecondSilences = OneSecondSilences
-  deriving Show
-
-instance Reducer CountLines where
-  type instance Accum CountLines = Int
-  initialOf _ = 0
-  reducerOf _ = (\l _ -> l + 1)
-  showAns   _ = show
-
-instance Reducer CountStartLeadershipCheckPlus where
-  type instance Accum CountStartLeadershipCheckPlus = Int
-  initialOf _ = 0
-  reducerOf _ = (\l (Cursor _ maybeMsg) ->
-    case maybeMsg of
-      Nothing -> l
-      (Just msg) ->
-        if ns msg == "Forge.Loop.StartLeadershipCheckPlus"
-        then l + 1
-        else l
-    )
-  showAns  _ = show
-
-instance Reducer HeapChanges where
-  type instance Accum HeapChanges = (Maybe Integer, Seq.Seq (UTCTime, Integer))
-  initialOf _ = (Nothing, Seq.empty)
-  reducerOf _ ans (Cursor _ Nothing) = ans
-  reducerOf _ ans@(maybePrevHeap, sq) (Cursor _ (Just !cursorTrace)) =
-    case Aeson.eitherDecodeStrictText (more cursorTrace) of
-      (Right !resourcesMore) ->
-        -- TODO: Use `unsnoc` when available
-        let actualHeap = resourcesHeap $ traceData resourcesMore
-        in case maybePrevHeap of
-          Nothing -> (Just actualHeap, Seq.singleton (at cursorTrace, actualHeap))
-          (Just prevHeap) ->
-            if actualHeap == prevHeap
-            then ans
-            else (Just actualHeap, sq Seq.|> (at cursorTrace, actualHeap))
-      (Left _) -> ans
-  showAns _ = show
-  printAns _ (_, sq) = mapM_
-    (\(t,h) -> putStrLn $ show t ++ ": " ++ show h)
-    (toList sq)
-
-instance Reducer MissedSlots where
-  type instance Accum MissedSlots = (Maybe Integer, Seq.Seq Integer)
-  initialOf _ = (Nothing, Seq.empty)
-  reducerOf _ ans (Cursor _ Nothing) = ans
-  reducerOf _ ans@(maybePrevSlot, !sq) (Cursor _ (Just !(Trace _ "Forge.Loop.StartLeadershipCheckPlus" aeson))) =
-    case Aeson.eitherDecodeStrictText aeson of
-      (Right !dataWithSlot) ->
-        -- TODO: Use `unsnoc` when available
-        let actualSlot = slot $ traceData dataWithSlot
-        in case maybePrevSlot of
-          Nothing -> (Just actualSlot, Seq.empty)
-          (Just prevSlot) ->
-            if actualSlot == prevSlot + 1
-            then (Just actualSlot, sq)
-            else (Just actualSlot, sq Seq.>< (Seq.fromList [(prevSlot+1)..(actualSlot-1)]))
-      (Left _) -> ans
-  reducerOf _ ans (Cursor _ (Just _)) = ans
-  showAns _ = show
-  printAns _ (_, sq) = do
-    ans <- foldM
-      (\maybePrevSlot lostSlot ->
-        case maybePrevSlot of
-          Nothing -> putStr (show lostSlot) >> return (Just lostSlot)
-          (Just prevSlot) ->
-            if prevSlot + 1 == lostSlot
-            then return (Just lostSlot)
-            else putStrLn (".." ++ show prevSlot) >> return Nothing
-      )
-      Nothing
-      (toList sq)
-    when (ans /= Nothing) (putStrLn "")
-
-instance Reducer OneSecondSilences where
-  type instance Accum OneSecondSilences = (Maybe Trace, Seq.Seq (NominalDiffTime, Trace, Trace))
-  initialOf _ = (Nothing, Seq.empty)
-  reducerOf _ (Nothing, sq) cursor = (_maybeCursorMsg cursor, sq)
-  reducerOf _ (Just _prevMsg, sq) (Cursor _ Nothing) = (Just _prevMsg, sq)
-  reducerOf _ (Just _prevMsg, sq) (Cursor _ (Just cursorTrace)) =
-    let diffTime = diffUTCTime (at cursorTrace) (at _prevMsg)
-    in if diffTime >  fromInteger 2
-    then (Just cursorTrace, sq Seq.|> (diffTime, _prevMsg, cursorTrace))
-    else (Just cursorTrace, sq)
-  showAns   _ = show
-  printAns _ (_,sq) = mapM_
-    (\(ndt, t1, _) ->
-      putStrLn $ (show ndt) ++ " (" ++ (show $ at t1) ++ ")"
-    )
-    (toList sq)
 
 --------------------------------------------------------------------------------
 
@@ -366,11 +239,11 @@ run cliOpts@(CliOpts _ parallel ((MkReducer r):_)) = do
     mapM_
       (\(logName,fp) -> do
         ans <- lineFoldl'
-          (reducerOf r)
-          (initialOf r)
+          (Reducer.reducerOf r)
+          (Reducer.initialOf r)
           fp
         print logName
-        printAns r ans
+        Reducer.printAns r ans
       )
       (files cliOpts)
   else do
@@ -382,8 +255,8 @@ run cliOpts@(CliOpts _ parallel ((MkReducer r):_)) = do
     ansParallel <- Async.mapConcurrently
       (\(logName,fp) -> do
         ans <- lineFoldl'
-          (reducerOf r)
-          (initialOf r)
+          (Reducer.reducerOf r)
+          (Reducer.initialOf r)
           fp
         return (logName, ans)
       )
@@ -391,7 +264,7 @@ run cliOpts@(CliOpts _ parallel ((MkReducer r):_)) = do
     mapM_
       (\(logName,ans) -> do
         print logName
-        printAns r ans
+        Reducer.printAns r ans
       )
       ansParallel
   t1 <- getCurrentTime
@@ -414,103 +287,14 @@ run cliOpts@(CliOpts _ parallel ((MkReducer r):_)) = do
   -- End
   return ()
 
--- A log message.
---------------------------------------------------------------------------------
-
--- TODO:
--- All traces start with, use this assumption to build a "fast" decoder.
--- {"at":"2024-03-30T00:30:27.015631111Z","ns":"Reflection.TracerInfo"
--- Keep it simple!
-data Trace = Trace
-  -- Strict of keep thunks of `Data.Time.FromText.parseUTCTime`.
-  { at :: UTCTime -- "2024-04-06T11:27:45.37268578Z"
-  , ns :: Text.Text
-  -- Only do `fromJSON` if needed!
-  , more :: Text.Text
-  }
-  deriving (Eq, Show)
-
--- Fast & Ugly, Ugly & Fast.
--- Too many assumptions (assumption is the parent of all thing that did not go
--- quite as expected).
-traceFromJson :: Text.Text -> Either String Trace
-traceFromJson text =
-  -- Assume '{"at":"'' is there
-  case Text.splitAt 7 text of
-    -- Property 'at' assumed as expected.
-    ("{\"at\":\"", text') ->
-           -- Assume a date like '2024-04-11T12:01:33.2135764Z' is there.
-           -- The milliseconds part is variable, can't read a fix amount.
-      let (atText, text'' ) = Text.break (== '"') text'
-          -- Consume all the text until the next '"'.
-          -- First drop the date's last '"' ans assume ',"ns":"' is there.
-          (nsText, text''') = Text.break (== '"') (Text.drop 8 text'')
-      --in Left $ show (atText, text'', nsText, text''')
-      in  case ParseTime.parseUTCTime atText of
-            (Left err) -> Left $ "parseUTCTime: " ++ err
-            (Right utcTime) -> Right $
-              -- Drop closing '",' of 'ns' and leave unconsumed as a new object.
-              Trace utcTime nsText ("{" <> (Text.drop 2 text'''))
-    _ -> Left "No {\"at\":\""
-
--- Keep it simple!
-data TraceData a = TraceData
-  -- Strict of keep thunks of `Data.Time.FromText.parseUTCTime`.
-  { traceData :: a
-  , sev       :: Text.Text
-  , thread    :: Text.Text
-  , host      :: Text.Text
-  }
-  deriving (Eq, Show, Generic)
-
-instance Aeson.ToJSON a => Aeson.ToJSON (TraceData a) where
-  -- Only using a non-automatic instance because of "data" and "msgData".
-  toJSON p@(TraceData _ _ _ _) =
-    Aeson.object
-      [ "data"   Aeson..= traceData p
-      , "sev"    Aeson..= sev       p
-      , "thread" Aeson..= thread    p
-      , "host"   Aeson..= host      p
-      ]
-
-instance Aeson.FromJSON a => Aeson.FromJSON (TraceData a) where
-  -- Only using a non-automatic instance because of "data" and "msgData".
-  parseJSON =
-    Aeson.withObject "TraceData" $ \o -> do
-      TraceData
-        <$> o Aeson..: "data"
-        <*> o Aeson..: "sev"
-        <*> o Aeson..: "thread"
-        <*> o Aeson..: "host"
-
-{--
-class Cursor a where
-  cursorText :: a -> Text.Text
-  maybeCursorMsg :: a -> Maybe Msg
-
-instance Cursor BasicCursor where
-  cursorText = _cursorText
-  maybeCursorMsg = _cursorText
---}
-
--- Keep it simple, stak accumulators if you need more things like line number.
-data Cursor = Cursor
-  { _cursorText :: Text.Text
-  -- All lines are read (converted to Text) but `fromJSON` is lazy.
-  , _maybeCursorMsg :: Maybe Trace
-  }
-
 -- Allow to `fold'` through the log file but in JSON format.
-lineFoldl' :: (a -> Cursor -> a) -> a -> FilePath -> IO a
+lineFoldl' :: (a -> (Either Text.Text Trace.Trace) -> a) -> a -> FilePath -> IO a
 lineFoldl' f initialAcc filePath = do
   Log.lineFoldl'
     (\acc textLine ->
-      let maybeTrace = case traceFromJson textLine of
-                      (Left _str) -> Nothing
-                      (Right trace) -> Just trace
-          -- CRITICAL: Has to be "STRICT" to keep `Log.lineFoldl'`'s behaviour.
-          --           I repeat, the accumulator function has to be strict!
-          !nextAcc = f acc (Cursor textLine maybeTrace)
+      -- CRITICAL: Has to be "STRICT" to keep `Log.lineFoldl'`'s behaviour.
+      --           I repeat, the accumulator function has to be strict!
+      let !nextAcc = f acc (Trace.fromJson textLine)
       in nextAcc
     )
     initialAcc
@@ -518,20 +302,8 @@ lineFoldl' f initialAcc filePath = do
 
 --------------------------------------------------------------------------------
 
-{--
--- Keep it simple!
-data MsgAt = MsgAt
-  -- Strict of keep thunks of `Data.Time.FromText.parseUTCTime`.
-  { at :: !UTCTime -- "2024-04-06T11:27:45.37268578Z"
-  }
-  deriving (Eq, Show, Generic)
-
-instance Aeson.FromJSON Msg where
-
-instance Aeson.ToJSON Msg where
---}
-
 -- TODO:
+{--
 _foldlLog3 :: (
                ((a -> Cursor -> a), a)
              , ((b -> Cursor -> b), b)
@@ -550,73 +322,9 @@ _foldlLog3 ((fa,a),(fb,b),(fc,c)) filePath = do
   --return $! (a', b', c')
   return $ seq a' $ seq b' $ seq c' (a', b', c')
 
-{-- TODO:
 foldlWhile :: Foldable t => (a -> Bool) -> (r -> a -> r) -> r -> t a -> r
 foldlWhile t f a xs  =  foldr cons (\acc -> acc) xs a
   where
     cons x r acc | t x  =  r (f acc x)
                  | otherwise  =  acc
 --}
-
---------------------------------------------------------------------------------
-
-{--
-  "data": {
-    "Alloc": 98169147912,
-    "CentiBlkIO": 0,
-    "CentiCpu": 10831,
-    "CentiGC": 4726,
-    "CentiMut": 6104,
-    "FsRd": 0,
-    "FsWr": 8192,
-    "GcsMajor": 7,
-    "GcsMinor": 3590,
-    "Heap": 8629780480,
-    "Live": 2529171488,
-    "NetRd": 0,
-    "NetWr": 0,
-    "RSS": 8683200512,
-    "Threads": 9,
-    "kind": "ResourceStats"
-  },
---}
-data Resources = Resources
-  { resourcesAlloc :: Integer
-  , resourcesCentiBlkIO :: Integer
-  , resourcesCentiCpu :: Integer
-  , resourcesCentiGC :: Integer
-  , resourcesCentiMut :: Integer
-  , resourcesFsRd :: Integer
-  , resourcesGcsMajor :: Integer
-  , resourcesGcsMinor :: Integer
-  , resourcesHeap :: Integer
-  , resourcesLive :: Integer
-  , resourcesNetRd :: Integer
-  , resourcesNetWr :: Integer
-  , resourcesRSS :: Integer
-  , resourcesThreads :: Integer
-  }
-  deriving (Eq, Show, Generic)
-
-msgCustomOptions :: Aeson.Options
-msgCustomOptions = Aeson.defaultOptions
-  { Aeson.fieldLabelModifier = drop 9
-  , Aeson.constructorTagModifier = id
-  }
-
-instance Aeson.ToJSON Resources where
-  toJSON = Aeson.genericToJSON msgCustomOptions
-  toEncoding = Aeson.genericToEncoding msgCustomOptions
-
-instance Aeson.FromJSON Resources where
-  parseJSON = Aeson.genericParseJSON msgCustomOptions
-
---------------------------------------------------------------------------------
-
-data DataWithSlot = DataWithSlot
-  { slot :: Integer }
-  deriving Generic
-
-instance Aeson.ToJSON DataWithSlot where
-
-instance Aeson.FromJSON DataWithSlot where
---
+++ bench/stdout-tools/src/Cardano/Tracer/Reducer.hs
@@ -0,0 +1,150 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TypeFamilies #-}
+
+--------------------------------------------------------------------------------
+
+module Cardano.Tracer.Reducer
+  ( Reducer (..)
+  , CountLines (..)
+  , CountStartLeadershipCheckPlus (..)
+  , HeapChanges (..)
+  , MissedSlots (..)
+  , OneSecondSilences (..)
+  ) where
+
+--------------------------------------------------------------------------------
+
+-- base.
+import           Control.Monad (when, foldM)
+import           Data.Kind (Type)
+import           Data.Foldable (toList)
+-- package: time.
+import           Data.Time.Clock (UTCTime, NominalDiffTime, diffUTCTime)
+-- package: text.
+import qualified Data.Text as Text
+-- package: containers.
+import qualified Data.Sequence as Seq
+-- package: aeson.
+import qualified Data.Aeson as Aeson
+-- library.
+import qualified Cardano.Tracer.Trace as Trace
+
+--------------------------------------------------------------------------------
+
+class Show r => Reducer r where
+  type family Accum r :: Type
+  initialOf :: r -> Accum r
+  reducerOf :: r -> Accum r -> (Either Text.Text Trace.Trace) -> Accum r
+  showAns   :: r -> Accum r -> String
+  printAns  :: r -> Accum r -> IO ()
+  printAns r acc = putStrLn $ showAns r acc
+
+--------------------------------------------------------------------------------
+
+data CountLines = CountLines
+  deriving Show
+
+data CountStartLeadershipCheckPlus = CountStartLeadershipCheckPlus
+  deriving Show
+
+data HeapChanges = HeapChanges
+  deriving Show
+
+data MissedSlots = MissedSlots
+  deriving Show
+
+data OneSecondSilences = OneSecondSilences
+  deriving Show
+
+--------------------------------------------------------------------------------
+
+instance Reducer CountLines where
+  type instance Accum CountLines = Int
+  initialOf _ = 0
+  reducerOf _ = (\l _ -> l + 1)
+  showAns   _ = show
+
+instance Reducer CountStartLeadershipCheckPlus where
+  type instance Accum CountStartLeadershipCheckPlus = Int
+  initialOf _ = 0
+  reducerOf _ = (\l eitherTrace ->
+    case eitherTrace of
+      (Left _) -> l
+      (Right trace) ->
+        if Trace.ns trace == "Forge.Loop.StartLeadershipCheckPlus"
+        then l + 1
+        else l
+    )
+  showAns  _ = show
+
+instance Reducer HeapChanges where
+  type instance Accum HeapChanges = (Maybe Integer, Seq.Seq (UTCTime, Integer))
+  initialOf _ = (Nothing, Seq.empty)
+  reducerOf _ ans (Left _) = ans
+  reducerOf _ ans@(maybePrevHeap, sq) (Right trace) =
+    case Aeson.eitherDecodeStrictText (Trace.remainder trace) of
+      (Right !remainder) ->
+        -- TODO: Use `unsnoc` when available
+        let actualHeap = Trace.resourcesHeap $ Trace.remainderData remainder
+        in case maybePrevHeap of
+          Nothing -> (Just actualHeap, Seq.singleton (Trace.at trace, actualHeap))
+          (Just prevHeap) ->
+            if actualHeap == prevHeap
+            then ans
+            else (Just actualHeap, sq Seq.|> (Trace.at trace, actualHeap))
+      (Left _) -> ans
+  showAns _ = show
+  printAns _ (_, sq) = mapM_
+    (\(t,h) -> putStrLn $ show t ++ ": " ++ show h)
+    (toList sq)
+
+instance Reducer MissedSlots where
+  type instance Accum MissedSlots = (Maybe Integer, Seq.Seq Integer)
+  initialOf _ = (Nothing, Seq.empty)
+  reducerOf _ ans (Left _) = ans
+  reducerOf _ ans@(maybePrevSlot, !sq) (Right !(Trace.Trace _ "Forge.Loop.StartLeadershipCheckPlus" remainder)) =
+    case Aeson.eitherDecodeStrictText remainder of
+      (Right !dataWithSlot) ->
+        -- TODO: Use `unsnoc` when available
+        let actualSlot = Trace.slot $ Trace.remainderData dataWithSlot
+        in case maybePrevSlot of
+          Nothing -> (Just actualSlot, Seq.empty)
+          (Just prevSlot) ->
+            if actualSlot == prevSlot + 1
+            then (Just actualSlot, sq)
+            else (Just actualSlot, sq Seq.>< (Seq.fromList [(prevSlot+1)..(actualSlot-1)]))
+      (Left _) -> ans
+  reducerOf _ ans (Right _) = ans
+  showAns _ = show
+  printAns _ (_, sq) = do
+    ans <- foldM
+      (\maybePrevSlot lostSlot ->
+        case maybePrevSlot of
+          Nothing -> putStr (show lostSlot) >> return (Just lostSlot)
+          (Just prevSlot) ->
+            if prevSlot + 1 == lostSlot
+            then return (Just lostSlot)
+            else putStrLn (".." ++ show prevSlot) >> return Nothing
+      )
+      Nothing
+      (toList sq)
+    when (ans /= Nothing) (putStrLn "")
+
+instance Reducer OneSecondSilences where
+  type instance Accum OneSecondSilences = (Maybe Trace.Trace, Seq.Seq (NominalDiffTime, Trace.Trace, Trace.Trace))
+  initialOf _ = (Nothing, Seq.empty)
+  reducerOf _ (Nothing, sq) (Left _) = (Nothing, sq)
+  reducerOf _ (Nothing, sq) (Right trace) = (Just trace, sq)
+  reducerOf _ (Just prevTrace, sq) (Left _) = (Just prevTrace, sq)
+  reducerOf _ (Just prevTrace, sq) (Right trace) =
+    let diffTime = diffUTCTime (Trace.at trace) (Trace.at prevTrace)
+    in if diffTime >  fromInteger 2
+    then (Just trace, sq Seq.|> (diffTime, prevTrace, trace))
+    else (Just trace, sq)
+  showAns   _ = show
+  printAns _ (_,sq) = mapM_
+    (\(ndt, t1, _) ->
+      putStrLn $ (show ndt) ++ " (" ++ (show $ Trace.at t1) ++ ")"
+    )
+    (toList sq)
