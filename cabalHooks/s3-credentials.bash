45759954e4ec5f05bcdef629f7e87b2c191b491d
Moritz Angermann <moritz.angermann@gmail.com>
2024-05-08 16:20:23 +0800

    add hooks directly to PR

---
+++ cabalHooks/s3-credentials.bash
@@ -0,0 +1,5 @@
+CACHE_BUCKET="cabal-cache"  # Replace with your bucket name
+export AWS_ACCESS_KEY_ID=$CABAL_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$CABAL_AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=auto
+AWS_ENDPOINT="https://c1fe99e5a421ac98af478e51dda5db4f.r2.cloudflarestorage.com"
