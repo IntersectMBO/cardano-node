b4296ed7d92582f6a99ed2a1158fec241ef0572d
Michael Karg <michael.karg@iohk.io>
2024-05-21 18:16:33 +0200

    wb | tracing: silence some UTxO-HD specific trace msgs

--- nix/workbench/service/tracing.nix
+++ nix/workbench/service/tracing.nix
@@ -89,6 +89,17 @@ let
       "TxSubmission.TxOutbound".severity = "Notice";
       "Version.NodeVersion".severity = "Info";
 
+      ## These messages are UTxO-HD specific. On a regular node, the tracing system might warn at startup about config incosistencies (as those tracers do not exist).
+      ## This warning is expected, and can be safely ignored.
+      ## Silencing the tracers below aims at having a comparable log line rates (msgs per second) on UTxO-HD and regular nodes.
+      "ChainDB.LedgerEvent.Forker".severity = "Silence";
+      "Mempool.MempoolAttemptAdd".severity = "Silence";
+      "Mempool.MempoolAttemptingSync".severity = "Silence";
+      "Mempool.MempoolLedgerFound".severity = "Silence";
+      "Mempool.MempoolLedgerNotFound".severity = "Silence";
+      "Mempool.MempoolSyncDone".severity = "Silence";
+      "Mempool.MempoolSyncNotNeeded".severity = "Silence";
+
       ## enable this to investigate tx validation errors, e.g. fee to small for Plutus script txns
       ## comes with too much overhead to be the default for benchmarks
       # "Mempool.RejectedTx".detail = "DDetailed";
