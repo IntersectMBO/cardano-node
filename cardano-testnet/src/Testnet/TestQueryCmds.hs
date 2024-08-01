9004be22d64857a4a54657e2755d7544889edd4a
Pablo Lamela <pablo.lamela@iohk.io>
2024-08-01 14:31:32 +0200

    Add comments to `TestQueryCmds`

--- cardano-testnet/src/Testnet/TestQueryCmds.hs
+++ cardano-testnet/src/Testnet/TestQueryCmds.hs
@@ -12,9 +12,12 @@ import           Cardano.CLI.EraBased.Commands.Query (QueryCmds (..))
 
 import           Testnet.TestEnumGenerator (genAllConstructorsList, genTestType)
 
+-- | A datatype with the same constructors as 'QueryCmds', but with a "Test" prefix and no arguments.
+-- The generated type is called 'TestQueryCmds'.
 $(genTestType ''QueryCmds)
-
+-- | A list of all constructors of 'TestQueryCmds', which are nullary.
 $(genAllConstructorsList ''TestQueryCmds)
 
+-- | Maps a function over all constructors of 'TestQueryCmds' and sequences the results over a monad.
 forallQueryCommands :: Monad m => (TestQueryCmds -> m a) -> m ()
 forallQueryCommands f = mapM_ f allTestQueryCmdsConstructors
