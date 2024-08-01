daf6a7a6e02a982e964086072077e2fb2363f6ba
Pablo Lamela <pablo.lamela@iohk.io>
2024-08-01 15:26:01 +0200

    Improve comment for `allTestQueryCmdsConstructors`

--- cardano-testnet/src/Testnet/TestQueryCmds.hs
+++ cardano-testnet/src/Testnet/TestQueryCmds.hs
@@ -15,7 +15,9 @@ import           Testnet.TestEnumGenerator (genAllConstructorsList, genTestType)
 -- | A datatype with the same constructors as 'QueryCmds', but with a "Test" prefix and no arguments.
 -- The generated type is called 'TestQueryCmds'.
 $(genTestType ''QueryCmds)
+
 -- | A list of all constructors of 'TestQueryCmds', which are nullary.
+-- The generated list is called 'allTestQueryCmdsConstructors'.
 $(genAllConstructorsList ''TestQueryCmds)
 
 -- | Maps a function over all constructors of 'TestQueryCmds' and sequences the results over a monad.
