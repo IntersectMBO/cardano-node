fe53db85bc00b2b5c02e29b9aff0809ccb85bdc9
Nadia Yvette Chambers <nadia.chambers@iohk.io>
2024-08-07 15:28:02 +0000

    hlint-bench: pass --max-jobs, --cores more places
    nix.mk had a few invocations of nix I needed to sweep

--- nix.mk
+++ nix.mk
@@ -3,19 +3,19 @@ bump-cardano-node-workbench:
 bump-node-measured:
 	nix flake lock --update-input node-measured
 bump-cardano-deployment: ## Sync the flake.lock to the CI check
-	nix run nixpkgs#nixUnstable -- build .#hydraJobs.cardano-deployment
+	nix run --cores 0 --max-jobs $(JOBS) nixpkgs#nixUnstable -- build .#hydraJobs.cardano-deployment
 membench-1:    ## Membench:  one iteration, current commit
-	nix build .#membench-node-this-1.batch-report      --out-link result-batch-1-report
+	nix build --cores 0 --max-jobs $(JOBS) .#membench-node-this-1.batch-report      --out-link result-batch-1-report
 membench-1-at: ## Membench:  one iteration, set commit by:  make membench-1-at REV=[master]
-	nix build .#membench-node-measured-1.batch-report  --out-link result-batch-1-report --override-input node-measured github:intersectmbo/cardano-node/${REV}
+	nix build --cores 0 --max-jobs $(JOBS) .#membench-node-measured-1.batch-report  --out-link result-batch-1-report --override-input node-measured github:intersectmbo/cardano-node/${REV}
 membench-5:    ## Membench:  5 iterations, current commit
-	nix build .#membench-node-this-5.batch-report      --out-link result-batch-5-report
+	nix build --cores 0 --max-jobs $(JOBS) .#membench-node-this-5.batch-report      --out-link result-batch-5-report
 membench-5-at: ## Membench:  5 iterations, set commit by:  make membench-5-at REV=[master]
-	nix build .#membench-node-this-5.batch-report      --out-link result-batch-5-report --override-input node-measured github:intersectmbo/cardano-node/${REV}
+	nix build --cores 0 --max-jobs $(JOBS) .#membench-node-this-5.batch-report      --out-link result-batch-5-report --override-input node-measured github:intersectmbo/cardano-node/${REV}
 
 CI_TEST_NIXATTR = workbench-ci-test
 workbench-ci-test smoke: ## Workbench:  test a-la Hydra, the ci-test profile, full Nix engaged
-	nix build --out-link result-ci-test '.#hydraJobs.native.$(CI_TEST_NIXATTR)' --cores 0 ${EXTRA_ARGS}
+	nix build --cores 0 --max-jobs $(JOBS) --out-link result-ci-test '.#hydraJobs.native.$(CI_TEST_NIXATTR)' ${EXTRA_ARGS}
 	ID=`jq -r .meta.tag result-ci-test/meta.json`; test -e "run/$$ID" || mv result-ci-test "run/$$ID"
 
 ##  Use the -keep targets to debug failures:
