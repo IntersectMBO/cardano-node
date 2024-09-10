5cca45209f2285bead7d8e3baccc052c6db90330
Michael Karg <michael.karg@iohk.io>
2024-09-10 16:40:56 +0200

    trace-forward: remove potentially leaky continuation passing - datapoints

--- trace-forward/src/Trace/Forward/Protocol/DataPoint/Forwarder.hs
+++ trace-forward/src/Trace/Forward/Protocol/DataPoint/Forwarder.hs
@@ -17,7 +17,7 @@ data DataPointForwarder m a = DataPointForwarder
   { -- | The acceptor sent us a request for new 'DataPoint's.
     recvMsgDataPointsRequest
       :: [DataPointName]
-      -> m (DataPointValues, DataPointForwarder m a)
+      -> m DataPointValues
 
     -- | The acceptor terminated. Here we have a pure return value, but we
     -- could have done another action in 'm' if we wanted to.
@@ -30,19 +30,21 @@ dataPointForwarderPeer
   :: Monad m
   => DataPointForwarder m a
   -> Peer DataPointForward 'AsServer 'StIdle m a
-dataPointForwarderPeer DataPointForwarder{recvMsgDataPointsRequest, recvMsgDone} =
-  -- In the 'StIdle' state the forwarder is awaiting a request message
-  -- from the acceptor.
-  Await (ClientAgency TokIdle) $ \case
-    -- The acceptor sent us a request for new 'DataPoint's, so now we're
-    -- in the 'StBusy' state which means it's the forwarder's turn to send
-    -- a reply.
-    MsgDataPointsRequest request -> Effect $ do
-      (reply, next) <- recvMsgDataPointsRequest request
-      return $ Yield (ServerAgency TokBusy)
-                     (MsgDataPointsReply reply)
-                     (dataPointForwarderPeer next)
+dataPointForwarderPeer DataPointForwarder{recvMsgDataPointsRequest, recvMsgDone} = go
+  where
+    go =
+      -- In the 'StIdle' state the forwarder is awaiting a request message
+      -- from the acceptor.
+      Await (ClientAgency TokIdle) $ \case
+        -- The acceptor sent us a request for new 'DataPoint's, so now we're
+        -- in the 'StBusy' state which means it's the forwarder's turn to send
+        -- a reply.
+        MsgDataPointsRequest request -> Effect $ do
+          reply <- recvMsgDataPointsRequest request
+          return $ Yield (ServerAgency TokBusy)
+                         (MsgDataPointsReply reply)
+                         go
 
-    -- The acceptor sent the done transition, so we're in the 'StDone' state
-    -- so all we can do is stop using 'done', with a return value.
-    MsgDone -> Effect $ Done TokDone <$> recvMsgDone
+        -- The acceptor sent the done transition, so we're in the 'StDone' state
+        -- so all we can do is stop using 'done', with a return value.
+        MsgDone -> Effect $ Done TokDone <$> recvMsgDone
