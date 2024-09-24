6274c26aefc8085ebc6974aaa29b7b3bca65ea2f
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2024-09-24 09:27:02 +0200

    cardano-testnet | Reenable chairman test

--- cardano-node-chairman/test/Spec/Chairman/Cardano.hs
+++ cardano-node-chairman/test/Spec/Chairman/Cardano.hs
@@ -4,21 +4,20 @@
 
 module Spec.Chairman.Cardano where
 
-import           Cardano.Testnet (NodeRuntime (nodeName), allNodes,
-                   cardanoTestnetDefault, mkConf)
+import           Cardano.Testnet (allNodes, cardanoTestnetDefault, mkConf)
 
 import           Data.Default.Class
+
 import           Testnet.Property.Util (integrationRetryWorkspace)
 
 import qualified Hedgehog as H
 
 import           Spec.Chairman.Chairman (chairmanOver)
 
--- TODO: Conway broken in conway
 hprop_chairman :: H.Property
 hprop_chairman = integrationRetryWorkspace 2 "cardano-chairman" $ \tempAbsPath' -> do
   conf <- mkConf tempAbsPath'
 
-  allNodes' <- fmap nodeName . allNodes <$> cardanoTestnetDefault def def conf
+  allNodes' <- allNodes <$> cardanoTestnetDefault def def conf
 
   chairmanOver 120 50 conf allNodes'
