32a434711e8802faf3473f7d6ce92ad4d2fab792
Your Name <you@example.com>
2024-09-30 07:20:12 +0000

    haskellNix | use mgmeier:speceval-backport constraining GHC9.x speculative evaluation

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       flake = false;
     };
     haskellNix = {
-      url = "github:input-output-hk/haskell.nix";
+      url = "github:mgmeier/haskell.nix/?ref=speceval-backport";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.hackage.follows = "hackageNix";
     };
