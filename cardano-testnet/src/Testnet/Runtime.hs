bb75b2c16ce74f6acfe56b1370cf60109798c945
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2024-10-11 13:12:53 +0200

    Remove redundant functions used for starting a process

--- cardano-testnet/src/Testnet/Runtime.hs
+++ cardano-testnet/src/Testnet/Runtime.hs
@@ -60,7 +60,7 @@ import qualified Hedgehog.Extras.Test.Concurrent as H
 
 data NodeStartFailure
   = ProcessRelatedFailure ProcessError
-  | ExecutableRelatedFailure ExecutableError
+  | ExecutableRelatedFailure SomeException
   | FileRelatedFailure IOException
   | NodeExecutableError (Doc Ann)
   | NodeAddressAlreadyInUseError (Doc Ann)
@@ -143,8 +143,8 @@ startNode tp node ipv4 port testnetMagic nodeCmd = GHC.withFrozenCallStack $ do
     let socketAbsPath = H.sprocketSystemName sprocket
 
     nodeProcess
-      <- firstExceptT ExecutableRelatedFailure
-           $ hoistExceptT liftIO $ procNode $ mconcat
+      <- newExceptT . fmap (first ExecutableRelatedFailure) . try
+           $ procNode $ mconcat
                          [ nodeCmd
                          , [ "--socket-path", H.sprocketArgumentName sprocket
                            , "--port", show port
