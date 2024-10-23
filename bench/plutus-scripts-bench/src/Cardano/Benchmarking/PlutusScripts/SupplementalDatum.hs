007c841c1b34871f29da4aae3ff697c623710f54
Jordan Millar <jordan.millar@iohk.io>
2024-10-23 13:36:39 -0400

    This script checks for the supplementary datum and the datum that existed at the script address already. Succeeds

--- bench/plutus-scripts-bench/src/Cardano/Benchmarking/PlutusScripts/SupplementalDatum.hs
+++ bench/plutus-scripts-bench/src/Cardano/Benchmarking/PlutusScripts/SupplementalDatum.hs
@@ -41,12 +41,15 @@ import qualified Data.ByteString.Short as SBS
 
 import           Language.Haskell.TH
 import           Language.Haskell.TH.Syntax
--- import qualified PlutusTx.Builtins.HasOpaque as PlutusTx
+import qualified PlutusTx.Builtins as PlutusTx
+import qualified PlutusTx.AssocMap as PlutusTx
+import qualified PlutusLedgerApi.V1.Scripts as V1
+import qualified PlutusTx.Builtins.HasOpaque as PlutusTx
 -- import qualified PlutusTx.Maybe as PlutusTx
--- import qualified PlutusLedgerApi.V3.Contexts as V3
+import qualified PlutusLedgerApi.V3.Contexts as V3
 import           PlutusTx.Prelude as Tx hiding (Semigroup (..), (.), (<$>))
 import PlutusTx
-import qualified PlutusTx.Foldable as PlutusTx
+-- import qualified PlutusTx.Foldable as PlutusTx
 import qualified PlutusTx.Prelude as PlutusTx
 
 scriptName :: Haskell.String
@@ -59,18 +62,32 @@ script = mkPlutusBenchScript scriptName (toScriptInAnyLang (PlutusScript PlutusS
 {-# INLINABLE typedValidator #-}
 typedValidator :: V3.ScriptContext -> Bool
 typedValidator scriptContext = 
-    numDatums PlutusTx.== 2
+    PlutusTx.elem datum1 datums &&
+    PlutusTx.elem datum0 datums
   where
     -- supplementalDatumHash = cardano-cli latest transaction hash-script-data --script-data-value 1
     -- FAILS TO FIND THIS: supplementalDatumHash = V3.DatumHash (PlutusTx.stringToBuiltinByteString "ee155ace9c40292074cb6aff8c9ccdd273c81648ff1149ef36bcea6ebb8a3e25")
     -- FAILS TO FIND THIS: supplementalDatumHash = V3.DatumHash (PlutusTx.stringToBuiltinByteString "03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314")
     -- Fails to find either datum. So we need to see how big the datum map is
     -- Return to this once you confirm there are two datums in the datum map
-    -- supplementalDatumHash = V3.DatumHash (PlutusTx.stringToBuiltinByteString "???")
-    -- mSupplementalDatum = V3.findDatum supplementalDatumHash txInfo
+    supplementalDatumHash = datum1Hash
+    _mSupplementalDatum = V3.findDatum supplementalDatumHash txInfo
     txInfo = V3.scriptContextTxInfo scriptContext
     datumMap = V3.txInfoData txInfo
-    numDatums = PlutusTx.length datumMap
+    datums = PlutusTx.elems datumMap
+
+{-# INLINABLE datum1 #-}
+datum1 :: V1.Datum 
+datum1 = V1.Datum (PlutusTx.mkI 1)
+
+{-# INLINABLE datum0 #-}
+datum0 :: V1.Datum
+datum0 = V1.Datum (PlutusTx.mkI 0)
+
+-- Looking up by hash fails. Not sure why 
+{-# INLINABLE datum1Hash #-}
+datum1Hash :: V3.DatumHash 
+datum1Hash = V3.DatumHash $ PlutusTx.encodeUtf8 $ PlutusTx.stringToBuiltinString "03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314"
 
 untypedValidator :: BuiltinData -> BuiltinUnit 
 untypedValidator ctx = 
