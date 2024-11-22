05eefd49038244d00405291570fb9541c31305b4
Clément Hurlin <clement.hurlin@moduscreate.com>
2024-11-22 12:17:17 +0100

    cardano-testnet: remove code that was dead before this PR

--- cardano-testnet/src/Testnet/Start/Byron.hs
+++ cardano-testnet/src/Testnet/Start/Byron.hs
@@ -7,8 +7,6 @@
 
 module Testnet.Start.Byron
   ( createByronGenesis
-  , createByronUpdateProposal
-  , createByronUpdateProposalVote
   , byronDefaultGenesisOptions
   ) where
 
@@ -66,35 +64,3 @@ createByronGenesis testnetMagic' startTime testnetOptions pParamFp genOutputDir 
     , "--protocol-parameters-file", pParamFp
     , "--genesis-output-dir", genOutputDir
     ]
-
-createByronUpdateProposal
-  :: (MonadTest m, MonadCatch m, MonadIO m, HasCallStack)
-  => Int -> String -> String -> Int -> m ()
-createByronUpdateProposal testnetMagic' signingKeyFp updateProposalFp ptclMajorVersion =
-  withFrozenCallStack $ execCli_
-    [ "byron", "governance", "create-update-proposal"
-    , "--filepath", updateProposalFp
-    , "--testnet-magic", show testnetMagic'
-    , "--signing-key", signingKeyFp
-    , "--protocol-version-major", show ptclMajorVersion
-    , "--protocol-version-minor", "0"
-    , "--protocol-version-alt", "0"
-    , "--application-name", "cardano-sl"
-    , "--software-version-num", "1"
-    , "--system-tag", "linux"
-    , "--installer-hash", "0"
-    ]
-
-createByronUpdateProposalVote
-  :: (MonadTest m, MonadCatch m, MonadIO m, HasCallStack)
-  => Int -> String -> String -> String -> m ()
-createByronUpdateProposalVote testnetMagic' updateProposalFp signingKey outputFp =
-    withFrozenCallStack $ execCli_
-      [ "byron", "governance", "create-proposal-vote"
-      , "--proposal-filepath", updateProposalFp
-      , "--testnet-magic", show testnetMagic'
-      , "--signing-key", signingKey
-      , "--vote-yes"
-      , "--output-filepath", outputFp
-      ]
-
