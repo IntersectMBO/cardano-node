cedc23fd91da625790679bc957d92db329ae1990
Marcin Szamotulski <coot@coot.me>
2024-12-06 06:48:26 +0100

    Added diffusionMode to local root peers group configuration
    "diffusionMode" can be either `"InitiatorOnly"` or
    `"InitiatorAndResponder"`.  If not given, the latter is the default
    - for backward compatibility.  When "InitiatorOnly" is used, the
    connections to these local roots will only negotiate `InitiatorOnly`
    mode (it won't be possible to be reused by the other side) and will not
    bind to the server address, e.g. it will be made from an ephemeral port.
    See https://github.com/IntersectMBO/ouroboros-network/issues/5020 why
    this feature was requested by some SPOs.
    
    An example configuration:
    ```json
    { "localRoots":
        [ { "accessPoints":
              [ { "address": "10.0.0.1"
                , "port": 3001
                }
              ]
          , "advertise": false
          , "diffusionMode": "InitiatorOnly"
          , "warmValency": 1
          , "hotValency": 1
          }
        , { "accessPoints":
              [ { "address": "10.0.0.2"
                , "port": 3001
                }
              ]
          , "advertise": true
          , "diffusionMode": "InititiatorAndResponder"
          , "warmValency": 1
          , "hotValency": 1
          }
        ]
    , "publicRoots": []
    , "useLedgerAfterSlot": -1
    }
    ```

--- cardano-node/src/Cardano/Node/Configuration/TopologyP2P.hs
+++ cardano-node/src/Cardano/Node/Configuration/TopologyP2P.hs
@@ -31,7 +31,7 @@ import           Cardano.Node.Types
 import           Cardano.Logging (traceWith)
 import           Cardano.Tracing.OrphanInstances.Network ()
 import           Ouroboros.Network.ConsensusMode
-import           Ouroboros.Network.NodeToNode (PeerAdvertise (..))
+import           Ouroboros.Network.NodeToNode (DiffusionMode (..), PeerAdvertise (..))
 import           Ouroboros.Network.PeerSelection.Bootstrap (UseBootstrapPeers (..))
 import           Ouroboros.Network.PeerSelection.LedgerPeers.Type (LedgerPeerSnapshot (..),
                    UseLedgerPeers (..))
@@ -111,7 +111,7 @@ instance ToJSON RootConfig where
 rootConfigToRelayAccessPoint
   :: RootConfig
   -> [(RelayAccessPoint, PeerAdvertise)]
-rootConfigToRelayAccessPoint RootConfig { rootAccessPoints, rootAdvertise } =
+rootConfigToRelayAccessPoint RootConfig { rootAccessPoints, rootAdvertise  } =
     [ (ap, rootAdvertise) | ap <- rootAccessPoints ]
 
 
@@ -128,6 +128,8 @@ data LocalRootPeersGroup = LocalRootPeersGroup
   , trustable   :: PeerTrustable
     -- ^ 'trustable' configures whether the root should be trusted in fallback
     -- state.
+  , rootDiffusionMode :: DiffusionMode
+    -- ^ diffusion mode; used for local root peers.
   } deriving (Eq, Show)
 
 -- | Does not use the 'FromJSON' instance of 'RootConfig', so that
@@ -142,6 +144,9 @@ instance FromJSON LocalRootPeersGroup where
                   <*> pure hv
                   <*> o .:? "warmValency" .!= WarmValency v
                   <*> o .:? "trustable" .!= IsNotTrustable
+                      -- deserialise via NodeDiffusionMode
+                  <*> (maybe InitiatorAndResponderDiffusionMode getDiffusionMode
+                        <$> o .:? "diffusionMode")
 
 instance ToJSON LocalRootPeersGroup where
   toJSON lrpg =
@@ -151,6 +156,8 @@ instance ToJSON LocalRootPeersGroup where
       , "hotValency" .= hotValency lrpg
       , "warmValency" .= warmValency lrpg
       , "trustable" .= trustable lrpg
+        -- serialise via NodeDiffusionMode
+      , "diffusionMode" .= NodeDiffusionMode (rootDiffusionMode lrpg)
       ]
 
 newtype LocalRootPeersGroups = LocalRootPeersGroups
@@ -280,10 +287,11 @@ isValidTrustedPeerConfiguration (RealNodeTopology (LocalRootPeersGroups lprgs) _
       UseBootstrapPeers (_:_) -> True
   where
     anyTrustable =
-      any (\(LocalRootPeersGroup lr _ _ pt) -> case pt of
+      any (\LocalRootPeersGroup {localRoots, trustable} ->
+            case trustable of
               IsNotTrustable -> False
               IsTrustable    -> not
                               . null
                               . rootAccessPoints
-                              $ lr
+                              $ localRoots
           ) lprgs
