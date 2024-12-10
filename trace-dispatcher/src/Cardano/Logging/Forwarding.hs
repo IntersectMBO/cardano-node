d5ae3181a7cf21b570a9a7398fc84406a355eecb
soonsouth <cuibuwei@163.com>
2024-12-10 11:24:00 +0800

    chore: fix some typos in comment
    Signed-off-by: soonsouth <cuibuwei@163.com>

--- trace-dispatcher/src/Cardano/Logging/Forwarding.hs
+++ trace-dispatcher/src/Cardano/Logging/Forwarding.hs
@@ -141,7 +141,7 @@ launchForwarders iomgr magic
                  ekgStore sink dpStore tracerSocketMode =
   -- If 'tracerSocketMode' is not specified, it's impossible to establish
   -- network connection with acceptor application (for example, 'cardano-tracer').
-  -- In this case, we should not lauch forwarders.
+  -- In this case, we should not launch forwarders.
   case tracerSocketMode of
     Nothing -> return ()
     Just (socketPath, mode) ->
