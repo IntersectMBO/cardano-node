80504e5e5376e1b38b3f464e8ec7dbbf75fdccb0
Javier Sagredo <jasataco@gmail.com>
2025-01-10 16:53:01 +0100

    Adapt to https://github.com/IntersectMBO/ouroboros-consensus/pull/1356

--- cardano-node/src/Cardano/Tracing/OrphanInstances/Shelley.hs
+++ cardano-node/src/Cardano/Tracing/OrphanInstances/Shelley.hs
@@ -1332,13 +1332,6 @@ instance ToJSON ShelleyNodeToNodeVersion where
   toJSON ShelleyNodeToNodeVersion1 = String "ShelleyNodeToNodeVersion1"
 
 instance ToJSON ShelleyNodeToClientVersion where
-  toJSON ShelleyNodeToClientVersion1 = String "ShelleyNodeToClientVersion1"
-  toJSON ShelleyNodeToClientVersion2 = String "ShelleyNodeToClientVersion2"
-  toJSON ShelleyNodeToClientVersion3 = String "ShelleyNodeToClientVersion3"
-  toJSON ShelleyNodeToClientVersion4 = String "ShelleyNodeToClientVersion4"
-  toJSON ShelleyNodeToClientVersion5 = String "ShelleyNodeToClientVersion5"
-  toJSON ShelleyNodeToClientVersion6 = String "ShelleyNodeToClientVersion6"
-  toJSON ShelleyNodeToClientVersion7 = String "ShelleyNodeToClientVersion7"
   toJSON ShelleyNodeToClientVersion8 = String "ShelleyNodeToClientVersion8"
   toJSON ShelleyNodeToClientVersion9 = String "ShelleyNodeToClientVersion9"
   toJSON ShelleyNodeToClientVersion10 = String "ShelleyNodeToClientVersion10"
