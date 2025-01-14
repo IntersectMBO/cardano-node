84af3a91391aaf5be0c9b49aa7a8dd2d48402504
Jürgen Nicklisch-Franken <jnf@arcor.de>
2025-01-14 14:16:39 +0100

    trace-dispatcher: ledger metrics frequency

--- trace-dispatcher/src/Cardano/Logging/Types.hs
+++ trace-dispatcher/src/Cardano/Logging/Types.hs
@@ -480,6 +480,8 @@ data TraceConfig = TraceConfig {
   , tcPeerFrequency  :: Maybe Int
     -- | Optional resource trace frequency in milliseconds.
   , tcResourceFrequency :: Maybe Int
+    -- | Optional ledger metrics frequency in milliseconds.
+  , tcLedgerMetricsFrequency :: Maybe Int
 }
   deriving (Eq, Ord, Show)
 
