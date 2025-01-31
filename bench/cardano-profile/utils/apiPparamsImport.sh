a3ecaf5902c3ba650e41b30e40cae6ec91f1b164
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2025-01-31 17:21:27 +0000

    wb | cardano-profile utils

--- bench/cardano-profile/utils/apiPparamsImport.sh
+++ bench/cardano-profile/utils/apiPparamsImport.sh
@@ -4,6 +4,6 @@ jq --null-input '
   include "api-pparams-to-genesis";
 
   cardano_api_pparams_to_geneses($pparams[0]; $desc)
-  ' --slurpfile pparams ${1:?USAGE: $0 CARDANO-API-PPARAMS-JSON} \
-    --arg       desc    "Imported from $(basename $1)" \
+  ' --slurpfile pparams "${1:?USAGE: $0 CARDANO-API-PPARAMS-JSON}" \
+    --arg       desc    "Imported from $(basename "$1")" \
     --sort-keys
