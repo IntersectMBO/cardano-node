036bb31f7692b2654c1d2a83cf418929b0c7774a
John Lotoski <john.lotoski@iohk.io>
2025-02-19 21:20:02 -0600

    byron-to-alonzo: fix mint and burn during mary era

--- scripts/byron-to-alonzo/mint.sh
+++ scripts/byron-to-alonzo/mint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-set -e
+set -euo pipefail
 
 # This script creates, signs, and submits a transaction that creates some new tokens.
 # It uses the output of the transaction from update-4.sh.
@@ -8,7 +7,7 @@ set -e
 [ -n "${DEBUG:-}" ] && set -x
 
 ROOT=example
-COINS_IN_INPUT=1000000000
+SPLIT_OUTPUT_ALLOC=1000000000
 pushd ${ROOT}
 
 export CARDANO_NODE_SOCKET_PATH=node-bft1/node.sock
@@ -29,29 +28,48 @@ rm -f $SCRIPT
   echo "}"
 } >> $SCRIPT
 
-TXID3=$(cardano-cli transaction txid --tx-body-file tx3.txbody)
+TXID3=$(cardano-cli mary transaction txid --tx-file tx3.tx)
 
-POLICYID=$(cardano-cli transaction policyid --script-file ma/policy.script)
+POLICYID=$(cardano-cli mary transaction policyid --script-file ma/policy.script)
+TOKEN_NAME="couttscoin"
+TOKEN_NAME_HEX=$(printf "%s" "$TOKEN_NAME" | od -A n -t x1 | tr -d ' \n')
 
-cardano-cli transaction build-raw \
-            --mary-era \
-            --fee 0 \
+# Obtain the input lovelace dynamically to reduce change calc complexity
+TOTAL_INPUT_LOVELACE=$(
+  cardano-cli query utxo --whole-utxo --output-json \
+    | jq -er '[to_entries[] | select(.value.value | length == 1) | .value.value.lovelace] | add')
+
+# Slight over-estimate on the fee
+FEE=200000
+MIN_UTXO=1000000
+CHANGE=$((
+  + TOTAL_INPUT_LOVELACE
+  - SPLIT_OUTPUT_ALLOC
+  - MIN_UTXO
+  - FEE
+))
+
+# Overwrite the prior tx3 tx out file so update-5.sh still obtains the correct utxo after mint/burn
+cardano-cli mary transaction build-raw \
             --tx-in "$TXID3#0" \
-            --tx-out="$(cat addresses/user1.addr)+$((COINS_IN_INPUT / 2))+5 $POLICYID.couttscoin" \
-            --mint="5 $POLICYID.couttscoin" \
-            --out-file tx4.txbody
+            --tx-in "$TXID3#1" \
+            --tx-in "$TXID3#2" \
+            --tx-out "$(cat addresses/user1.addr)+$((SPLIT_OUTPUT_ALLOC / 2))" \
+            --tx-out "$(cat addresses/user1.addr)+$((SPLIT_OUTPUT_ALLOC / 2))" \
+            --tx-out "$(cat addresses/user1.addr)+$CHANGE" \
+            --tx-out="$(cat addresses/user1.addr)+$MIN_UTXO+5 $POLICYID.$TOKEN_NAME_HEX" \
+            --mint="5 $POLICYID.$TOKEN_NAME_HEX" \
+            --mint-script-file $SCRIPT \
+            --fee "$FEE" \
+            --out-file tx3.txbody
 
-cardano-cli transaction sign \
+cardano-cli mary transaction sign \
             --signing-key-file addresses/user1.skey \
             --signing-key-file ma/policy.skey \
-            --script-file $SCRIPT \
             --testnet-magic 42 \
-            --tx-body-file  tx4.txbody \
-            --out-file      tx4.tx
+            --tx-body-file  tx3.txbody \
+            --out-file      tx3.tx
 
-cardano-cli transaction submit --tx-file  tx4.tx --testnet-magic 42
-
-
+cardano-cli mary transaction submit --tx-file tx3.tx --testnet-magic 42
 
 popd
-
