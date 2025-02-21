9b68efc9c1d35aa023da131d672449684aade8d0
Nadia Chambers <nadia.chambers@iohk.io>
2025-02-21 07:22:35 -0600

    add new option to stock config and nix

--- nix/nixos/cardano-tracer-service.nix
+++ nix/nixos/cardano-tracer-service.nix
@@ -76,6 +76,7 @@ in pkgs.commonLib.defServiceModule
         prometheus      = opt    attrs {}      "Prometheus overrides: see cardano-tracer documentation.";
         resourceFreq    = mayOpt int           "Frequency (1/ms) for tracing resource usage.";
         metricsHelp     = mayOpt str           "JSON file containing metrics help annotations for Prometheus";
+        tracerConfigDebug = mayOpt bool        "debug output";
 
         ### Here be dragons, on the other hand..
         configFile      = mayOpt str
