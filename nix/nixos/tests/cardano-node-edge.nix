a2f28217dc66c178101d710f52c6533f3eb32ad5
John Lotoski <john.lotoski@iohk.io>
2025-03-19 17:37:05 -0500

    nixosTests: update the cardano-node-edge test for new tracing

--- nix/nixos/tests/cardano-node-edge.nix
+++ nix/nixos/tests/cardano-node-edge.nix
@@ -1,65 +1,66 @@
-{ pkgs, ... }:
-with pkgs;
-{
+{pkgs, ...}:
+with pkgs; let
+  environment = "mainnet";
+in {
   name = "cardano-node-edge-test";
   nodes = {
-    machine = { config, ... }: {
+    machine = {config, ...}: {
       nixpkgs.pkgs = pkgs;
       imports = [
         ../.
       ];
-      services.cardano-node = {
-        enable = true;
-        port = 3001;
-        hostAddr = "127.0.0.1";
-        environment = "mainnet";
-        topology = commonLib.mkEdgeTopologyP2P {
-          edgeNodes = [
-            {
-              addr = "127.0.0.1";
-              port = 3001;
-            }
-          ];
+
+      services = {
+        cardano-node = {
+          inherit environment;
+
+          enable = true;
+          port = 3001;
+          hostAddr = "127.0.0.1";
+          topology = commonLib.mkEdgeTopologyP2P {
+            edgeNodes = [
+              {
+                addr = "127.0.0.1";
+                port = 3001;
+              }
+            ];
+          };
+
+          # Default tracing system logging is to stdout and default prometheus
+          # metrics are exported to localhost on port 12798.
+          nodeConfig = config.services.cardano-node.environments.${environment}.nodeConfig;
         };
-        nodeConfig = config.services.cardano-node.environments.${config.services.cardano-node.environment}.nodeConfig // {
-          hasPrometheus = [ config.services.cardano-node.hostAddr 12798 ];
-          # Use Journald output:
-          setupScribes = [{
-            scKind = "JournalSK";
-            scName = "cardano";
-            scFormat = "ScText";
-          }];
-          defaultScribes = [
-            [
-              "JournalSK"
-              "cardano"
-            ]
-          ];
+
+        cardano-submit-api = {
+          enable = true;
+          port = 8101;
+          network = environment;
+          socketPath = config.services.cardano-node.socketPath 0;
         };
       };
-      systemd.services.cardano-node.serviceConfig.Restart = lib.mkForce "no";
-      services.cardano-submit-api = {
-        enable = true;
-        port = 8101;
-        network = "mainnet";
-        socketPath = config.services.cardano-node.socketPath 0;
+
+      systemd.services = {
+        cardano-node.serviceConfig.Restart = lib.mkForce "no";
+        cardano-submit-api.serviceConfig.SupplementaryGroups = "cardano-node";
       };
-      systemd.services.cardano-submit-api.serviceConfig.SupplementaryGroups = "cardano-node";
     };
   };
   testScript = ''
     start_all()
     machine.wait_for_unit("cardano-node.service")
     machine.wait_for_file("/run/cardano-node/node.socket")
-    machine.wait_for_open_port(12798)
+
+    # Re-enable once PrometheusSimple with node >= 10.3 is available
+    # machine.wait_for_open_port(12798)
+
     machine.wait_for_open_port(3001)
     machine.succeed("systemctl status cardano-node")
-    # FIXME reenable and check the cli syntax when https://github.com/intersectmbo/cardano-node/pull/4664 is merged
-    #machine.succeed(
-    #    "${cardanoNodePackages.cardano-cli}/bin/cardano-cli ping -h 127.0.0.1 -c 1 -q --json | ${jq}/bin/jq -c"
-    #)
+
+    machine.succeed(
+      "${cardanoNodePackages.cardano-cli}/bin/cardano-cli ping -h 127.0.0.1 -c 1 -q --json | ${jq}/bin/jq -c"
+    )
+
     machine.wait_for_open_port(8101)
     machine.succeed("systemctl status cardano-submit-api")
   '';
-
 }
