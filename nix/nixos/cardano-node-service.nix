75c83323834140f44eff37ec115b6ea7a13f4490
John Lotoski <john.lotoski@iohk.io>
2025-03-29 00:30:40 -0500

    bump: iohkNix for cardano-tracer env cfgs; adjust nixos svc default to new tracing

--- nix/nixos/cardano-node-service.nix
+++ nix/nixos/cardano-node-service.nix
@@ -583,7 +583,7 @@ in {
 
       useLegacyTracing = mkOption {
         type = bool;
-        default = true;
+        default = false;
         description = ''
           Use the legacy tracing, based on iohk-monitoring-framework.
         '';
