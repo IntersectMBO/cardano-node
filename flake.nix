712944d293d424afb948afcec0a953ce64b10311
John Lotoski <john.lotoski@iohk.io>
2025-04-24 13:05:25 -0500

    imp: add consensus snapshot-converter to pkgs and release bin

--- flake.nix
+++ flake.nix
@@ -130,7 +130,7 @@
       // (with project.hsPkgs; {
         # Add some executables from other relevant packages
         inherit (bech32.components.exes) bech32;
-        inherit (ouroboros-consensus-cardano.components.exes) db-analyser db-synthesizer db-truncater;
+        inherit (ouroboros-consensus-cardano.components.exes) db-analyser db-synthesizer db-truncater snapshot-converter;
         # Add cardano-node and cardano-cli with their git revision stamp.
         # Keep available an alternative without the git revision, like the other
         # passthru (profiled, asserted and eventlogged in nix/haskell.nix) that
