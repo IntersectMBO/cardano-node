4adf0d67275216e6192f18edab3f8e63b910913b
Nadia Chambers <nadia.chambers@iohk.io>
2025-04-25 15:06:22 +0200

    more SDU printing helpers

--- cardano-tracer/src/Cardano/Tracer/Acceptors/Utils.hs
+++ cardano-tracer/src/Cardano/Tracer/Acceptors/Utils.hs
@@ -20,7 +20,8 @@
 {-# OPTIONS_GHC -Wno-error=unused-imports #-}
 
 module Cardano.Tracer.Acceptors.Utils
-  ( handshakeCodec
+  ( fileOffsetsSDU
+  , handshakeCodec
   , notifyAboutNodeDisconnected
   , parseHandshakeLog
   , parseSDU
@@ -28,6 +29,8 @@ module Cardano.Tracer.Acceptors.Utils
   , parseSDUatOffset
   , prepareDataPointRequestor
   , prepareMetricsStores
+  , printSDU
+  , printSDUallOffsets
   , removeDisconnectedNode
   , decodeConfirm
   , decodeDone
@@ -41,13 +44,13 @@ import           Cardano.Logging (SeverityS (..))
 import           Cardano.Tracer.Handlers.Notifications.Types
 import           Cardano.Tracer.Handlers.Notifications.Utils
 #endif
-import           Cardano.Logging.Version (ForwardingVersion, forwardingVersionCodec, forwardingCodecCBORTerm)
+import           Cardano.Logging.Version (ForwardingVersion, forwardingVersionCodec)
 import           Cardano.Tracer.Environment
 import           Cardano.Tracer.Types
 import           Cardano.Tracer.Utils
 import qualified Network.Mux.Codec as Mux (decodeSDU)
 import qualified Network.Mux.Trace as Mux (Error (..))
-import qualified Network.Mux.Types as Mux (RemoteClockModel (..), SDU (..), SDUHeader (..))
+import qualified Network.Mux.Types as Mux (MiniProtocolNum (..), RemoteClockModel (..), SDU (..), SDUHeader (..))
 import           Network.TypedProtocol.Codec (Codec (..), DecodeStep (..), SomeMessage (..), runDecoder)
 import           Network.TypedProtocol.Core (IsActiveState (..), Protocol (..))
 import           Ouroboros.Network.Protocol.Handshake.Codec (codecHandshake)
@@ -64,21 +67,25 @@ import           Control.Arrow (ArrowChoice (..))
 -- potentially pure use in the decoding functions.
 import           Control.Concurrent.STM (atomically)
 import           Control.Concurrent.STM.TVar (TVar, modifyTVar', newTVarIO)
+import           Control.Exception (throw)
 import           Control.Monad.IO.Class (MonadIO (..))
 import           Control.Monad.Trans.Except (ExceptT, except, runExceptT, tryE)
 import           Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Bimap as BM
 import qualified Data.ByteString.Lazy as LBS (ByteString, hGet, readFile, splitAt)
-import           Data.Either.Extra (eitherToMaybe)
+import           Data.Either.Extra (eitherToMaybe, fromEither)
 import           Data.Functor ((<&>))
 import           Data.Kind (Type)
+import           Data.List.Extra (unsnoc)
 import qualified Data.Map.Strict as M
+import           Data.Maybe (fromJust)
 import qualified Data.Set as S
 import           Data.Time.Clock.POSIX (getPOSIXTime)
 #if RTVIEW
 import           Data.Time.Clock.System (getSystemTime, systemToUTCTime)
 #endif
 import           GHC.Generics (Generic (..))
+import           Numeric (showHex)
 import qualified System.Metrics as EKG
 import           System.Metrics.Store.Acceptor (MetricsLocalStore, emptyMetricsLocalStore)
 import           System.IO (Handle, IOMode (..), SeekMode (..))
@@ -237,7 +244,43 @@ parseFileSDUs filePath = do
       Mux.msHeader <$> parseSDUatOffset handle offset 8
     pure $ maybeSDUH <&> \sduH@Mux.SDUHeader {..} ->
       let newOffset = offset + fromIntegral mhLength
-       in ((sduH, newOffset), newOffset)
+       in ((sduH, offset), newOffset)
+
+infixr 8 <$$>
+(<$$>) :: forall (f :: Type -> Type) (g :: Type -> Type) (t :: Type) (t' :: Type) . ()
+  => Functor f
+  => Functor g
+  => (t -> t') -> f (g t) -> f (g t')
+f <$$> xss = fmap f <$> xss
+
+fileOffsetsSDU :: FilePath -> ExceptT Mux.Error IO [Integer]
+fileOffsetsSDU filePath = snd <$$> parseFileSDUs filePath
+
+printSDU :: Mux.SDUHeader -> Integer -> String
+printSDU Mux.SDUHeader {..} offset = unlines is''' where
+  showHex' n = "0x" <> showHex n ""
+  Mux.MiniProtocolNum mhNum' = mhNum
+  is''' :: [String]
+  is''' = [ "SDU header at off=" <> showHex' offset ] <> is''
+  is'' :: [String]
+  is'' | (frontList, backElt) <- fromJust $ unsnoc is'
+       = [ "struct sdu {" ] <> frontList <> [ backElt <> " };" ]
+  is'  :: [String]
+  is'  = ("\t" <>) . (<> ";") . unwords <$> is
+  is   :: [[String]]
+  is   = [ [ "uint32_t", "sdu_xmit", "="
+             , showHex' $ Mux.unRemoteClockModel mhTimestamp ]
+        , [ "uint16_t sdu_proto_num", "="
+             , show mhNum', "(" <> showHex' mhNum' <> ")" ]
+        , [ "uint16_t sdu_len", "="
+             , show mhLength, "(" <> showHex' mhLength <> ")" ]
+        , [ "bool", "sdu_init_or_resp", "=", show mhDir ]
+        , [ "const", "char", "*sdu_data", "=", "(nil)" ] ]
+
+printSDUallOffsets :: FilePath -> IO ()
+printSDUallOffsets filePath = fromEither . left throw <$> runExceptT monad where
+  monad = printSDU' <$$> parseFileSDUs filePath >>= mapM_ (liftIO . putStrLn)
+  printSDU' = head . lines . uncurry printSDU
 
 prepareDataPointRequestor
   :: TracerEnv
