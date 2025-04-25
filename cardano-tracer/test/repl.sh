d5be77f41c179603405217170e492fe5437439cc
Nadia Chambers <nadia.chambers@iohk.io>
2025-04-25 10:45:27 +0200

    tweak helper scripts for debug/etc. effort

--- cardano-tracer/test/repl.sh
+++ cardano-tracer/test/repl.sh
@@ -3,14 +3,14 @@ exec cabal --jobs=256 --keep-going --disable-documentation repl \
 	--repl-options="-ghci-script=$(pwd)/cardano-tracer/test/ghci.script" \
 	--repl-options=-fprint-potential-instances \
 	--repl-options=-Wno-error=unused-packages \
-	--repl-options=-Wno-unused-packages \
+	--repl-options=-Wno-error=missing-home-modules \
 	--build-depends bytestring \
 	--build-depends cborg \
 	--build-depends extra \
 	--build-depends network-mux \
 	--build-depends optparse-applicative-fork \
 	--build-depends ouroboros-network-framework \
-	--build-depends trace-dispatcher \
+	--build-depends transformers \
 	--build-depends typed-protocols \
 	--build-depends typed-protocols-examples \
-	cardano-tracer:exe:cardano-tracer 
+	cardano-tracer:lib:cardano-tracer
