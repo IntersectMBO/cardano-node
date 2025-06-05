643ca1b2503202ab6515cffe176bef4b12372db5
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2025-06-05 16:38:54 +0200

    Interrupt cardano nodes when `cardano-testnet` gets interrupted

---
+++ cardano-testnet/src/Testnet/Handlers.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+#if !defined(mingw32_HOST_OS)
+#define UNIX
+#endif
+
+module Testnet.Handlers
+  ( interruptNodesOnSigINT
+  ) where
+
+#ifdef UNIX
+import           Control.Monad
+import           System.Posix.Signals (Handler (..), installHandler, sigINT, raiseSignal)
+import           System.Process (interruptProcessGroupOf)
+#endif
+
+import           Testnet.Types
+
+interruptNodesOnSigINT :: [TestnetNode] -> IO ()
+#ifdef UNIX
+interruptNodesOnSigINT testnetNodes =
+  -- Interrupt cardano nodes when the main process is interrupted
+  void $ flip (installHandler sigINT) Nothing $ CatchOnce $ do
+    forM_ testnetNodes $ \TestnetNode{nodeProcessHandle} ->
+      interruptProcessGroupOf nodeProcessHandle
+    raiseSignal sigINT
+#else
+interruptNodesOnSigINT _testnetNodes = pure ()
+#endif
