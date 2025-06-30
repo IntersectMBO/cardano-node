5ab353dece788ccc9d0b8eac08c7fbf3da049a1b
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2025-06-30 15:46:45 +0000

    wb | add optional cgroup memory workload

---
+++ bench/cardano-profile/src/Cardano/Benchmarking/Profile/Workload/CGroupMemory.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+--------------------------------------------------------------------------------
+
+module Cardano.Benchmarking.Profile.Workload.CGroupMemory (
+  cgroupMemoryWorkload
+) where
+
+--------------------------------------------------------------------------------
+
+import           Prelude
+-- Package: self.
+import qualified Cardano.Benchmarking.Profile.Types as Types
+
+--------------------------------------------------------------------------------
+
+cgroupMemoryWorkload :: Types.Workload
+cgroupMemoryWorkload = Types.Workload {
+    Types.workloadName = "cgroup_memory"
+  , Types.parameters = mempty
+  , Types.entrypoints = Types.Entrypoints {
+      Types.pre_generator = Nothing
+    , Types.producers = "cgroup_memory"
+    }
+  , Types.wait_pools = True
+}
