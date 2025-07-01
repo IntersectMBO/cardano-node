62e292d992be608b6977c16297ef2221251918ff
John Lotoski <john.lotoski@iohk.io>
2025-07-01 15:56:29 -0500

    releaseArtifacts: add cardano-tracer to the bins as a musl build

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -114,7 +114,6 @@ let
       modules =
         [
           ({ lib, pkgs, ... }: {
-            packages.cardano-tracer.package.buildable = with pkgs.stdenv.hostPlatform; lib.mkForce (!isMusl);
             packages.cardano-node-chairman.components.tests.chairman-tests.buildable = lib.mkForce pkgs.stdenv.hostPlatform.isUnix;
             packages.plutus-tx-plugin.components.library.platforms = with lib.platforms; [ linux darwin ];
             packages.tx-generator.package.buildable = with pkgs.stdenv.hostPlatform; !isMusl;
