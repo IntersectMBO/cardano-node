4fca3c8c37b40be6c88bdd60a74a4e4483984c49
Nicolas BACQUEY <nicolas.bacquey@tweag.io>
2025-07-02 15:13:29 +0200

    Add test for `cardano-testnet` P2P topology files

--- cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/P2PTopology.hs
+++ cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/P2PTopology.hs
@@ -1,81 +1,56 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Cardano.Testnet.Test.DumpConfig
-  ( hprop_dump_config
+module Cardano.Testnet.Test.P2PTopology
+  ( hprop_p2p_topology
   ) where
 
-import           Cardano.Api (BlockNo (..), ChainTip (..), ShelleyGenesis, runExceptT,
-                   sgSystemStart)
-import           Cardano.Api.Byron (GenesisData (..))
-import qualified Cardano.Api.Byron as Byron
-
+import           Cardano.Api (BlockNo (..), ChainTip (..))
 import           Cardano.CLI.Type.Output (QueryTipLocalStateOutput (..))
-import           Cardano.Prelude (canonicalEncodePretty)
+import           Cardano.Node.Configuration.Topology (NodeId)
+import qualified Cardano.Node.Configuration.TopologyP2P as P2P
 import           Cardano.Testnet hiding (shelleyGenesisFile)
 
 import           Prelude
 
 import qualified Data.Aeson as A
-import           Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as B
 import           Data.Default.Class (def)
-import qualified Data.Time.Clock as Time
-import           GHC.Float (double2Int)
 import           System.Exit (ExitCode (..))
 import           System.FilePath ((</>))
 import qualified System.Process as IO
 
-import           Testnet.Components.Configuration (startTimeOffsetSeconds)
 import           Testnet.Process.Run (execCli', mkExecConfig)
 import           Testnet.Property.Util (integrationRetryWorkspace)
-import           Testnet.Start.Types (GenesisHashesPolicy (..), GenesisOptions (..),
-                   UserProvidedData (..), UserProvidedEnv (..))
+import           Testnet.Start.Types (GenesisOptions (..),
+                   UserProvidedData (..), UserProvidedEnv (..), TopologyType (..))
 
 import           Hedgehog ((===))
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras as H
 
+
 -- | Execute me with:
--- @DISABLE_RETRIES=1 cabal test cardano-testnet-test --test-options '-p "/Supports dumping/loading config files/"'@
-hprop_dump_config :: H.Property
-hprop_dump_config = integrationRetryWorkspace 2 "dump-config-files" $ \tmpDir -> H.runWithDefaultWatchdog_ $ do
+-- @DISABLE_RETRIES=1 cabal test cardano-testnet-test --test-options '-p "/Can be started with P2P topology file/"'@
+hprop_p2p_topology :: H.Property
+hprop_p2p_topology = integrationRetryWorkspace 2 "p2p-topology" $ \tmpDir -> H.runWithDefaultWatchdog_ $ do
 
   let testnetOptions = def { cardanoOutputDir = UserProvidedEnv tmpDir }
       genesisOptions = def { genesisEpochLength = 200 }
-      byronGenesisFile = tmpDir </> "byron-genesis.json"
-      shelleyGenesisFile = tmpDir </> "shelley-genesis.json"
+      someTopologyFile = tmpDir </> "node-data" </> "node1" </> "topology.json"
 
   -- Generate the sandbox
   conf <- mkConf tmpDir
   createTestnetEnv
-    testnetOptions genesisOptions def
+    testnetOptions genesisOptions P2PTopology
     NoUserProvidedData NoUserProvidedData NoUserProvidedData
-    -- Do not add hashes to the main config file, so that genesis files
-    -- can be modified without having to recompute hashes every time.
-    conf{genesisHashesPolicy = WithoutHashes}
+    conf
 
-  -- Wait for 20% more than `startTimeOffsetSeconds`, to ensure that
-  -- the time bounds in the sandbox' config files are no longer valid
-  H.threadDelay $ double2Int $ realToFrac startTimeOffsetSeconds * 1_000_000 * 1.2
-
-  currentTime <- H.noteShowIO Time.getCurrentTime
-  startTime <- H.noteShow $ Time.addUTCTime startTimeOffsetSeconds currentTime
-
-  -- Update start time in Byron genesis file
-  eByron <- runExceptT $ Byron.readGenesisData byronGenesisFile
-  (byronGenesis', _byronHash) <- H.leftFail eByron
-  let byronGenesis = byronGenesis'{gdStartTime = startTime}
-  H.lbsWriteFile byronGenesisFile $ canonicalEncodePretty byronGenesis
-
-  -- Update start time in Shelley genesis file
-  eShelley <- H.readJsonFile shelleyGenesisFile
-  shelleyGenesis' :: ShelleyGenesis <- H.leftFail eShelley
-  let shelleyGenesis = shelleyGenesis'{sgSystemStart = startTime}
-  H.lbsWriteFile shelleyGenesisFile $ encodePretty shelleyGenesis
+  -- Check that the topology is indeed P2P
+  eTopology <- H.readJsonFile someTopologyFile
+  (_topology :: P2P.NetworkTopology NodeId) <- H.leftFail eTopology
 
   -- Run testnet with generated config
   TestnetRuntime
