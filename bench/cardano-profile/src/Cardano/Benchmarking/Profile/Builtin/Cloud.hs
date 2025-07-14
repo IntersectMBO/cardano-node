4660fbb91fbede0ad5518403f24b6e0e4cf451bc
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2025-07-14 16:24:34 +0000

    wb | more memory limited value-volt-lmdb nomadperf profiles

--- bench/cardano-profile/src/Cardano/Benchmarking/Profile/Builtin/Cloud.hs
+++ bench/cardano-profile/src/Cardano/Benchmarking/Profile/Builtin/Cloud.hs
@@ -170,6 +170,8 @@ profilesNoEraCloud =
               . P.workloadAppend C.cgroupMemoryWorkload
   , valueVolt & P.name "value-volt-lmdb-10gb-nomadperf"    . mem 10000 . P.dreps  10000 . P.newTracing . P.p2pOn . lmdb
               . P.workloadAppend C.cgroupMemoryWorkload
+  , valueVolt & P.name "value-volt-lmdb-9.5gb-nomadperf"   . mem  9500 . P.dreps  10000 . P.newTracing . P.p2pOn . lmdb
+              . P.workloadAppend C.cgroupMemoryWorkload
   , valueVolt & P.name "value-volt-lmdb-9gb-nomadperf"     . mem  9000 . P.dreps  10000 . P.newTracing . P.p2pOn . lmdb
               . P.workloadAppend C.cgroupMemoryWorkload
   -- Plutus (pre-Voltaire profiles)
