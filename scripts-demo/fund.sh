66f2e723004f87b4d1ef888ca02d104fa5379753
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2025-08-01 11:26:02 +0200

    Fix funding script

--- scripts-demo/fund.sh
+++ scripts-demo/fund.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 # Example usage:
 # ./scripts-demo/fund.sh addr_test1vq922scgdwrrfa3n2pzu3empkju9ekregg0tza0xnveya3gfl0ycn 1000000000
+
+set -euo pipefail
+
+trap 'rm -f fundint.txbody' EXIT
+rm -f funding.txbody funding.tx
+
+run_cardano_cli() {
+  cabal run -v0 cardano-cli -- "$@"
+}
+
 export CARDANO_NODE_SOCKET_PATH=./testnet-data/socket/node1/sock
 export CARDANO_NODE_NETWORK_ID=42
 SRC_ADDR=$(cat testnet-data/utxo-keys/utxo1/utxo.addr)
-SRC_UTXO=$(cabal run cardano-cli -- latest query utxo --address $SRC_ADDR | jq -r 'keys'[0])
-cabal run cardano-cli -- latest transaction build --tx-in $SRC_UTXO --tx-out $1+$2 --change-address $SRC_ADDR --out-file funding.txbody
-cabal run cardano-cli -- latest transaction sign --tx-body-file funding.txbody --signing-key-file testnet-data/utxo-keys/utxo1/utxo.skey --out-file funding.tx
-cabal run cardano-cli -- latest transaction submit --tx-file funding.tx
-rm -f funding.txbody funding.tx
+SRC_UTXO=$(run_cardano-cli latest query utxo --address "$SRC_ADDR" | jq -r 'keys[0]')
+run_cardano-cli latest transaction build --tx-in "$SRC_UTXO" --tx-out "$1+$2" --change-address "$SRC_ADDR" --out-file funding.txbody
+run_cardano-cli latest transaction sign --tx-body-file funding.txbody --signing-key-file testnet-data/utxo-keys/utxo1/utxo.skey --out-file funding.tx
+run_cardano-cli latest transaction submit --tx-file funding.tx
