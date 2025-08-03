53ba27dd4a2a43fb1b824c5fbf7cf63fb65d44d2
Pablo Lamela <pablo.lamela@iohk.io>
2025-08-03 18:12:37 +0200

    Update deps and fix fund script

--- scripts-demo/fund.sh
+++ scripts-demo/fund.sh
@@ -14,7 +14,7 @@ run_cardano_cli() {
 export CARDANO_NODE_SOCKET_PATH=./testnet-data/socket/node1/sock
 export CARDANO_NODE_NETWORK_ID=42
 SRC_ADDR=$(cat testnet-data/utxo-keys/utxo1/utxo.addr)
-SRC_UTXO=$(run_cardano-cli latest query utxo --address "$SRC_ADDR" | jq -r 'keys[0]')
-run_cardano-cli latest transaction build --tx-in "$SRC_UTXO" --tx-out "$1+$2" --change-address "$SRC_ADDR" --out-file funding.txbody
-run_cardano-cli latest transaction sign --tx-body-file funding.txbody --signing-key-file testnet-data/utxo-keys/utxo1/utxo.skey --out-file funding.tx
-run_cardano-cli latest transaction submit --tx-file funding.tx
+SRC_UTXO=$(run_cardano_cli latest query utxo --address "$SRC_ADDR" | jq -r 'keys[0]')
+run_cardano_cli latest transaction build --tx-in "$SRC_UTXO" --tx-out "$1+$2" --change-address "$SRC_ADDR" --out-file funding.txbody
+run_cardano_cli latest transaction sign --tx-body-file funding.txbody --signing-key-file testnet-data/utxo-keys/utxo1/utxo.skey --out-file funding.tx
+run_cardano_cli latest transaction submit --tx-file funding.tx
