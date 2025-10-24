e0e44647938e5673f763a1936761e19bb2221579
Javier Sagredo <javier.sagredo@iohk.io>
2025-10-24 15:28:16 +0200

    Stealing tracers from #6333

--- cardano-node/src/Cardano/Node/TraceConstraints.hs
+++ cardano-node/src/Cardano/Node/TraceConstraints.hs
@@ -25,6 +25,7 @@ import           Ouroboros.Consensus.Ledger.SupportsMempool (ApplyTxErr, HasTxId
 import           Ouroboros.Consensus.Node.NetworkProtocolVersion
                    (HasNetworkProtocolVersion (BlockNodeToClientVersion, BlockNodeToNodeVersion))
 import           Ouroboros.Consensus.Node.Run (RunNode, SerialiseNodeToNodeConstraints)
+import           Ouroboros.Consensus.Peras.SelectView
 import           Ouroboros.Consensus.Protocol.Abstract (SelectView, ValidationErr)
 import           Ouroboros.Consensus.Shelley.Ledger.Mempool (GenTx, TxId)
 import           Ouroboros.Network.Block (Serialised)
@@ -53,7 +54,7 @@ type TraceConstraints blk =
     , ToObject (LedgerError blk)
     , ToObject (LedgerEvent blk)
     , ToObject (OtherHeaderEnvelopeError blk)
-    , ToObject (SelectView (BlockProtocol blk))
+    , ToObject (WeightedSelectView (BlockProtocol blk))
     , ToObject (ValidationErr (BlockProtocol blk))
     , ToObject (CannotForge blk)
     , ToObject (ForgeStateUpdateError blk)
@@ -68,7 +69,7 @@ type TraceConstraints blk =
     , LogFormatting (LedgerUpdate blk)
     , LogFormatting (LedgerWarning blk)
     , LogFormatting (OtherHeaderEnvelopeError blk)
-    , LogFormatting (SelectView (BlockProtocol blk))
+    , LogFormatting (WeightedSelectView (BlockProtocol blk))
     , LogFormatting (ValidationErr (BlockProtocol blk))
     , LogFormatting (CannotForge blk)
     , LogFormatting (ForgeStateUpdateError blk)
