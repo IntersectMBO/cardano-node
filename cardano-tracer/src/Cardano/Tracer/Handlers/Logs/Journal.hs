e4f2bc7464b2eb6837d3f4452aed64be86e3d5ff
Michael Karg <michael.karg@iohk.io>
2025-11-19 18:20:59 +0100

    cardano-tracer (maintenance): split up Journal implementation into internal modules

--- cardano-tracer/src/Cardano/Tracer/Handlers/Logs/Journal.hs
+++ cardano-tracer/src/Cardano/Tracer/Handlers/Logs/Journal.hs
@@ -1,72 +1,11 @@
 {-# LANGUAGE CPP #-}
-#ifdef SYSTEMD
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
-#endif
 
 module Cardano.Tracer.Handlers.Logs.Journal
-  ( writeTraceObjectsToJournal
+  ( module Impl
   ) where
 
 #ifdef SYSTEMD
-import qualified Cardano.Logging as L
-#endif
-import           Cardano.Logging (TraceObject (..))
-import           Cardano.Tracer.Configuration (LogFormat (..))
-import           Cardano.Tracer.Handlers.Utils (normalizeNamespace)
-import           Cardano.Tracer.Types (NodeName)
-
-#ifdef SYSTEMD
-import           Data.Char (isDigit)
-import           Data.Maybe (fromMaybe)
-import qualified Data.HashMap.Strict as HM
-import qualified Data.Text as T
-import           Data.Text.Encoding (encodeUtf8)
-import           Data.Time.Format (defaultTimeLocale, formatTime)
-
-import           Systemd.Journal (Priority (..), message, mkJournalField, priority,
-                   sendJournalFields, syslogIdentifier)
-
--- | Store 'TraceObject's in Linux systemd's journal service.
-writeTraceObjectsToJournal :: LogFormat -> NodeName -> [TraceObject] -> IO ()
-writeTraceObjectsToJournal logFormat nodeName =
-  mapM_ (sendJournalFields . mkJournalFields)
- where
-  -- when no forHuman message is implemented for a trace, fallback to forMachine (same as for file handler)
-  getMsg :: TraceObject -> T.Text
-  getMsg = case logFormat of
-    ForMachine -> toMachine
-    ForHuman   -> \TraceObject{toHuman, toMachine} -> fromMaybe toMachine toHuman
-
-  mkJournalFields trObj@TraceObject{toSeverity, toNamespace, toThreadId, toTimestamp} =
-       syslogIdentifier nodeName
-    <> message (getMsg trObj)
-    <> priority (mkPriority toSeverity)
-    <> HM.fromList
-         [ (namespace, encodeUtf8 $ mkName toNamespace)
-         , (thread,    encodeUtf8 $ T.filter isDigit toThreadId)
-         , (time,      encodeUtf8 $ formatAsIso8601 toTimestamp)
-         ]
-
-  mkName (normalizeNamespace -> ns) = if T.null ns then "noname" else ns
-
-  namespace = mkJournalField "namespace"
-  thread    = mkJournalField "thread"
-  time      = mkJournalField "time"
-
-  formatAsIso8601 = T.pack . formatTime defaultTimeLocale "%F %T%12QZ"
-
-  mkPriority L.Debug     = Debug
-  mkPriority L.Info      = Info
-  mkPriority L.Notice    = Notice
-  mkPriority L.Warning   = Warning
-  mkPriority L.Error     = Error
-  mkPriority L.Critical  = Critical
-  mkPriority L.Alert     = Alert
-  mkPriority L.Emergency = Emergency
+import           Cardano.Tracer.Handlers.Logs.Journal.Systemd as Impl
 #else
--- It works only on Linux distributions with systemd support.
-writeTraceObjectsToJournal :: LogFormat -> NodeName -> [TraceObject] -> IO ()
-writeTraceObjectsToJournal _ _ _ = pure ()
+import           Cardano.Tracer.Handlers.Logs.Journal.NoSystemd as Impl
 #endif
