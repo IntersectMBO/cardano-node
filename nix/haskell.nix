0fbf77d357161d56a6d4390774a7385a5fc1c107
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2025-11-28 12:00:21 +1300

    Try ghc966 again

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -21,7 +21,7 @@ let
     {
       src = ../.;
       name = "cardano-node";
-      compiler-nix-name = lib.mkDefault "ghc967";
+      compiler-nix-name = lib.mkDefault "ghc966";
       # Extra-compilers
       # flake.variants = lib.genAttrs ["ghc$VERSION"] (x: {compiler-nix-name = x;});
       cabalProjectLocal = ''
