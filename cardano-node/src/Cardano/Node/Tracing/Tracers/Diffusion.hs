1043764932623b09cf43ff9b990a38190fc5dce8
Federico Mastellone <federico.pascual-mastellone@iohk.io>
2026-02-12 22:07:37 +0000

    tracing: remove ouroboros-network:framework-tracing Ouroboros.Network

--- cardano-node/src/Cardano/Node/Tracing/Tracers/Diffusion.hs
+++ cardano-node/src/Cardano/Node/Tracing/Tracers/Diffusion.hs
@@ -28,6 +28,7 @@ import qualified Ouroboros.Network.Diffusion.Types as Diff
 import           Ouroboros.Network.PeerSelection.LedgerPeers (NumberOfPeers (..), PoolStake (..),
                    TraceLedgerPeers (..))
 import qualified Ouroboros.Network.Protocol.Handshake.Type as HS
+import           Ouroboros.Network.Tracing ()
 
 import           Data.Aeson (Value (String), (.=))
 import qualified Data.List as List
@@ -41,80 +42,6 @@ import           Network.Mux.TCPInfo (StructTCPInfo (..))
 #endif
 import           Network.Mux.Types (SDUHeader (..), unRemoteClockModel)
 import           Network.TypedProtocol.Codec (AnyMessage (..))
-
---------------------------------------------------------------------------------
--- Handshake Tracer
---------------------------------------------------------------------------------
-
-instance (Show term, Show ntcVersion) =>
-  LogFormatting (AnyMessage (HS.Handshake ntcVersion term)) where
-  forMachine _dtal (AnyMessageAndAgency stok msg) =
-    mconcat [ "kind" .= String kind
-            , "msg" .= (String . showT $ msg)
-            , "agency" .= String (pack $ show stok)
-            ]
-    where
-      kind = case msg of
-        HS.MsgProposeVersions {} -> "ProposeVersions"
-        HS.MsgReplyVersions   {} -> "ReplyVersions"
-        HS.MsgQueryReply      {} -> "QueryReply"
-        HS.MsgAcceptVersion   {} -> "AcceptVersion"
-        HS.MsgRefuse          {} -> "Refuse"
-
-  forHuman (AnyMessageAndAgency stok msg) =
-    "Handshake (agency, message) = " <> "(" <> showT stok <> "," <> showT msg <> ")"
-
-instance MetaTrace (AnyMessage (HS.Handshake a b)) where
-  namespaceFor (AnyMessage msg) = Namespace [] $ case msg of
-    HS.MsgProposeVersions {} -> ["ProposeVersions"]
-    HS.MsgReplyVersions   {} -> ["ReplyVersions"]
-    HS.MsgQueryReply      {} -> ["QueryReply"]
-    HS.MsgAcceptVersion   {} -> ["AcceptVersion"]
-    HS.MsgRefuse          {} -> ["Refuse"]
-
-  severityFor (Namespace _ [sym]) _ = case sym of
-    "ProposeVersions" -> Just Debug
-    "ReplyVersions"   -> Just Debug
-    "QueryReply"      -> Just Debug
-    "AcceptVersion"   -> Just Debug
-    "Refuse"          -> Just Debug
-    _otherwise        -> Nothing
-  severityFor _ _ = Nothing
-
-  documentFor (Namespace _ sym) = wrap . mconcat $ case sym of
-    ["ProposeVersions"] ->
-      [ "Propose versions together with version parameters.  It must be"
-      , " encoded to a sorted list.."
-      ]
-    ["ReplyVersions"]   ->
-      [ "`MsgReplyVersions` received as a response to 'MsgProposeVersions'.  It"
-      , " is not supported to explicitly send this message. It can only be"
-      , " received as a copy of 'MsgProposeVersions' in a simultaneous open"
-      , " scenario."
-      ]
-    ["QueryReply"]      ->
-      [ "`MsgQueryReply` received as a response to a handshake query in "
-      , " 'MsgProposeVersions' and lists the supported versions."
-      ]
-    ["AcceptVersion"]   ->
-      [ "The remote end decides which version to use and sends chosen version."
-      , "The server is allowed to modify version parameters."
-      ]
-    ["Refuse"]          -> ["It refuses to run any version."]
-    _otherwise          -> [] :: [Text]
-    where
-      wrap it = case it of
-        ""  -> Nothing
-        it' -> Just it'
-
-  allNamespaces = [
-      Namespace [] ["ProposeVersions"]
-    , Namespace [] ["ReplyVersions"]
-    , Namespace [] ["QueryReply"]
-    , Namespace [] ["AcceptVersion"]
-    , Namespace [] ["Refuse"]
-    ]
-
 
 --------------------------------------------------------------------------------
 -- DiffusionInit Tracer
