857aa7a3bf0b87c942bee5d30de7f729078d7faa
Georgy Lukyanov <georgy.lukyanov@iohk.io>
2026-04-21 16:09:36 +0200

    Bump the `npcExperimentalHardForksEnabled`-gated ProtVer to 12
    - The experimental hard fork becomes `ProtVer 12`
    - `ProtVer 11` is not the default

--- cardano-node/src/Cardano/Node/Protocol/Cardano.hs
+++ cardano-node/src/Cardano/Node/Protocol/Cardano.hs
@@ -181,8 +181,8 @@ mkSomeConsensusProtocolCardano NodeByronProtocolConfiguration {
           shelleyBasedLeaderCredentials = shelleyLeaderCredentials
         }
       , Consensus.cardanoProtocolVersion = if npcExperimentalHardForksEnabled
-                                           then ProtVer (natVersion @11) 0
-                                           else ProtVer (natVersion @10) 8
+                                           then ProtVer (natVersion @12) 0
+                                           else ProtVer (natVersion @11) 0
         -- The remaining arguments specify the parameters needed to transition between two eras
       , Consensus.cardanoLedgerTransitionConfig =
           Ledger.mkLatestTransitionConfig
