e8a43cee4b10a246ff9c5d8a4c99f922959537f4
Russoul <ruslan.feizerakhmanov@iohk.io>
2026-04-23 14:13:40 +0400

    cardano-recon-framework: Support referral to nested event properties

--- bench/cardano-recon-framework/src/Cardano/ReCon/Trace/Event.hs
+++ bench/cardano-recon-framework/src/Cardano/ReCon/Trace/Event.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Cardano.ReCon.Trace.Event where
 
 import           Cardano.Logging.Types.TraceMessage (TraceMessage (..))
@@ -14,28 +15,37 @@ import           Data.Map.Strict (Map)
 import           Data.Maybe (isJust)
 import           Data.Text (Text, unpack)
 
-extractIntProps :: Object -> Map VariableIdentifier IntValue
-extractIntProps = Map.delete "kind" . Map.mapMaybe f . Map.mapKeysMonotonic toText . KeyMap.toMap
-  where
-    f (Number v) = Just (truncate v)
-    f _          = Nothing
+class Extractable a where
+  extract :: Value -> Maybe a
 
-extractTextProps :: Object -> Map VariableIdentifier Text
-extractTextProps = Map.delete "kind" . Map.mapMaybe f . Map.mapKeysMonotonic toText . KeyMap.toMap
+instance Extractable IntValue where
+  extract (Number n) = Just (truncate n)
+  extract _          = Nothing
+
+instance Extractable Text where
+  extract (String s) = Just s
+  extract _          = Nothing
+
+extractProps :: Extractable a => Object -> Map VariableIdentifier a
+extractProps = Map.delete "kind" . go ""
   where
-    f (String v) = Just v
-    f _          = Nothing
+    go prefix = Map.foldlWithKey' (\acc k v ->
+        let key = prefix <> k
+        in case v of
+          Object nested -> Map.union acc (go (key <> ".") nested)
+          _             -> maybe acc (\val -> Map.insert key val acc) (extract v)
+      ) Map.empty . Map.mapKeysMonotonic toText . KeyMap.toMap
 
 instance Event TemporalEvent Text where
   ofTy (TemporalEvent _ msgs) c = isJust $ find (\msg -> msg.tmsgNS == c) msgs
   intProps (TemporalEvent _ msgs) c =
     case find (\msg -> msg.tmsgNS == c) msgs of
-      Just x  -> extractIntProps x.tmsgData
+      Just x  -> extractProps x.tmsgData
       Nothing -> error ("Not an event of type " <> unpack c)
   textProps (TemporalEvent _ msgs) c =
     case find (\msg -> msg.tmsgNS == c) msgs of
       Just x  -> Map.insert "host"   x.tmsgHost   $
                    Map.insert "thread" x.tmsgThread $
-                     extractTextProps x.tmsgData
+                     extractProps x.tmsgData
       Nothing -> error ("Not an event of type " <> unpack c)
   beg (TemporalEvent t _) = t
