70f225e1c77ddb7bf157a0e66f24d8d83f0324cd
John Lotoski <john.lotoski@iohk.io>
2026-08-05 13:32:06 -0500

    cfg: rm dead mainnet-ci, byron-testnet config dirs
    Neither directory is referenced anywhere in the tree.
    
    configuration/mainnet-ci/ last had a content change in ee9ad13a9
    (2020-04-06). Its consumers were both removed years ago:
    
      dbb7120da 2020-10-05  Remove chairmans-cluster nixos test.
      46b6d0712 2021-03-25  nixos: remove unsused cardano-cluster-service and
                            systemd template support
    
    It was also used by the earliest docker image work, 475049cd5 (2020-02-24),
    which has long since moved to configuration/cardano.
    
    configuration/defaults/byron-testnet/ was last touched by f7902ed3f
    (2023-06-01), and that was a repo wide sweep removing ApplicationName and
    ApplicationVersion rather than a use of the directory.

--- configuration/mainnet-ci/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-## Run this inside the `cardano-sl` repository:
-
-nix-build -j auto --cores 0 -A mkGenesis --arg genesisArgs '{ systemStart = 1000000000; configurationKey = "mainnet_ci_full"; configurationKeyLaunch = "mainnet_ci"; }'
