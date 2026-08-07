89b2fe38ace31287e63d8fda07fa920e7956dc54
John Lotoski <john.lotoski@iohk.io>
2026-08-07 17:25:40 -0500

    nix: replace deprecated pkgs.hostPlatform with stdenv.hostPlatform

--- nix/set-git-rev.nix
+++ nix/set-git-rev.nix
@@ -6,13 +6,13 @@ buildPackages.runCommand drv.name
   inherit (drv) exeName exePath meta passthru;
   # this is to ensure we are re-signing macOS binaries that might have been
   # mutilated by set-git-rev (e.g. patching in the git revision.)
-  nativeBuildInputs = lib.optionals hostPlatform.isDarwin [ darwin.signingUtils ];
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ darwin.signingUtils ];
 } (''
   mkdir -p $out
   cp --no-preserve=timestamps --recursive ${drv}/* $out/
   chmod -R +w $out/bin
   ${pkgsBuildBuild.haskellBuildUtils}/bin/set-git-rev "${gitrev}" $out/bin/*
-'' + lib.optionalString hostPlatform.isDarwin ''
+'' + lib.optionalString stdenv.hostPlatform.isDarwin ''
   for exe in $out/bin/*; do
     signIfRequired "$exe"
   done
