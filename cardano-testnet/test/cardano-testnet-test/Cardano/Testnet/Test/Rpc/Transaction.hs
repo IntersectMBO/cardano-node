cc487bccb4edab333ad2b40909ec02472e3a2446
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2026-09-07 19:01:41 +0200

    Disambiguate colliding proto field imports in RPC tests

--- cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Rpc/Transaction.hs
+++ cardano-testnet/test/cardano-testnet-test/Cardano/Testnet/Test/Rpc/Transaction.hs
@@ -18,9 +18,9 @@ import qualified Cardano.Api.Ledger as L
 
 import qualified Cardano.Rpc.Client as Rpc
 import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Query as U5c hiding (cardano)
-import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Query as UtxoRpc
-import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Submit as U5c
-import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Submit as UtxoRpc
+import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Query as UtxoRpc hiding (items)
+import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Submit as U5c hiding (items)
+import qualified Cardano.Rpc.Proto.Api.UtxoRpc.Submit as UtxoRpc hiding (items)
 import           Cardano.Rpc.Server.Internal.UtxoRpc.Predicate (exactAddressPredicate)
 import           Cardano.Rpc.Server.Internal.UtxoRpc.Type
 import           Cardano.Testnet
